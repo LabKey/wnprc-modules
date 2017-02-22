bf02576299d900489b2fa759afcb5a1bee17d3f1
Jon Richardson <jonathon.m.richardson@gmail.com>
2017-02-21 18:09:13 -0600

    Drop webutils first.

--- WebUtils/resources/schemas/dbscripts/postgresql/webutils-15.10-15.11.sql
+++ WebUtils/resources/schemas/dbscripts/postgresql/webutils-15.10-15.11.sql
@@ -1,3 +1,4 @@
 -- Create schema, tables, indexes, and constraints used for WebUtils module here
 -- All SQL VIEW definitions should be created in webutils-create.sql and dropped in webutils-drop.sql
+DROP SCHEMA webutils CASCADE;
 CREATE SCHEMA webutils;

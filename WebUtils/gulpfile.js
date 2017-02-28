b5abf07970c9fb8ec2c6cbbb7fe1db747d4225b1
Jon Richardson <jonathon.m.richardson@gmail.com>
2017-02-28 17:53:18 -0600

    Add a precompile step for WebUtils.

--- WebUtils/gulpfile.js
+++ WebUtils/gulpfile.js
@@ -1,8 +1,22 @@
 const gulp = require("gulp");
 const lkpm = require('lkpm');
+const path = require("path");
+
+var buildDir = path.join(__dirname, "build");
+var compiledResourcesDir = path.join(buildDir, "compiledResources");
+
+var compileJSLibTask = "javascript-libraries";
+gulp.task(compileJSLibTask, function() {
+    return gulp.src(path.join("resources", "**", "*"))
+        .pipe(gulp.dest(path.join(compiledResourcesDir)));
+});
 
 var taskExporter = new lkpm.TaskExporter({
     moduleBase: __dirname
 }, gulp);
+
+var stageTask = taskExporter.tasks[lkpm.TASK_NAMES.STAGE_STATIC];
+stageTask.dependencies.push(compileJSLibTask);
+stageTask.setResourceRoot(compiledResourcesDir);
 
 taskExporter.exportTasks();

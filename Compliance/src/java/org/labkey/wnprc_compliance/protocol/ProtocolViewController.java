1e6e3ec2c70650402195e8960751cd1b69673f9e
Jon Richardson <jonathon.m.richardson@gmail.com>
2017-03-24 17:48:22 -0500

    Fix permissions

--- Compliance/src/java/org/labkey/wnprc_compliance/protocol/ProtocolViewController.java
+++ Compliance/src/java/org/labkey/wnprc_compliance/protocol/ProtocolViewController.java
@@ -2,10 +2,12 @@ package org.labkey.wnprc_compliance.protocol;
 
 import org.labkey.api.action.SpringActionController;
 import org.labkey.api.security.ActionNames;
+import org.labkey.api.security.RequiresPermission;
 import org.labkey.api.security.RequiresSiteAdmin;
 import org.labkey.webutils.api.action.ReactPageAction;
 import org.labkey.webutils.api.action.annotation.JspPath;
 import org.labkey.webutils.api.action.annotation.PageTitle;
+import org.labkey.wnprc_compliance.security.ComplianceAdminPermission;
 
 /**
  * Created by jon on 3/21/17.
@@ -28,13 +30,13 @@ public class ProtocolViewController extends SpringActionController {
     @ActionNames("EditProtocol")
     @JspPath("protocol/view/EditProtocol.jsp")
     @PageTitle("New Protocol")
-    @RequiresSiteAdmin()
+    @RequiresPermission(ComplianceAdminPermission.class)
     public class NewProtocolPage extends ProtocolPageAction {}
 
 
     @ActionNames("ProtocolList")
     @JspPath("protocol/view/ProtocolList.jsp")
     @PageTitle("Protocols List")
-    @RequiresSiteAdmin()
+    @RequiresPermission(ComplianceAdminPermission.class)
     public class ListProtocolsPage extends ProtocolPageAction {}
 }

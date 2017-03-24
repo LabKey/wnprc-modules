4cfff17acdb632f1c67e93b67f4f0bf10652f41f
Jon Richardson <jonathon.m.richardson@gmail.com>
2017-03-23 19:25:12 -0500

    Remove flash from block by only calling blockUI() when we need to.

--- Compliance/src/ts/pages/new-protocol.tsx
+++ Compliance/src/ts/pages/new-protocol.tsx
@@ -119,25 +119,40 @@ interface BlockableDivProps {
     className: string;
 }
 
-class BlockableDiv extends React.Component<BlockableDivProps, {}> {
+class BlockableDiv extends React.Component<BlockableDivProps, {isBlocked: boolean}> {
     id: string = newUUID();
     _div: HTMLDivElement;
+    private _isBlocked: boolean;
 
     constructor(props: BlockableDivProps) {
         super(props);
+
+        this._isBlocked = props.disabled;
     }
 
     componentDidMount() {
+        console.log('create');
         if (this._div) {
             let $div = $(this._div) as any;
-            (this.props.disabled) ? $div.block({message: null}) : $div.unblock();
+
+            if (this.props.disabled) {
+                $div.block({message: null});
+            }
+            else {
+                $div.unblock();
+            }
         }
     }
 
     componentDidUpdate() {
+        console.log('update');
         if (this._div) {
             let $div = $(this._div) as any;
-            (this.props.disabled) ? $div.block({message: null}) : $div.unblock();
+
+            if (this._isBlocked !== this.props.disabled) {
+                (this.props.disabled) ? $div.block({message: null}) : $div.unblock();
+                this._isBlocked = this.props.disabled;
+            }
         }
     }
 

a07165ecd8a704bc60da6c84e1ee4aead40f47b1
Jonathon Richardson <jonathon.m.richardson@gmail.com>
2017-03-26 22:40:39 -0500

    Refactor main to match required signature for main and use a set to remove duplicates from the list of classes to compile.

--- src/main/java/com/github/jonathonrichardson/java2ts/Compiler.java
+++ src/main/java/com/github/jonathonrichardson/java2ts/Compiler.java
@@ -11,6 +11,8 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Enumeration;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Created by jon on 3/24/17.
@@ -27,7 +29,10 @@ public class Compiler {
         CompilerOutput output = new CompilerOutput();
         output.outfiles = new HashMap<>();
 
-        for (String className : this.manifest.fullyQualifiedClassNames) {
+        Set<String> classesToCompile = new HashSet<>();
+        classesToCompile.addAll(this.manifest.fullyQualifiedClassNames);
+
+        for (String className : classesToCompile) {
             Class clazz = this.getClass().getClassLoader().loadClass(className);
             this.namespace.addClass(clazz);
         }
@@ -37,22 +42,43 @@ public class Compiler {
         return output;
     }
 
-    public static void main() throws IOException, ClassNotFoundException {
+    public static void main(String[] args) {
         Manifest mainManifest = new Manifest();
         ObjectMapper mapper = new ObjectMapper();
 
-        Enumeration<URL> manifests = Compiler.class.getClassLoader().getResources("/java2ts/manifest.json");
-        while(manifests.hasMoreElements()) {
-            Manifest curManifest = mapper.readValue(manifests.nextElement().openStream(), Manifest.class);
-            mainManifest.fullyQualifiedClassNames.addAll(curManifest.fullyQualifiedClassNames);
+        try {
+            //System.out.println("Gathering manifests");
+
+            Enumeration<URL> manifests = Thread.currentThread().getContextClassLoader().getResources("java2ts/manifest.json");
+            while (manifests.hasMoreElements()) {
+                URL manifestURL = manifests.nextElement();
+                System.out.println(String.format(
+                        "Reading manifest: %s",
+                        manifestURL.toString()
+                ));
+
+                Manifest curManifest = mapper.readValue(manifestURL.openStream(), Manifest.class);
+                //System.out.println(String.format(
+                //        "Manifest has %s lines",
+                //        curManifest.fullyQualifiedClassNames.size()
+                //));
+                mainManifest.fullyQualifiedClassNames.addAll(curManifest.fullyQualifiedClassNames);
+            }
+
+            Compiler compiler = new Compiler(mainManifest);
+
+            ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+            compiler.compile(buffer);
+
+            Path outFilePath = Paths.get(System.getProperty("user.dir"), "GeneratedFromJava.ts");
+            if (args.length > 0) {
+                outFilePath = Paths.get(args[0]);
+            }
+
+            Files.write(outFilePath, buffer.toByteArray());
         }
-
-        Compiler compiler = new Compiler(mainManifest);
-
-        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
-        compiler.compile(buffer);
-
-        Path outFilePath = Paths.get(System.getProperty("user.dir"), "GeneratedFromJava.ts");
-        Files.write(outFilePath, buffer.toByteArray());
+        catch (IOException|ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 }

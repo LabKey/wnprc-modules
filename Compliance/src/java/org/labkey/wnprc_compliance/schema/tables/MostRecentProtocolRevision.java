e102a355fc76dc117f32228c6be1639f64ecc779
Jon Richardson <jonathon.m.richardson@gmail.com>
2017-03-28 14:35:17 -0500

    Rework table to use jOOQTableInfo from DBUtils.

--- Compliance/src/java/org/labkey/wnprc_compliance/schema/tables/MostRecentProtocolRevision.java
+++ Compliance/src/java/org/labkey/wnprc_compliance/schema/tables/MostRecentProtocolRevision.java
@@ -1,11 +1,10 @@
 package org.labkey.wnprc_compliance.schema.tables;
 
 import org.jetbrains.annotations.Nullable;
-import org.jooq.Select;
-import org.labkey.api.data.*;
-import org.labkey.api.query.ExprColumn;
+import org.jooq.*;
 import org.labkey.api.query.UserSchema;
 import org.labkey.dbutils.jooq.jOOQConnection;
+import org.labkey.dbutils.jooq.jOOQTableInfo;
 import org.labkey.wnprc_compliance.WNPRC_ComplianceSchema;
 
 import static org.labkey.wnprc_compliance.model.jooq.Tables.PROTOCOLS;
@@ -14,38 +13,19 @@ import static org.labkey.wnprc_compliance.model.jooq.Tables.PROTOCOL_REVISIONS;
 /**
  * Created by jon on 3/27/17.
  */
-public class MostRecentProtocolRevision extends AbstractTableInfo {
+public class MostRecentProtocolRevision extends jOOQTableInfo {
     public static String NAME = "MostRecentProtocolRevision";
-    private UserSchema schema;
 
     public MostRecentProtocolRevision(@Nullable UserSchema userSchema) {
-        super(DbSchema.get(WNPRC_ComplianceSchema.NAME), NAME);
-        schema = userSchema;
-
-        this.addColumn(new ExprColumn(this, PROTOCOL_REVISIONS.PROTOCOL_ID.getName(), new SQLFragment(ExprColumn.STR_TABLE_ALIAS + "." + PROTOCOL_REVISIONS.PROTOCOL_ID.getName().toString()), JdbcType.VARCHAR));
-        this.addColumn(new ExprColumn(this, PROTOCOL_REVISIONS.APPROVAL_DATE.getName(), new SQLFragment(ExprColumn.STR_TABLE_ALIAS + "." + PROTOCOL_REVISIONS.APPROVAL_DATE.getName().toString()), JdbcType.DATE));
+        super(NAME, WNPRC_ComplianceSchema.NAME, userSchema);
     }
 
-
     @Override
-    public SQLFragment getFromSQL() {
-        SQLFragment sql = new SQLFragment();
-
-
+    public Table<? extends Record> getjOOQTable() {
         try (jOOQConnection jOOQConnection = new jOOQConnection(WNPRC_ComplianceSchema.NAME)) {
-            String query = jOOQConnection.create().select(PROTOCOL_REVISIONS.PROTOCOL_ID, PROTOCOL_REVISIONS.APPROVAL_DATE)
+            return jOOQConnection.create().select(PROTOCOL_REVISIONS.PROTOCOL_ID, PROTOCOL_REVISIONS.APPROVAL_DATE, PROTOCOL_REVISIONS.HAS_PHYSICAL_HAZARDS)
                     .from(PROTOCOL_REVISIONS)
-                    .getSQL();
-
-            sql.append(query);
+                    .asTable();
         }
-
-        return sql;
-    }
-
-    @Nullable
-    @Override
-    public UserSchema getUserSchema() {
-        return schema;
     }
 }

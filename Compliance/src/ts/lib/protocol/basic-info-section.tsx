7ae7df739bb42eb11be70fd8c533fb4a2355f95f
Jon Richardson <jonathon.m.richardson@gmail.com>
2017-03-28 02:07:39 -0500

    Factor form out of frame.

--- Compliance/src/ts/lib/protocol/basic-info-section.tsx
+++ Compliance/src/ts/lib/protocol/basic-info-section.tsx
@@ -3,9 +3,51 @@ import Component = React.Component;
 import {BasicInfoForm} from "../../../../build/generated-ts/GeneratedFromJava";
 import {buildURLWithParams, getCurrentContainer} from "../../../../lkpm/modules/WebUtils/src/ts/WebUtils/LabKey";
 import {getJSON, postJSON} from "WebUtils/API";
-import {EditableSection} from "../editable-section";
 import {TextInput} from "./fields/text-input";
-import * as toastr from "toastr";
+import {FormProvider, SimpleFormEditor, SimpleFormProps} from "../editable-form";
+
+let basicInfoFormProvider: FormProvider<BasicInfoForm> = {
+    title: "Basic Info",
+
+    getFormData: (id: string) => {
+
+        let url = buildURLWithParams('wnprc_compliance-protocol-api', 'getBasicInfo', getCurrentContainer(), {'revision_id': id});
+
+        return getJSON(url, {}).then((rawData: any) => {
+            let data = BasicInfoForm.fromJSON(rawData);
+
+            return data;
+        });
+    },
+
+    saveFormData: (id: string, form: BasicInfoForm | null) => {
+        if (form == null) {
+            return Promise.resolve(new BasicInfoForm());
+        }
+
+        let url = buildURLWithParams('wnprc_compliance-protocol-api', 'saveBasicInfo', getCurrentContainer(), {'revision_id': id});
+
+        return postJSON(url, form as Object, {});
+    },
+
+    cloneForm: (form: BasicInfoForm) => {
+        return form.clone();
+    },
+
+    renderForm: (form: BasicInfoForm, enabled: boolean) => {
+        if (form == null) {
+            form = new BasicInfoForm();
+        }
+
+        return (
+            <fieldset className="form-horizontal" disabled={!enabled}>
+                <TextInput editable={enabled} value={form.principal_investigator || ''} label="Principal Investigator" property_name="principal_investigator"  />
+                <TextInput editable={enabled} value={form.spi_primary || ''} label="SPI Primary" property_name="spi_primary" />
+                <TextInput editable={enabled} value={form.spi_secondary || ''} label="SPI Secondary" property_name="spi_secondary" />
+            </fieldset>
+        )
+    }
+};
 
 export interface ProtocolBasicInfoProps {
     revision_id: string;
@@ -13,118 +55,14 @@ export interface ProtocolBasicInfoProps {
     endEdit?: () => void;
 }
 
-type Status = "loading" | "saving" | "editing" | "viewing" | "error"
+class BasicFormEditor extends SimpleFormEditor<BasicInfoForm> {
 
-interface ProtocolBasicInfoState {
-    status: Status;
-    form: BasicInfoForm | null;
-    errorMessage: string | null;
 }
 
-export class ProtocolBasicInfoEditor extends Component<ProtocolBasicInfoProps, ProtocolBasicInfoState> {
-    originalForm: BasicInfoForm | null;
-
-    constructor(props: any) {
-        super(props);
-
-        let state: ProtocolBasicInfoState = {
-            status: "loading",
-            form: null,
-            errorMessage: null
-        };
-        this.state = state;
-
-        this.load();
-
-        this.load = this.load.bind(this);
-        this.handleLoad = this.handleLoad.bind(this);
-        this.edit = this.edit.bind(this);
-        this.save = this.save.bind(this);
-        this.cancel = this.cancel.bind(this);
-    }
-
-    load() {
-        if (this.state.status != 'loading') {
-            this.setState({
-                status: 'loading'
-            });
-        }
-
-        let url = buildURLWithParams('wnprc_compliance-protocol-api', 'getBasicInfo', getCurrentContainer(), {'revision_id': this.props.revision_id});
-
-        getJSON(url, {}).then((rawData: any) => {
-            let data = BasicInfoForm.fromJSON(rawData);
-
-            this.handleLoad(data);
-        })
-    }
-
-    handleLoad(info: BasicInfoForm) {
-        this.originalForm = info.clone();
-
-        this.setState({
-            status: 'viewing',
-            form: info
-        });
-    }
-
-    edit() {
-        this.setState({
-            status: 'editing'
-        });
-
-        if (this.props.startEdit) {
-            this.props.startEdit();
-        }
-    }
-
-    save() {
-        this.setState({
-            status: 'saving'
-        });
-
-        let url = buildURLWithParams('wnprc_compliance-protocol-api', 'saveBasicInfo', getCurrentContainer(), {'revision_id': this.props.revision_id});
-
-        postJSON(url, this.state.form as Object, {}).then((rawData: any) => {
-            this.originalForm = (this.state.form == null) ? null : this.state.form.clone();
-
-            this.setState({
-                status: 'viewing'
-            });
-
-            toastr.success("Successfully saved basic info");
-        });
-
-        if (this.props.endEdit) {
-            this.props.endEdit();
-        }
-    }
-
-    cancel() {
-        this.setState({
-            status: 'viewing',
-            form: this.originalForm
-        });
-
-        if (this.props.endEdit) {
-            this.props.endEdit();
-        }
-    }
-
+export class ProtocolBasicInfoEditor extends React.Component<ProtocolBasicInfoProps, {}> {
     render() {
-        let status = this.state.status;
-        let disabled = (status != 'editing');
-        let form: BasicInfoForm = this.state.form || new BasicInfoForm();
-
         return (
-            <EditableSection title="Basic Info" editMode={(status == 'editing') ? true : (status == 'viewing') ? false : null}
-                             handleEdit={this.edit} handleSave={this.save} handleCancel={this.cancel}>
-                <fieldset className="form-horizontal" disabled={disabled}>
-                    <TextInput editable={!disabled} value={form.principal_investigator || ''} label="Principal Investigator" property_name="principal_investigator"  />
-                    <TextInput editable={!disabled} value={form.spi_primary || ''} label="SPI Primary" property_name="spi_primary" />
-                    <TextInput editable={!disabled} value={form.spi_secondary || ''} label="SPI Secondary" property_name="spi_secondary" />
-                </fieldset>
-            </EditableSection>
+            <BasicFormEditor formProvider={basicInfoFormProvider} {...this.props} />
         )
     }
 }

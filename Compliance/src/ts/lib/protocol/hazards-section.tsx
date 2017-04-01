48bd7f04312285663f8c64b767f4a5dc63b26f19
Jonathon Richardson <jonathon.m.richardson@gmail.com>
2017-04-01 17:40:46 -0500

    Finish up working example of UI for hazards checklist.

--- Compliance/src/ts/lib/protocol/hazards-section.tsx
+++ Compliance/src/ts/lib/protocol/hazards-section.tsx
@@ -9,6 +9,9 @@ import {getJSON, postJSON} from "WebUtils/API";
 import {TextInput} from "./fields/text-input";
 import {FormProvider, SimpleFormEditor, SimpleFormProps} from "../editable-form";
 import {URLForAction} from "../../../../../WebUtils/build/generated-ts/GeneratedFromJava";
+import ChangeEvent = React.ChangeEvent;
+import {newUUID} from "../../../../lkpm/modules/WebUtils/src/ts/WebUtils/Util";
+import * as s from "underscore.string";
 
 export interface HazardsEditorProps {
     revision_id: string;
@@ -23,6 +26,8 @@ class HazardsFormEditor extends SimpleFormEditor<HazardsForm> {
 }
 
 export class HazardsEditor extends React.Component<HazardsEditorProps, {}> {
+    private _id: string = newUUID();
+
     render() {
         let hazardFormProvider: FormProvider<HazardsForm> = {
             title: "Hazards",
@@ -54,9 +59,56 @@ export class HazardsEditor extends React.Component<HazardsEditorProps, {}> {
                     form = new HazardsForm();
                 }
 
+                type CheckboxFieldName = keyof HazardsForm;
+                let checkboxNames: CheckboxFieldName[] = ["biological", "chemical", "physical", "other"];
+                let checkboxes = checkboxNames.map((fieldName: CheckboxFieldName) => {
+                    let id = `${this._id}-${fieldName}`;
+
+                    let handleChange = (name: CheckboxFieldName) => {
+                        return (e: ChangeEvent<HTMLInputElement>) => {
+                            let target = e.target;
+                            let value = target.type === 'checkbox' ? target.checked : target.value;
+                            form[name] = value;
+
+                            if (name == 'other' && !value) {
+                                form.other_notes = '';
+                            }
+
+                            updateForm(form);
+                        };
+                    };
+
+                    return (
+                        <div className="checkbox checkbox-primary form-group col-sm-3" key={fieldName}>
+                            <input type="checkbox" checked={!!form[fieldName]} name={id} onChange={handleChange(fieldName)} />
+
+                            <label htmlFor={id}>
+                                {s.titleize(fieldName)}
+                            </label>
+                        </div>
+                    )
+                });
+
+                let handleOtherNotesChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+                    form.other_notes = e.target.value;
+                    updateForm(form);
+                };
+
                 return (
-                    <fieldset className="form-horizontal" disabled={!enabled}>
-                        <h1>Hello</h1>
+                    <fieldset className="form" disabled={!enabled}>
+                        <div className="container-fluid">
+                            <div className="row form-inline" style={{marginBottom: '15px'}}>
+                                {checkboxes}
+                            </div>
+
+                            <div className="row">
+                                <div className="form-group col-sm-12">
+                                    <label className="control-label">Explain Other Hazards:</label>
+                                    <textarea value={form.other_notes || ''} onChange={handleOtherNotesChange} className="form-control" style={{resize: 'none'}} />
+                                </div>
+                            </div>
+
+                        </div>
                     </fieldset>
                 )
             }

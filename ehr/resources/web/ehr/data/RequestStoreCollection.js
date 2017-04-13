204c6fbc1b7108a8cb0b3a5c621be12ddcc9bd99
Jon Richardson <jonathon.m.richardson@gmail.com>
2017-04-13 13:21:09 -0500

    Apply fix from TaskStoreCollection to RequestStoreCollection to ensure that requestids get properly applied to submitted requests.

--- ehr/resources/web/ehr/data/RequestStoreCollection.js
+++ ehr/resources/web/ehr/data/RequestStoreCollection.js
@@ -20,5 +20,43 @@ Ext4.define('EHR.data.RequestStoreCollection', {
         }
 
         return this.callParent([model]);
+    },
+
+    commitChanges: function(){
+        // ensure all records are using this requestid and alert if not
+        var requestid = this.getRequestId();
+        if (requestid){
+            this.clientStores.each(function(cs){
+                if (cs.getFields().get('requestid') != null){
+                    cs.each(function(r){
+                        if (requestid != r.get('requestid')){
+                            LDK.Assert.assertEquality('Incorrect requestid for client store:' + cs.storeId, requestid, r.get('requestid'));
+                            r.beginEdit();
+                            r.set('requestid', this.getRequestId());
+                            r.endEdit(true);
+                        }
+                    }, this);
+                }
+            }, this);
+
+            this.serverStores.each(function(cs){
+                if (cs.getFields().get('requestid') != null){
+                    cs.each(function(r){
+                        if (r.isRemovedRequest){
+                            return;  //do not check these records.  they have deliberately been separated.
+                        }
+
+                        if (requestid != r.get('requestid')){
+                            LDK.Assert.assertEquality('Incorrect requestid for server store:' + cs.storeId, requestid, r.get('requestid'));
+                            r.beginEdit();
+                            r.set('requestid', this.getRequestId());
+                            r.endEdit(true);
+                        }
+                    }, this);
+                }
+            }, this);
+        }
+
+        return this.callParent(arguments);
     }
 });

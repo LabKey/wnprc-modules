0f0cc4bd45f8f0598a269980094cfa6cae524627
Clay Stevens <cstevens@primate.wisc.edu>
2017-10-17 09:58:15 -0500

    fixed up the docker business to point to 15.2
    also eliminated the need for the TeamCity user/password by using guestAuth

--- docker/wait-for-postgres.sh
+++ docker/wait-for-postgres.sh
@@ -6,9 +6,21 @@ shift
 cmd="$@"
 
 >&2 echo "Waiting for postgres..."
-until psql -h "$host" -U "postgres" -c '\l' &>/dev/null; do
+
+limit=120
+count=0
+
+while [ $count -lt $limit ]; do
+  if PGPASSWORD="$PG_PASS" psql -h "$host" -U "$PG_USER" -d "$PG_NAME" -c "\l" &>/dev/null; then
+    break;
+  fi
   sleep 1
+  let count=count+1
 done
 
->&2 echo "Starting LabKey (postgres ready)"
+if [ $count -lt $limit ]; then
+  >&2 echo "Starting LabKey (postgres ready)"
+else
+  >&2 echo "Starting LabKey (postgres still down after $limit attempts, starting anyway)"
+fi
 exec $cmd

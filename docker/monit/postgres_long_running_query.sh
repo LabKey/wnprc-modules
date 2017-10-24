88ce82ba79ca0341ed8d0ac7fd2856b50c16d6bc
Clay Stevens <cstevens@primate.wisc.edu>
2017-10-24 14:56:55 -0500

    updated the postgres hostname for monit

--- docker/monit/postgres_long_running_query.sh
+++ docker/monit/postgres_long_running_query.sh
@@ -5,7 +5,7 @@ threshold=120000
 timeformat=0000000000D999
 
 # executes a query to determine if there have been any long-running queries
-psql -h localhost -p 9081 -U postgres -d labkey <<EOF >/dev/null && [ ! -s "$filename" ]
+psql -h postgres -U postgres -d labkey <<EOF >/dev/null && [ ! -s "$filename" ]
   \copy (select replace(query, chr(10), ' ') query, calls, to_char(total_time, '$timeformat') total_time, to_char(total_time/calls, '$timeformat') mean_time from pg_stat_statements where total_time/calls > $threshold order by mean_time desc) to '$filename' (format csv, delimiter ';', force_quote *);
   select pg_stat_statements_reset();
 EOF

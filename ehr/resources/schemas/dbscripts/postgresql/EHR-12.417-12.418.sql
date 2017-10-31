49f204b9e6e30ac16eee412aed9705d4457b45b6
Clay Stevens <cstevens@primate.wisc.edu>
2017-10-31 14:15:36 -0500

    remove the ehr module
    we should be relying on LabKey for this one, so we can get our own changes out of GitHub and do them in the SVN if necessary

--- ehr/resources/schemas/dbscripts/postgresql/EHR-12.417-12.418.sql
+++
@@ -1,7 +0,0 @@
-/*
- * Copyright (c) 2014 LabKey Corporation
- *
- * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
- */
-DROP INDEX ehr.encounter_flags_objectid;
-DROP INDEX ehr.encounter_flags_parentid;
--- ehr/resources/schemas/dbscripts/postgresql/obsolete/EHR_Lookups-12.343-12.344.sql
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (c) 2013-2016 LabKey Corporation
- *
- * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
- */
---NOTE: this is a placeholder b/c an index is created for SQLServer on snomed that isnt supported in PG
-;
-
-

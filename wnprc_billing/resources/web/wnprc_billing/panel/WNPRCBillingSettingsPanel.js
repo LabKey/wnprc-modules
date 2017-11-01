245105409f0ec82888d58acbba3a3bb80a7a809c
Ed  Younskevicius <eyounske@labkey.com>
2017-11-01 02:02:40 +0000

    Update copyrights for 17.3

--- wnprc_billing/resources/web/wnprc_billing/panel/WNPRCBillingSettingsPanel.js
+++ wnprc_billing/resources/web/wnprc_billing/panel/WNPRCBillingSettingsPanel.js
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2017 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
+ */
 Ext4.define('wnprc_billing.panel.WNPRCBillingSettingsPanel', {
     extend: 'Ext.panel.Panel',
 

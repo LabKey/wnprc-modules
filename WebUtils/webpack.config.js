97d40af3b9c68089f24e75dc39cdd585014ac47c
Clay Stevens <cstevens@primate.wisc.edu>
2017-11-03 16:15:18 -0500

    moved the gradle tasks for typescript and webpack to the main, got webpack and jsp working

--- WebUtils/webpack.config.js
+++ WebUtils/webpack.config.js
@@ -1,63 +1,65 @@
 const path = require('path');
-var webpack = require("webpack");
 
-module.exports = {
-    entry: {
-        "legacy": path.resolve(__dirname, 'src', 'ts', 'legacy.ts')
-    },
+// expected environment variables:
+//
+//   env.BUILD_DIR: the Gradle build directory (i.e., '[labkey root]/build/modules/[module name]')
+//
+module.exports = function(env) {
+    return {
+        entry: path.resolve(__dirname, 'src', 'ts', 'legacy.ts'),
 
-    output: {
-        path: path.resolve(__dirname, 'build', 'explodedModule', 'web', 'webutils', 'lib'),
-        filename: '[name].js',
-        // Export the module as the SimpleFilter variable
-        library: "WebUtils",
-        libraryTarget: "umd",
-        sourceMapFilename: '[name].js.map'
-    },
-
-    // Enable sourcemaps for debugging webpack's output.
-    // TODO: switch to 'source-map' for production
-    devtool: "eval-source-map",
-
-    resolve: {
-        alias: {
-            classify: path.join(__dirname, "./external_resources/js/classify.js"),
-            supersqlstore: path.join(__dirname, "./external_resources/js/supersqlstore.js")
+        output: {
+            path: path.resolve(env.BUILD_DIR, 'explodedModule', 'web', 'webutils', 'lib'),
+            filename: 'webutils.js',
+            library: 'WebUtils',
+            libraryTarget: 'umd',
+            sourceMapFilename: 'webutils.js.map'
         },
 
-        // Add '.ts' and '.tsx' as resolvable extensions.
-        extensions: [".ts", ".tsx", ".js"],
+        // Enable sourcemaps for debugging webpack's output.
+        // TODO: switch to 'source-map' for production
+        devtool: "eval-source-map",
 
-        // Include Bower to find certain modules.
-        modules: ["node_modules"],
-        descriptionFiles: ["package.json"]
-    },
+        resolve: {
+            alias: {
+                classify: path.join(__dirname, "./external_resources/js/classify.js"),
+                supersqlstore: path.join(__dirname, "./external_resources/js/supersqlstore.js")
+            },
 
-    module: {
-        rules: [
-            // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
-            { test: /\.tsx?$/, loader: "ts-loader" },
+            // Add '.ts' and '.tsx' as resolvable extensions.
+            extensions: [".ts", ".tsx", ".js"],
 
-            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-            { test: /\.js$/, loader: "source-map-loader", enforce: "pre" },
+            // Include Bower to find certain modules.
+            modules: ["node_modules"],
+            descriptionFiles: ["package.json"]
+        },
 
-            { test: /\.scss$/, loader: 'style!css!sass' }
-        ]
-    },
+        module: {
+            rules: [
+                // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+                {test: /\.tsx?$/, loader: "ts-loader"},
 
-    externals: {
-        "c3":     'c3',
-        "d3":     'd3',
-        "fetch":  'fetch',
-        "jquery": 'jQuery',
-        "knockout": "ko",
-        "moment": 'moment',
-        "qunit":  'QUnit',
-        "Qunit":  "Qunit",
-        "react":  'React',
-        "React":  'React',
+                // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+                {test: /\.js$/, loader: "source-map-loader", enforce: "pre"},
 
-        "react-dom":        'ReactDOM',
-        "react-dom/server": 'ReactDOMServer'
+                {test: /\.scss$/, loader: 'style!css!sass'}
+            ]
+        },
+
+        externals: {
+            'c3': 'c3',
+            'd3': 'd3',
+            'fetch': 'fetch',
+            'jquery': 'jQuery',
+            'knockout': 'ko',
+            'moment': 'moment',
+            'qunit': 'QUnit',
+            'Qunit': 'Qunit',
+            'react': 'React',
+            'React': 'React',
+
+            'react-dom': 'ReactDOM',
+            'react-dom/server': 'ReactDOMServer'
+        }
     }
 };

8e3c9e3a95c165d2aa0c9f74bd18d65d93e5c138
Clay Stevens <cstevens@primate.wisc.edu>
2017-12-06 10:28:59 -0600

    applied tslint fixes to the webpack.config.ts, too

--- breeding/webpack.config.ts
+++ breeding/webpack.config.ts
@@ -1,29 +1,30 @@
+// noinspection TsLint
 import { Configuration as WebpackConfig, optimize as Optimize } from 'webpack';
 
-module.exports = function(env: any): WebpackConfig {
+module.exports = function wp(env: any): WebpackConfig {
     return {
         devtool:    'source-map',
         entry:      './src/ts/index.tsx',
         externals: {
             'react':            'React',
+            'react-data-grid':  'ReactDataGrid',
             'react-dom':        'ReactDOM',
-            'react-data-grid':  'ReactDataGrid'
         },
         module: {
             rules: [
                 { test: /\.tsx?$/, loader: 'awesome-typescript-loader' },
-                { test: /\.js$/,   loader: 'source-map-loader', enforce: 'pre' }
-            ]
+                { test: /\.js$/,   loader: 'source-map-loader', enforce: 'pre' },
+            ],
         },
         output: {
             filename:   'breeding.js',
-            path:       `${env.BUILD_DIR}/explodedModule/web/breeding`
+            path:       `${env.BUILD_DIR}/explodedModule/web/breeding`,
         },
         plugins: [
-            //new Optimize.UglifyJsPlugin()
+            // new Optimize.UglifyJsPlugin()
         ],
         resolve: {
-            extensions: ['.ts', '.tsx', '.js', '.json' ]
-        }
+            extensions: ['.ts', '.tsx', '.js', '.json' ],
+        },
     };
 };

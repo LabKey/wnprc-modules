7eb485722fc973bf8a1cea219819f74a307cc9bc
Clay Stevens <cstevens@primate.wisc.edu>
2018-01-11 10:25:45 -0600

    switching from the custom async loader to use ES6 Promises instead
    for now, using Bluebird as the browser polyfill (in extjs.lib.xml)

--- breeding/resources/web/breeding/extjs/ChildRecordsPanel.js
+++ breeding/resources/web/breeding/extjs/ChildRecordsPanel.js
@@ -6,34 +6,6 @@
         extend: 'Ext.panel.Panel',
         layout: 'fit',
         loadFromStore: function (store) {
-            // creates a scoped helper object to co-ordinate the async functions from each of the children. essentially
-            // a poor-man's 'promise' library, which will only work in-so-far as all the calls to 'promise' happen
-            // before any of the calls to 'resolve' (which--in this instance--they _should_, but still be advised that
-            // there is no protection currently for an instance where they do interleave)
-            const onLoad = function (callback) {
-                var promises = 0;
-                return {
-                    promise: function () {
-                        promises++;
-                        return this.resolve;
-                    },
-                    resolve: function () {
-                        if ((--promises) === 0) callback();
-                    }
-                };
-            };
-
-            // creates and returns a new handler function for the success handler that closes over the passed resolve
-            // function argument so that each instance of the handler can resolve its own unique promise instance
-            const onSuccess = function (resolve) {
-                return function () {
-                    // noinspection JSUnresolvedFunction: 'this' is assumed to be a LABKEY.Query.DataRegion
-                    this.hideMessage(true);
-                    resolve();
-                };
-            };
-
-            const loader = onLoad(this.fireEvent.bind(this, 'childload'));
             // set the style for the child divs
             const style = 'margin: 0px 0px 10px 0px';
             // get the first record for the filtering
@@ -41,28 +13,40 @@
             // loop over each of the child record configs passed to the component and generate a new QueryWebPart to
             // display the data for the child record. that involves creating a blank div into which to render, adding
             // the parameters and the success handler, then creating and rendering the web part
-            (this.childRecords || []).forEach(function (c, i) {
-                // create the new element into which to render each child
-                const childId = this.id + '-child-' + i;
-                Ext.DomHelper.append(this.body, {tag: 'div', id: childId, style: style});
-                // add the filter for the participant id and the success handler
-                // noinspection JSUnresolvedVariable, JSUnresolvedFunction: standardButtons defined in the LABKEY code
-                Ext.apply(c, {
-                    buttonBar: {
-                        items: [
-                            LABKEY.QueryWebPart.standardButtons.insertNew,
-                            LABKEY.QueryWebPart.standardButtons.exportRows,
-                            LABKEY.QueryWebPart.standardButtons.print
-                        ]
-                    },
-                    failure: LABKEY.Utils.onError,
-                    filterArray: c.filterArrayFactory ? c.filterArrayFactory(record) : null,
-                    parameters: c.parametersFactory ? c.parametersFactory(record) : null,
-                    success: onSuccess(loader.promise())
-                });
-                // noinspection JSUnresolvedFunction: LABKEY.QueryWebPart defined in the LabKey code
-                (new LABKEY.QueryWebPart(c)).render(childId);
+            const callbacks = (this.childRecords || []).map(function (c, i) {
+                // noinspection JSUnresolvedFunction: requires native/polyfill ES6 Promise
+                return new Promise((function (resolve, reject) {
+                    // create the new element into which to render each child
+                    const childId = this.id + '-child-' + i;
+                    Ext.DomHelper.append(this.body, {tag: 'div', id: childId, style: style});
+                    // add the filter for the participant id and the success handler
+                    // noinspection JSUnresolvedVariable, JSUnresolvedFunction: standardButtons defined in the LABKEY code
+                    Ext.apply(c, {
+                        buttonBar: {
+                            items: [
+                                LABKEY.QueryWebPart.standardButtons.insertNew,
+                                LABKEY.QueryWebPart.standardButtons.exportRows,
+                                LABKEY.QueryWebPart.standardButtons.print
+                            ]
+                        },
+                        failure: reject,
+                        filterArray: c.filterArrayFactory ? c.filterArrayFactory(record) : null,
+                        parameters: c.parametersFactory ? c.parametersFactory(record) : null,
+                        success: function () {
+                            // noinspection JSUnresolvedFunction: 'this' is assumed to be a LABKEY.Query.DataRegion
+                            this.hideMessage(true);
+                            resolve();
+                        }
+                    });
+                    // noinspection JSUnresolvedFunction: LABKEY.QueryWebPart defined in the LabKey code
+                    (new LABKEY.QueryWebPart(c)).render(childId);
+                }).bind(this));
             }, this);
+            // wait for the child grids to load, then fire the childload event
+            // noinspection JSUnresolvedVariable: requires native/polyfill ES6 Promise
+            Promise.all(callbacks).then((function () {
+                this.fireEvent('childload');
+            }).bind(this));
         }
     });
 })(Ext4);
--- breeding/resources/web/breeding/extjs/ParentChildDetailPanel.js
+++ breeding/resources/web/breeding/extjs/ParentChildDetailPanel.js
@@ -4,44 +4,10 @@
     Ext.define('WNPRC.ext4.ParentChildDetailPanel', {
         alias: 'widget.wnprc-parentchilddetailpanel',
         extend: 'LDK.panel.WebpartPanel',
-
         initComponent: function () {
-            // creates a scoped helper object to co-ordinate the async functions from each of the children. essentially
-            // a poor-man's 'promise' library, which will only work in-so-far as all the calls to 'promise' happen
-            // before any of the calls to 'resolve' (which--in this instance--they _should_, but still be advised that
-            // there is no protection currently for an instance where they do interleave)
-            const onLoad = function (callback) {
-                var promises = 0;
-                return {
-                    promise: function () {
-                        promises++;
-                        return this.resolve;
-                    },
-                    resolve: function () {
-                        if ((--promises) === 0) callback();
-                    }
-                };
-            };
-
-            // create the loader instance to turn off the 'loading' mask and resume
-            // rendering of the components (to make it a more pleasing visual experience)
-            const loader = onLoad(function () {
-                Ext.defer(this.table.unmask, 500, this.table);
-                // noinspection JSUnresolvedFunction: Ext.resumeLayouts (4.1.0)
-                Ext.resumeLayouts(true);
-            }.bind(this));
-
             // define the handler for loading the child records after the initial loading is complete
             const DETAIL_PANEL_XTYPE = 'wnprc-detailpanel';
             const CHILD_RECORD_XTYPE = 'wnprc-childrecordspanel';
-            const onStoreLoad = function (store) {
-                // noinspection JSUnresolvedFunction: Ext.suspendLayouts (4.1.1)
-                Ext.suspendLayouts();
-                this.query(DETAIL_PANEL_XTYPE + ', ' + CHILD_RECORD_XTYPE).forEach(function (i) {
-                    i.loadFromStore(store);
-                }, this);
-            };
-
             // noinspection JSUnresolvedExtXType: Ext.panel.Panel (2.3.0)
             Ext.apply(this, {
                 items: [{
@@ -55,9 +21,6 @@
                         border: false,
                         childRecords: this.childRecords,
                         columnWidth: 0.75,
-                        listeners: {
-                            'childload': loader.promise()
-                        },
                         margin: '0 0 0 20',
                         xtype: CHILD_RECORD_XTYPE
                     }],
@@ -72,16 +35,36 @@
                 }
             });
             this.callParent(arguments);
-
-            // make sure the store was actually created rather than simply being the store _configuration_ (which is
-            // what we might get sometimes), then add the 'load' handlers to:
-            //   a) load the details and the child records and
-            //   b) resolve one of the promises in the loader to indicate the main load has finished
-            this.store = this.store.events ? this.store
-                    : Ext.create('LABKEY.ext4.data.Store', Ext.apply(this.store, {autoLoad: false}));
-            this.mon(this.store, 'load', onStoreLoad, this, {single: true});
-            this.mon(this.store, 'load', loader.promise(), null, {single: true});
-            this.store.load();
+            // noinspection JSUnresolvedFunction: requires native/polyfill ES6 Promise
+            const callbacks = [
+                // make sure the store was actually created rather than simply being the store _configuration_ (which is
+                // what we might get sometimes), then add the 'load' handlers to:
+                //   a) load the details and the child records and
+                //   b) resolve one of the promises in the loader to indicate the main load has finished
+                new Promise((function (resolve) {
+                    this.store = this.store.events ? this.store
+                            : Ext.create('LABKEY.ext4.data.Store', Ext.apply(this.store, {autoLoad: false}));
+                    this.mon(this.store, 'load', function (store) {
+                        // noinspection JSUnresolvedFunction: Ext.suspendLayouts (4.1.1)
+                        Ext.suspendLayouts();
+                        this.query(DETAIL_PANEL_XTYPE + ', ' + CHILD_RECORD_XTYPE).forEach(function (i) {
+                            i.loadFromStore(store);
+                        }, this);
+                        resolve();
+                    }, this, {single: true});
+                    this.store.load();
+                }).bind(this)),
+                // wait for each of the child record grids to load (the panel will fire an event)
+                new Promise((function (resolve) {
+                    this.mon(this.down(CHILD_RECORD_XTYPE), 'childload', resolve, null, {single: true});
+                }).bind(this))
+            ];
+            // noinspection JSUnresolvedVariable: requires native/polyfill ES6 Promise
+            Promise.all(callbacks).then((function () {
+                Ext.defer(this.table.unmask, 500, this.table);
+                // noinspection JSUnresolvedFunction: Ext.resumeLayouts (4.1.0)
+                Ext.resumeLayouts(true);
+            }).bind(this));
         }
     });
 })(Ext4);

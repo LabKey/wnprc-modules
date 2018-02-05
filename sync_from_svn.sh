5139477727e2fbb39e973cc28a9bd68d452527a6
Clay Stevens <cstevens@primate.wisc.edu>
2018-02-05 14:31:14 -0600

    small changes to make the script prettier under syntax highlighting

--- sync_from_svn.sh
+++ sync_from_svn.sh
@@ -26,21 +26,20 @@ foldermap=("server/customModules/WNPRC_EHR:WNPRC_EHR"
 repo="https://hedgehog.fhcrc.org/tor/stedi/branches/modules15.2/"
 
 # set the start/end revisions based on the command line arguments
-srcrev=$1
+srcrev=${1}
 tgtrev=${2:-HEAD}
 
 # loop through the folders and do the following:
 #   1) generate the diff for the repo between the start/end revisions (or HEAD)
 #   2) replace any instances of "src/org/..." with "src/java/org/..." (because the structure changed)
 #   3) apply the diff to the git repo, using the folder name specified in the map
-echo "Reading changes to $repo between $srcrev and $tgtrev..."
+echo "Reading changes to $repo between ${srcrev} and ${tgtrev}..."
 for i in "${foldermap[@]}"
 do
-    srcpath=${i%:*}
-    tgtpath=${i#*:}
-    echo "  * applying changes from '$srcpath' to '$tgtpath'"
+    srcpath=${i%:*} # keep the substring prior to the colon
+    tgtpath=${i#*:} # keep the substring after the colon
+    echo "  * applying changes from '${srcpath}' to '${tgtpath}'"
     svn diff --diff-cmd diff --git "${repo}${srcpath}" -r "${srcrev}:${tgtrev}" \
     | pipe_if_not_empty sed 's/src\/org/src\/java\/org/g' \
-    | pipe_if_not_empty git apply -p0 --directory="$tgtpath"
+    | pipe_if_not_empty git apply -p0 --directory="${tgtpath}"
 done
-

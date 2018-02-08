9118f1cb603f864ec30de7c2345aaa17c00cc283
Josh Eckels <jeckels@labkey.com>
2018-02-08 01:38:42 +0000

    Spec Issue 33033: WNPRC - Generate Invoice PDF
    Action that creates PDF with hard-coded values for line items and customer info, using the FPDF library

--- wnprc_billing/src/org/labkey/wnprc_billing/WNPRC_BillingController.java
+++ wnprc_billing/src/org/labkey/wnprc_billing/WNPRC_BillingController.java
@@ -16,14 +16,26 @@
 
 package org.labkey.wnprc_billing;
 
+import org.labkey.api.action.ExportAction;
 import org.labkey.api.action.SimpleViewAction;
 import org.labkey.api.action.SpringActionController;
 import org.labkey.api.security.RequiresPermission;
 import org.labkey.api.security.permissions.ReadPermission;
+import org.labkey.api.util.PageFlowUtil;
 import org.labkey.api.view.JspView;
 import org.labkey.api.view.NavTree;
+import org.labkey.ehr.pdf.InvoicePDF;
 import org.springframework.validation.BindException;
 import org.springframework.web.servlet.ModelAndView;
+import sun.java2d.opengl.OGLSurfaceData;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
 
 public class WNPRC_BillingController extends SpringActionController
 {
@@ -33,5 +45,27 @@ public class WNPRC_BillingController extends SpringActionController
     public WNPRC_BillingController()
     {
         setActionResolver(_actionResolver);
+    }
+
+    @RequiresPermission(ReadPermission.class)
+    public class PDFExportAction extends ExportAction
+    {
+        @Override
+        public void export(Object o, HttpServletResponse response, BindException errors) throws Exception
+        {
+            InvoicePDF pdf = new InvoicePDF();
+
+            pdf.addPage();
+
+            List<InvoicePDF.FormattedLineItem> items = new ArrayList<>();
+            for (int i = 1; i < 100; i++)
+            {
+                items.add(new InvoicePDF.FormattedLineItem(new Date(2017, 12, i / 4 + 1), "Item " + i, 3, 1.3f * i));
+            }
+            pdf.addLines(items);
+
+            PageFlowUtil.prepareResponseForFile(getViewContext().getResponse(), Collections.emptyMap(), "WNPRC_Invoice.pdf", true);
+            pdf.output(getViewContext().getResponse().getOutputStream());
+        }
     }
 }

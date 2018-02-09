c3ac0d279f9810c5b99b4906f85acee2d4837416
Clay Stevens <cstevens@primate.wisc.edu>
2018-02-09 14:51:16 -0600

    fixing the 15.2 code to use the new 17.X Java interfaces

--- WNPRC_EHR/src/java/org/labkey/wnprc_ehr/notification/HusbandryNotification.java
+++ WNPRC_EHR/src/java/org/labkey/wnprc_ehr/notification/HusbandryNotification.java
@@ -9,35 +9,41 @@ import org.labkey.api.query.FieldKey;
 import org.labkey.api.security.User;
 
 import java.util.Date;
-import java.util.List;
 
 public class HusbandryNotification extends AbstractEHRNotification
 {
     public HusbandryNotification(Module owner){super (owner);}
 
+    @Override
     public String getName(){return "Husbandry Notification";}
 
+    @Override
     public String getDescription(){
-        return "This notification gets send everytime there is a new VVC request";
+        return "This notification gets sent every time there is a new VVC request";
     }
-    public String getEmailSubject(){
+
+    @Override
+    public String getEmailSubject(Container c){
         return "New Husbandry Request Submitted on "+ _dateTimeFormat.format(new Date());
     }
 
+    @Override
     public String getScheduleDescription(){
         return "As soon as food deprive is submitted";
     }
 
+    @Override
     public String getCategory(){
         return " ";
     }
 
-    public String getMessage(Container c, User u){
+    @Override
+    public String getMessageBodyHTML(Container c, User u){
 
         return " ";
     }
 
-    public void getReciepients (Container c, User u, String requestId){
+    public void getRecipients (Container c, User u, String requestId){
         SimpleFilter filter = new SimpleFilter(FieldKey.fromString("requestId"),requestId);
 
         TableInfo ti = getStudySchema(c,u).getTable("requests");

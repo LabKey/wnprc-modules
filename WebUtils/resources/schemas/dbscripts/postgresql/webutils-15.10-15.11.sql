869724825f25466534bbc0276a1a11dfb5fac851
Clay Stevens <cstevens@primate.wisc.edu>
2018-02-09 10:43:20 -0600

    adding an "IF EXISTS" to make sure we do not fail when dropping

--- WebUtils/resources/schemas/dbscripts/postgresql/webutils-15.10-15.11.sql
+++ WebUtils/resources/schemas/dbscripts/postgresql/webutils-15.10-15.11.sql
@@ -16,5 +16,5 @@
 
 -- Create schema, tables, indexes, and constraints used for WebUtils module here
 -- All SQL VIEW definitions should be created in webutils-create.sql and dropped in webutils-drop.sql
-DROP SCHEMA webutils CASCADE;
+DROP SCHEMA IF EXISTS webutils CASCADE;
 CREATE SCHEMA webutils;

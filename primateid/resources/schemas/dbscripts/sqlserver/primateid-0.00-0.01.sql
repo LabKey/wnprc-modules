534b9c2710f3bfed28046d8b0b8b63e5a788c4bb
Clay Stevens <cstevens@primate.wisc.edu>
2018-03-13 15:25:02 -0500

    adding a migration script for SqlServer, too

---
+++ primateid/resources/schemas/dbscripts/sqlserver/primateid-0.00-0.01.sql
@@ -0,0 +1,11 @@
+CREATE SCHEMA primateid;
+GO
+
+CREATE TABLE  primateid.unique_ids (
+  container     ENTITYID    NOT NULL,
+  participantid VARCHAR(32) NOT NULL,
+  primateid     VARCHAR(10) NOT NULL,
+
+  CONSTRAINT pk_unique_ids           PRIMARY KEY (container, participantid),
+  CONSTRAINT uq_unique_ids_primateid UNIQUE      (container, primateid)
+);

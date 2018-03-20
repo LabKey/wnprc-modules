3c53ad2030a46806d69d98c2de6cdcf8752ea8a5
Clay Stevens <cstevens@primate.wisc.edu>
2018-03-20 13:05:10 -0500

    fixed the Selenium test for generating the ids
    note that it does not currently verify that the prefix is correct, or that all the appropriate animals have primate ids, etc. it really only validates the happy path and makes sure that clicking the button results in the success text

--- primateid/test/src/org/labkey/test/primateid/PrimateIdTest.java
+++ primateid/test/src/org/labkey/test/primateid/PrimateIdTest.java
@@ -4,16 +4,17 @@ import org.jetbrains.annotations.Nullable;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
-import org.labkey.test.BaseWebDriverTest;
+import org.labkey.test.ModulePropertyValue;
 import org.labkey.test.categories.External;
 import org.labkey.test.categories.PrimateId;
+import org.labkey.test.tests.ehr.AbstractEHRTest;
 
 import java.util.Collections;
 import java.util.List;
 import java.util.UUID;
 
 @Category({External.class, PrimateId.class})
-public class PrimateIdTest extends BaseWebDriverTest
+public class PrimateIdTest extends AbstractEHRTest
 {
     private static final String PROJECT_NAME = "PrimateId_Test_Project_" + UUID.randomUUID();
 
@@ -24,22 +25,38 @@ public class PrimateIdTest extends BaseWebDriverTest
     }
 
     @Override
+    protected String getModuleDirectory()
+    {
+        return null;
+    }
+
+    @Override
     public List<String> getAssociatedModules()
     {
         return Collections.singletonList("PrimateId");
+    }
+
+    @Override
+    protected void importStudy()
+    {
+        goToManageStudy();
+        importStudyFromZip(STUDY_ZIP);
     }
 
     /**
      * Test setup method. Creates a new project for the test and enables all associated modules.
      */
     @Before
-    public void setup()
+    public void setup() throws Exception
     {
-        if (!_containerHelper.getCreatedProjects().contains(getProjectName()))
-        {
-            _containerHelper.createProject(getProjectName(), "EHR");
-            getAssociatedModules().forEach(m -> _containerHelper.enableModule(getProjectName(), m));
-        }
+        if (_containerHelper.getCreatedProjects().contains(getProjectName()))
+            return;
+
+        initProject("EHR");
+        goToEHRFolder();
+        getAssociatedModules().forEach(m -> _containerHelper.enableModule(m));
+        setModuleProperties(Collections.singletonList(new ModulePropertyValue("PrimateId",
+                "/" + getContainerPath(), "PrimateIdPrefix", "XX")));
     }
 
     /**
@@ -49,7 +66,18 @@ public class PrimateIdTest extends BaseWebDriverTest
     @Test
     public void smoke()
     {
-        beginAt("/primateid/" + getProjectName() + "/begin.view");
+        beginAt("/primateid/" + getContainerPath() + "/begin.view");
         assertNoLabKeyErrors();
+    }
+
+    @Test
+    public void testGeneratePrimateIds()
+    {
+        // navigate to the admin page
+        beginAt("/primateid/" + getContainerPath() + "/begin.view");
+        assertNoLabKeyErrors();
+
+        // click the "Generate PrimateIDs" button
+        clickButton("Generate PrimateIDs", "Successfully generated PrimateIDs.");
     }
 }

fcba644d49d85ca16de3efe87b6d57c970480167
Clay Stevens <cstevens@primate.wisc.edu>
2018-05-07 11:45:04 -0500

    added some error handling in case the mutation observer is not implemented

--- WNPRC_EHR/resources/web/wnprc_ehr/reports/PregnancyReport.js
+++ WNPRC_EHR/resources/web/wnprc_ehr/reports/PregnancyReport.js
@@ -1,12 +1,18 @@
 Ext4.namespace('EHR.reports');
-EHR.reports.PregnancyReport = function(panel, tab) {
+EHR.reports.PregnancyReport = function (panel, tab) {
     const target = tab.add({xtype: 'ldk-contentresizingpanel'});
 
-    const observer = new MutationObserver(target.fireEvent.bind(target, 'contentsizechange'));
-    observer.observe(target.getEl().dom, {childList: true, subtree: true});
+    try {
+        // according to the DOM spec, the mutation observer should be GC'd if/when the target node is removed
+        const observer = new MutationObserver(target.fireEvent.bind(target, 'contentsizechange'));
+        observer.observe(target.getEl().dom, {childList: true, subtree: true});
+    }
+    catch (e) {
+        console.warn("Could not attach mutation observer. Resizing will rely on older APIs, may not work right");
+    }
 
     const wp = new LABKEY.WebPart({
-        partConfig: {subjects: (tab.filters.subjects||[]).join(';')},
+        partConfig: {subjects: (tab.filters.subjects || []).join(';')},
         partName: 'Breeding',
         renderTo: target.renderTarget,
         style: 'margin-bottom: 20px;'

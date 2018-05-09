4dc631235c1a8a6fbe22f52a38f0aaccf39ebcc4
Clay Stevens <cstevens@primate.wisc.edu>
2018-05-09 14:50:09 -0500

    specifically ordering the columns for easy entry, added metadata for breeding encounters

--- WNPRC_EHR/resources/web/wnprc_ehr/model/sources/Breeding.js
+++ WNPRC_EHR/resources/web/wnprc_ehr/model/sources/Breeding.js
@@ -4,15 +4,40 @@
  */
 EHR.model.DataModelManager.registerMetadata('Breeding', {
     byQuery: {
+        'study.breeding_encounters': {
+            Id:             { columnConfig: { columnIndex: 1 } },
+            sireid:         { columnConfig: { columnIndex: 2 } },
+            remark:         { columnConfig: { columnIndex: 3, width: 400 } },
+            ejaculation:    { columnConfig: { columnIndex: 4 } },
+            performedby:    { columnConfig: { columnIndex: 5 } },
+            date:           { columnConfig: { columnIndex: 6 } }
+        },
+        'study.pregnancies': {
+            Id:             { columnConfig: { columnIndex: 1 } },
+            sireid:         { columnConfig: { columnIndex: 2 } },
+            date_conception:{ columnConfig: { columnIndex: 3 } },
+            date_due:       { columnConfig: { columnIndex: 4 } },
+            remark:         { columnConfig: { columnIndex: 5, width: 400 } },
+            pregnancyid:    { columnConfig: { columnIndex: 6, width: 200 } },
+            performedby:    { columnConfig: { columnIndex: 7 } },
+            date:           { columnConfig: { columnIndex: 8 } }
+        },
+        'study.pregnancy_outcomes': {
+            Id:             { columnConfig: { columnIndex: 1 } },
+            outcome:        { columnConfig: { columnIndex: 2 } },
+            remark:         { columnConfig: { columnIndex: 3, width: 400 } },
+            pregnancyid:    { columnConfig: { columnIndex: 4, width: 200 } },
+            infantid:       { columnConfig: { columnIndex: 5 } },
+            offered_to:     { columnConfig: { columnIndex: 6 } },
+            performedby:    { columnConfig: { columnIndex: 7 } },
+            date:           { columnConfig: { columnIndex: 8 } }
+        },
         'study.ultrasounds': {
-            Id: {
-                editorConfig: {
-                    plugins: ['wnprc-animalfield']
-                }
-            },
-            pregnancyid: {
-                xtype: 'wnprc-pregnancyfield'
-            }
-        }
+            Id:             { columnConfig: { columnIndex: 1 } },
+            remark:         { columnConfig: { columnIndex: 2, width: 400 } },
+            pregnancyid:    { columnConfig: { columnIndex: 3, width: 200 } },
+            performedby:    { columnConfig: { columnIndex: 4 } },
+            date:           { columnConfig: { columnIndex: 5 } }
+        },
     }
 });

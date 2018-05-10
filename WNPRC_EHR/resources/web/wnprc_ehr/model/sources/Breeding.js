c583f4d64052ea614b3c6f0e2517733bc1e2f155
Clay Stevens <cstevens@primate.wisc.edu>
2018-05-10 15:55:35 -0500

    added the new fields after talking to tammie and lisa

--- WNPRC_EHR/resources/web/wnprc_ehr/model/sources/Breeding.js
+++ WNPRC_EHR/resources/web/wnprc_ehr/model/sources/Breeding.js
@@ -5,39 +5,48 @@
 EHR.model.DataModelManager.registerMetadata('Breeding', {
     byQuery: {
         'study.breeding_encounters': {
-            Id:             { columnConfig: { columnIndex: 1 } },
-            sireid:         { columnConfig: { columnIndex: 2 } },
-            remark:         { columnConfig: { columnIndex: 3, width: 400 } },
-            ejaculation:    { columnConfig: { columnIndex: 4 } },
-            performedby:    { columnConfig: { columnIndex: 5 } },
-            date:           { columnConfig: { columnIndex: 6 } }
+            Id:                     { columnConfig: { columnIndex: 1 } },
+            sireid:                 { columnConfig: { columnIndex: 2 } },
+            remark:                 { columnConfig: { columnIndex: 3, width: 400 } },
+            ejaculation:            { columnConfig: { columnIndex: 4 } },
+            project:                { columnConfig: { columnIndex: 5 } },
+            performedby:            { columnConfig: { columnIndex: 6 } },
+            date:                   { columnConfig: { columnIndex: 7 } }
         },
         'study.pregnancies': {
-            Id:             { columnConfig: { columnIndex: 1 } },
-            sireid:         { columnConfig: { columnIndex: 2 } },
-            date_conception:{ columnConfig: { columnIndex: 3 } },
-            date_due:       { columnConfig: { columnIndex: 4 } },
-            remark:         { columnConfig: { columnIndex: 5, width: 400 } },
-            pregnancyid:    { columnConfig: { columnIndex: 6, width: 200 } },
-            performedby:    { columnConfig: { columnIndex: 7 } },
-            date:           { columnConfig: { columnIndex: 8 } }
+            Id:                     { columnConfig: { columnIndex: 1 } },
+            sireid:                 { columnConfig: { columnIndex: 2 } },
+            date_conception:        { columnConfig: { columnIndex: 3 } },
+            date_due:               { columnConfig: { columnIndex: 4 } },
+            remark:                 { columnConfig: { columnIndex: 5, width: 400 } },
+            pregnancyid:            { columnConfig: { columnIndex: 6, width: 200 } },
+            performedby:            { columnConfig: { columnIndex: 7 } },
+            date:                   { columnConfig: { columnIndex: 8 } }
         },
         'study.pregnancy_outcomes': {
-            Id:             { columnConfig: { columnIndex: 1 } },
-            outcome:        { columnConfig: { columnIndex: 2 } },
-            remark:         { columnConfig: { columnIndex: 3, width: 400 } },
-            pregnancyid:    { columnConfig: { columnIndex: 4, width: 200 } },
-            infantid:       { columnConfig: { columnIndex: 5 } },
-            offered_to:     { columnConfig: { columnIndex: 6 } },
-            performedby:    { columnConfig: { columnIndex: 7 } },
-            date:           { columnConfig: { columnIndex: 8 } }
+            Id:                     { columnConfig: { columnIndex: 1 } },
+            outcome:                { columnConfig: { columnIndex: 2 } },
+            remark:                 { columnConfig: { columnIndex: 3, width: 400 } },
+            pregnancyid:            { columnConfig: { columnIndex: 4, width: 200 } },
+            infantid:               { columnConfig: { columnIndex: 5 } },
+            project:                { columnConfig: { columnIndex: 6 } },
+            performedby:            { columnConfig: { columnIndex: 7 } },
+            date:                   { columnConfig: { columnIndex: 8 } }
         },
         'study.ultrasounds': {
-            Id:             { columnConfig: { columnIndex: 1 } },
-            remark:         { columnConfig: { columnIndex: 2, width: 400 } },
-            pregnancyid:    { columnConfig: { columnIndex: 3, width: 200 } },
-            performedby:    { columnConfig: { columnIndex: 4 } },
-            date:           { columnConfig: { columnIndex: 5 } }
-        },
+            Id:                     { columnConfig: { columnIndex:  1 } },
+            remark:                 { columnConfig: { columnIndex:  2, width: 400 } },
+            pregnancyid:            { columnConfig: { columnIndex:  3, width: 200 } },
+            fetal_hearbeat:         { columnConfig: { columnIndex:  4 } },
+            beats_per_minute:       { columnConfig: { columnIndex:  5 } },
+            crown_rump_cm:          { columnConfig: { columnIndex:  6 } },
+            head_circumference_cm:  { columnConfig: { columnIndex:  7 } },
+            femur_length_cm:        { columnConfig: { columnIndex:  8 } },
+            biparietal_diameter_cm: { columnConfig: { columnIndex:  9 } },
+            project:                { columnConfig: { columnIndex: 10 } },
+            restraint:              { columnConfig: { columnIndex: 11 } },
+            performedby:            { columnConfig: { columnIndex: 12 } },
+            date:                   { columnConfig: { columnIndex: 13 } }
+        }
     }
 });

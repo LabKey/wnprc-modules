49353d1169d96e446903acff34cc038f9e2746a0
Clay Stevens <cstevens@primate.wisc.edu>
2018-05-10 12:00:48 -0500

    migrating away from noinspection to using jsdoc for externals

--- WNPRC_EHR/resources/web/wnprc_ehr/ext4/panel/AppendRecordGridPanel.js
+++ WNPRC_EHR/resources/web/wnprc_ehr/ext4/panel/AppendRecordGridPanel.js
@@ -1,48 +1,53 @@
-Ext4.define('WNPRC.grid.AppendRecordGridPanel', {
-    extend: 'EHR.grid.Panel',
-    alias: 'widget.wnprc-appendrecordgridpanel',
-
-    /** @type {Function[]} */
-    _queue: [],
-
-    /** @override */
-    initComponent: function () {
-        this.callParent(arguments);
-
-        const plugin = Ext4.create('WNPRC.plugin.ButtonHotKeyPlugin', { // from ButtonHotKeyPlugin.js
-            buttonId: 'appendRecordBtn',                                // from AppendRecordButton.js
-            keyCode: 187, // the '=' key, next to delete/backspace
-            shift: true
-        });
-
-        this.plugins = this.plugins || [];
-        this.plugins.push(plugin);
-
-        // the plugin needs to execute after a slight delay in order to allow for the store collection
-        // to set the default values for the model that would be created. for some reason, if it races
-        // into this handler, that function does not properly fill the "perfomed by" field
-        //   - clay, 09 May 2018
-        this.mon(this, 'afterrender', plugin.execute, plugin, {single: true, delay: 500});
-
-        this.mon(this, 'beforeedit',    this._flush, this);
-        this.mon(this, 'edit',          this._flush, this);
-        this.mon(this, 'validateedit',  this._flush, this);
-        this.mon(this, 'canceledit',    this._flush, this);
-    },
-
-    /**
-     * Enqueues the passed action in the list of actions that will get executed on the next edit event.
-     * @param {Function} lambda
-     */
-    enqueueEditAction: function (lambda) {
-        this._queue.push(lambda);
-    },
-
+/**
+ * @external Ext4
+ */
+(function () {
     /**
      * Executes all the actions currently in the edit action queue, clearing the queue as it goes.
      * @private
      */
-    _flush: function () {
+    function _flush() {
         while (this._queue.length) this._queue.shift()();
     }
-});
+
+    Ext4.define('WNPRC.grid.AppendRecordGridPanel', {
+        extend: 'EHR.grid.Panel',
+        alias: 'widget.wnprc-appendrecordgridpanel',
+
+        /** @type {Function[]} */
+        _queue: [],
+
+        /** @override */
+        initComponent: function () {
+            this.callParent(arguments);
+
+            const plugin = Ext4.create('WNPRC.plugin.ButtonHotKeyPlugin', { // from ButtonHotKeyPlugin.js
+                buttonId: 'appendRecordBtn',                                // from AppendRecordButton.js
+                keyCode: 187, // the '=' key, next to delete/backspace
+                shift: true
+            });
+
+            this.plugins = this.plugins || [];
+            this.plugins.push(plugin);
+
+            // the plugin needs to execute after a slight delay in order to allow for the store collection
+            // to set the default values for the model that would be created. for some reason, if it races
+            // into this handler, that function does not properly fill the "perfomed by" field
+            //   - clay, 09 May 2018
+            this.mon(this, 'afterrender', plugin.execute, plugin, {single: true, delay: 500});
+
+            this.mon(this, 'beforeedit',    this._flush, this);
+            this.mon(this, 'edit',          this._flush, this);
+            this.mon(this, 'validateedit',  this._flush, this);
+            this.mon(this, 'canceledit',    this._flush, this);
+        },
+
+        /**
+         * Enqueues the passed action in the list of actions that will get executed on the next edit event.
+         * @param {Function} lambda
+         */
+        enqueueEditAction: function (lambda) {
+            this._queue.push(lambda);
+        }
+    });
+})();

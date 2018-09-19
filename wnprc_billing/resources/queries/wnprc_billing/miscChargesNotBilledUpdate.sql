a47b63976e63e4c3edeb184dac305475d3ae3e7e
Binal Patel <binalp@labkey.com>
2018-09-19 05:45:28 +0000

    Spec 33035 : Pre-Billing Run Reports.

---
+++ wnprc_billing/resources/queries/wnprc_billing/miscChargesNotBilledUpdate.sql
@@ -0,0 +1,23 @@
+SELECT 
+  mcfr.Id,
+  mcfr.date,
+  mcfr.project,
+  mcfr.quantity,
+  mcfr.unitCost,
+  mcfr.totalcost,
+  mcfr.item,
+  mcfr.chargeId,
+  mcfr.chargeId.serviceCode,
+  mcfr.chargeId.departmentCode AS groupName,
+  mcfr.category,
+  mcfr.comment AS itemDescription,
+  mcfr.debitedAccount,
+  mcfr.debitedAccount.investigatorName AS principalInvestigator,
+  mcfr.debitedAccount.contact_name AS contactName,
+  mcfr.debitedAccount.po_amount AS poAmount,
+  mcfr.created,
+  mcfr.createdby,
+  mc.invoiceId,
+  mc.taskId
+FROM wnprc_billing.miscChargesFeeRates mcfr JOIN ehr_billing.miscCharges mc on mcfr.taskId = mc.taskId and mcfr.sourceRecord = mc.objectID
+WHERE mc.invoiceId IS NULL

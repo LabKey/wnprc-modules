44bfe6e3e60096f96016f396e2013fe5dd56cb3e
Gabriel <gabriel.gottfredsen@gmail.com>
2018-10-08 16:09:33 -0500

    add on hold procedures to manage requests

--- WNPRC_EHR/resources/queries/study/SurgeryProcedureIndividualRequests.sql
+++ WNPRC_EHR/resources/queries/study/SurgeryProcedureIndividualRequests.sql
@@ -1,6 +1,6 @@
 SELECT 'EDIT' as editLink,participantid,date,enddate,proceduretype,procedurename,location,project,account,surgeon,surgerytechneeded,spineeded,vetneeded,statuschangereason,qcstate,lsid FROM study.surgery_procedure sp
 WHERE sp.requestid IN
       (SELECT r.requestid FROM ehr.requests r
-      WHERE ISMEMBEROF(r.createdby) AND r.formtype = 'SurgeryProcedureRequest' AND r.qcstate = 7);
+      WHERE ISMEMBEROF(r.createdby) AND r.formtype = 'SurgeryProcedureRequest' AND r.qcstate in (7,13));
 
 --SELECT * FROM ehr.requests

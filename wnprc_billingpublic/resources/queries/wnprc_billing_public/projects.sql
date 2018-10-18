bea05a4abb9acfc01158e79029127bf5b99bb90a
Binal Patel <binalp@labkey.com>
2018-10-18 01:25:36 +0000

    Spec 35474 : Access to current budget status for investigators - Add projects to PI Portal main page - public invoiced items queries and metadata - invoiced items by project query and metadata - data access metadata - linked schema template

---
+++ wnprc_billingpublic/resources/queries/wnprc_billing_public/projects.sql
@@ -0,0 +1,9 @@
+SELECT
+  t.project,
+  cast(t.project.title as varchar(200)) as title,
+  ' [By Invoice]' as summaryByInvoice,
+  ' [All Items]' as allItems
+
+FROM wnprc_billing_public.publicInvoicedItems t
+WHERE t.project is not null
+GROUP BY t.project, t.project.title

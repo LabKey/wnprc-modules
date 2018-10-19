5a122cfa20e8972e1dade47db4cd74212c9cc123
Binal Patel <binalp@labkey.com>
2018-10-19 02:11:36 +0000

    Spec 35474 : Removed redundant links. Fixed broken lookups. Added comments. Minor cleanup.

--- wnprc_billing/resources/queries/ehr_billing/publicInvoicedItems.sql
+++ wnprc_billing/resources/queries/ehr_billing/publicInvoicedItems.sql
@@ -13,49 +13,58 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-SELECT
-  i.rowId,
-  i.invoiceId,
-  i.transactionNumber,
-  i.date,
-  i.invoiceDate,
-  i.Id,
-  i.item,
-  i.itemCode,
-  i.category,
-  i.servicecenter,
-  i.project,
-  i.debitedaccount,
-  i.creditedaccount,
-  i.faid,
-  i.investigatorId,
-  i.firstName,
-  i.lastName,
-  i.department,
-  i.mailcode,
-  i.contactPhone,
-  i.chargeId,
-  i.objectid,
-  i.quantity,
-  i.unitCost,
-  i.totalcost,
-  i.chargeCategory
+SELECT i.rowId,
+       i.invoiceId,
+       i.invoiceId.rowId as invoiceRunId,
+       i.date,
+       i.Id as animalId,
+       i.item,
+       i.itemCode,
+       i.category,
+       i.servicecenter,
+       i.project,
+       i.project.protocol as protocol,
+       i.project.inves    as projectContact,
+       i.debitedaccount,
+       i.creditedaccount,
+       i.investigatorId,
+       i.firstName,
+       i.lastName,
+       i.department,
+       i.mailcode,
+       i.contactPhone,
+       i.chargeId,
+       i.objectid,
+       i.quantity,
+       i.unitCost,
+       i.totalcost
 
 FROM ehr_billing.invoicedItems i
-WHERE ((SELECT max(rowid) AS expr FROM ehr_billing.dataAccess da WHERE isMemberOf(da.userid) AND (
-    da.allData = true OR
-    (da.project = i.project)
-      OR
-    (
-      da.investigatorId = i.investigatorId
-      OR da.investigatorId = i.debitedaccount.investigatorId
---       OR da.investigatorId = i.project.investigatorId <!-- TODO: Currently, there is no easy way to add an investigatorId to ehr.project (editing on ehr.project routes to a custom page which has investigatorId as hidden) -->
-    )
-  )) IS NOT NULL
-      OR
+WHERE (
+          (
+            SELECT max(rowid) AS expr
+              FROM ehr_billing.dataAccess da
+              WHERE
+                 isMemberOf(da.userid)
+                 AND
+                 (
+                    da.allData = true
+                    OR
+                    (da.project = i.project)
+                    OR
+                    (
+                        da.investigatorId = i.investigatorId
+                        OR
+                        da.investigatorId = i.debitedaccount.investigatorId
+                        -- OR da.investigatorId = i.project.investigatorId <!-- TODO: Currently, there is no easy way to add an investigatorId to ehr.project (editing on ehr.project routes to a custom page which has investigatorId as hidden) -->
+                    )
+                 )
+          ) IS NOT NULL
 
-  --include if the user is either the project's PI, the account PI, or the financial analyst
---   isMemberOf(i.project.investigatorId.userid) OR <!-- TODO: same problem as mentioned above -->
-  isMemberOf(i.debitedaccount.investigatorId)
---          OR isMemberOf(i.project.investigatorId.financialAnalyst) <!-- TODO: same problem as mentioned above -->
-)
+          OR
+
+         --include if the user is either the project's PI, the account PI, or the financial analyst
+         --   isMemberOf(i.project.investigatorId.userid) OR <!-- TODO: same problem as mentioned above -->
+         isMemberOf(i.debitedaccount.investigatorId)
+            -- OR isMemberOf(i.project.investigatorId.financialAnalyst) <!-- TODO: same problem as mentioned above, also not sure if WNPRC has a concept of financialAnalyst. this column is not included in ehr.investigator table. -->
+      )

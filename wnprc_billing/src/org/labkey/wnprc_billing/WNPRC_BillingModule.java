e25d861fa9c5cbbf638e5b2355f99aa39c90dfc6
Adam Rauch <adam@labkey.com>
2018-10-26 22:35:08 +0000

    Consolidate SQL scripts and bump module versions for release 18.3 - wnprc_billing

--- wnprc_billing/src/org/labkey/wnprc_billing/WNPRC_BillingModule.java
+++ wnprc_billing/src/org/labkey/wnprc_billing/WNPRC_BillingModule.java
@@ -57,7 +57,7 @@ public class WNPRC_BillingModule extends ExtendedSimpleModule
     @Override
     public double getVersion()
     {
-        return 18.23;
+        return 18.30;
     }
 
     @Override

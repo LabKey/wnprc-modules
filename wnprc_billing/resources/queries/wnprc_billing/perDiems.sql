a11ed9260c3e7cc2b4c643eca985d96c170fa4c4
Ed  Younskevicius <eyounske@labkey.com>
2018-11-13 00:57:49 +0000

    Copyright and license updates   -- Update for November 2018   -- Now scanning .tf and .tfvars files (Terraform)   -- Deliberately leaving out SNPRC module (moving to Git very soon)

--- wnprc_billing/resources/queries/wnprc_billing/perDiems.sql
+++ wnprc_billing/resources/queries/wnprc_billing/perDiems.sql
@@ -1,4 +1,19 @@
 /*
+ * Copyright (c) 2017-2018 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/*
  * Below query is based on WNPRC's current financial system - a query that resides in perdiems.pl:
  * select a.Id as id, project.account as account, h.area as location,
  *               case

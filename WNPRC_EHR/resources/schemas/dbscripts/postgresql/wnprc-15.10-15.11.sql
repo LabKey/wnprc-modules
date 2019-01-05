a3b1133737723b996944e81246b9a4af1f5e4304
Binal <binalp@labkey.com>
2019-01-04 18:36:33 -0800

    Added upgrade script to handle unique constraint error on bootstrapped databases (FK constraint to 'room' column is no longer valid since 'room' column is longer a PK/unique in ehr_lookups.rooms as of ehr_lookups-18.11-18.12).

--- WNPRC_EHR/resources/schemas/dbscripts/postgresql/wnprc-15.10-15.11.sql
+++ WNPRC_EHR/resources/schemas/dbscripts/postgresql/wnprc-15.10-15.11.sql
@@ -13,5 +13,8 @@ CREATE TABLE wnprc.necropsy_suite (
   modified   TIMESTAMP,
 
   CONSTRAINT pk_necropsy_suite PRIMARY KEY (room, container),
-  CONSTRAINT fk_necropsy_suite_rooms FOREIGN KEY (room) REFERENCES ehr_lookups.rooms (room)
+
+  --adding 'container' to below constraint to avoid teamcity failure/for users using bootstrapped db: org.postgresql.util.PSQLException: ERROR: there is no unique constraint matching given keys for referenced table "rooms"
+  --this error is caused by 'room' no longer being a PK/unique in ehr_lookups.rooms as of ehr_lookups-18.11-18.12. This constraint will get dropped and re-added in wnprc-18.30-18.31.sql for production or staging server/non-bootstrapped db to take effect.
+  CONSTRAINT fk_necropsy_suite_rooms FOREIGN KEY (container, room) REFERENCES ehr_lookups.rooms (container, room)
 );

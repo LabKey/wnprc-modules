38f1e80a3833615dda28836dae6ff99511595f62
Josh Eckels <jeckels@labkey.com>
2019-01-06 08:56:30 -0800

    Try to track down location of breeding.js on TeamCity

--- WNPRC_EHR/webpack.config.ts
+++ WNPRC_EHR/webpack.config.ts
@@ -12,6 +12,7 @@ interface Configuration extends Webpack.Configuration {
 
 declare const module: any;
 module.exports = function wp(env: { BUILD_DIR: string }): Configuration {
+    console.log('Output dir for WNPRC_EHR webpack: ' + `${env.BUILD_DIR}/explodedModule/web/wnprc_ehr`)
     return {
         devtool: 'source-map',
         entry: './src/ts/breeding.ts',

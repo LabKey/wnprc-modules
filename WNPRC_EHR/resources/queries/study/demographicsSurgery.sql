ec1b6e03fe6e5537540286d8389dacfa791afd6c
fdnicolalde <fdnicolalde@primate.wisc.edu>
2019-01-14 14:04:13 -0600

    Changes to major surgery query.

--- WNPRC_EHR/resources/queries/study/demographicsSurgery.sql
+++ WNPRC_EHR/resources/queries/study/demographicsSurgery.sql
@@ -8,17 +8,21 @@ SELECT
 s.id,
 
 CASE
-WHEN (group_concat(s.major) like 'Yes') THEN
+WHEN (group_concat(s.major) like '%Yes%') THEN
     'Yes'
       ELSE
     null
 END AS MajorSurgery,
-
+SUM (
+  CASE WHEN s.major = 'Yes'
+    THEN 1 ELSE 0
+  END
+  ) AS NumberOfMajorSurgeries,
 true As AnySurgery,
 
 count(*) as NumberOfSurgeries
 
-FROM study."Clinical Encounters" s
+FROM study.encounters s
 WHERE s.qcstate.publicdata = true AND type = 'Surgery'
 
 GROUP BY s.id

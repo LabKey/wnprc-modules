bc82dc8711d26ea92b66083880c6a3243dad36c8
Gabriel <gabriel.gottfredsen@gmail.com>
2019-03-01 14:12:14 -0600

    used to query table to automatically determine estimated gestation day based on ultrasound measurements

--- WNPRC_EHR/resources/queries/study/getGestationalDay.sql
+++ WNPRC_EHR/resources/queries/study/getGestationalDay.sql
@@ -1,14 +1,15 @@
-PARAMETERS ( SPECIES VARCHAR, SEARCH_COLUMN_NAME VARCHAR )
-SELECT
-  po.date,
-  po.performedby,
-  po.outcome,
-  po.infantid,
-  po.project,
-  po.remark
-FROM pregnancy_outcomes po
-WHERE po.pregnancyid = (SELECT p.lsid
-                        FROM pregnancies p
-                        WHERE p.objectid = PARENT_RECORD_ID
-                        LIMIT 1)
-ORDER BY po.date DESC
+PARAMETERS ( SPECIES VARCHAR, SEARCH_COLUMN_NAME VARCHAR , SEARCH_VALUE FLOAT)
+
+SELECT gestational_day
+FROM wnprc.gestational_days
+WHERE species = SPECIES
+ORDER BY
+  CASE
+  WHEN SEARCH_COLUMN_NAME = 'beats_per_minute' THEN abs(SEARCH_VALUE - beats_per_minute)
+  WHEN SEARCH_COLUMN_NAME = 'crown_rump_mm' THEN abs(SEARCH_VALUE - crown_rump_mm)
+  WHEN SEARCH_COLUMN_NAME = 'head_circumference_mm' THEN abs(SEARCH_VALUE - head_circumference_mm)
+  WHEN SEARCH_COLUMN_NAME = 'femur_length_mm' THEN abs(SEARCH_VALUE - femur_length_mm)
+  WHEN SEARCH_COLUMN_NAME = 'biparietal_diameter_mm' THEN abs(SEARCH_VALUE - biparietal_diameter_mm)
+  ELSE -1
+END ASC
+LIMIT 1

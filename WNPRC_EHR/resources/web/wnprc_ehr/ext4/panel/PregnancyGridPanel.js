64a955489a3ff1683a5060113faf5ea058a4f9c0
Gabriel <gabriel.gottfredsen@gmail.com>
2019-03-05 16:32:15 -0600

    automatically populate various conception and due date fields when a breeding encounter is selected

--- WNPRC_EHR/resources/web/wnprc_ehr/ext4/panel/PregnancyGridPanel.js
+++ WNPRC_EHR/resources/web/wnprc_ehr/ext4/panel/PregnancyGridPanel.js
@@ -40,20 +40,53 @@
         }
 
         switch (args.field) {
-            case 'date_conception_early': {
-                model.set("date_due_early", Ext4.Date.add(args.value, Ext4.Date.DAY, gestationPeriod));
+            // case 'date_conception_early': {
+            //     model.set("date_due_early", Ext4.Date.add(args.value, Ext4.Date.DAY, gestationPeriod));
+            //     break;
+            // }
+            // case 'date_due_early': {
+            //     model.set("date_conception_early", Ext4.Date.add(args.value, Ext4.Date.DAY, -gestationPeriod));
+            //     break;
+            // }
+            // case 'date_conception_late': {
+            //     model.set("date_due_late", Ext4.Date.add(args.value, Ext4.Date.DAY, gestationPeriod));
+            //     break;
+            // }
+            // case 'date_due_late': {
+            //     model.set("date_conception_late", Ext4.Date.add(args.value, Ext4.Date.DAY, -gestationPeriod));
+            //     break;
+            // }
+            case 'date_conception': {
+                model.set("date_due", Ext4.Date.add(args.value, Ext4.Date.DAY, gestationPeriod));
                 break;
             }
-            case 'date_due_early': {
-                model.set("date_conception_early", Ext4.Date.add(args.value, Ext4.Date.DAY, -gestationPeriod));
+            case 'date_due': {
+                model.set("date_conception", Ext4.Date.add(args.value, Ext4.Date.DAY, -gestationPeriod));
                 break;
             }
-            case 'date_conception_late': {
-                model.set("date_due_late", Ext4.Date.add(args.value, Ext4.Date.DAY, gestationPeriod));
-                break;
-            }
-            case 'date_due_late': {
-                model.set("date_conception_late", Ext4.Date.add(args.value, Ext4.Date.DAY, -gestationPeriod));
+            case 'breedingencounterid': {
+                LABKEY.Query.selectRows({
+                    schemaName: 'study',
+                    queryName: 'breeding_encounters',
+                    columns: 'date,enddate',
+                    filterArray: [
+                        LABKEY.Filter.create('lsid', args.value, LABKEY.Filter.Types.EQUALS)
+                    ],
+                    scope: this,
+                    success: function(data) {
+                        if (data.rows && data.rows.length) {
+                            var row = data.rows[0];
+                            var early = LDK.ConvertUtils.parseDate(row.date, 'Y/m/d H:i:s');
+                            var late = Ext4.Date.add(LDK.ConvertUtils.parseDate(row.enddate, 'Y/m/d H:i:s'), Ext4.Date.DAY, 3);
+                            model.set("date_conception_early", early);
+                            model.set("date_conception_late", late);
+                            model.set("date_due_early", Ext4.Date.add(early, Ext4.Date.DAY, gestationPeriod));
+                            model.set("date_due_late", Ext4.Date.add(late, Ext4.Date.DAY, gestationPeriod));
+
+                        }
+                    },
+                    failure: EHR.Utils.onFailure
+                });
                 break;
             }
         }

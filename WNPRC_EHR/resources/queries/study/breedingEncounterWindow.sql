c500f1c22dd939c96525b56eac60c7cc52a34c5e
Gabriel <gabriel.gottfredsen@gmail.com>
2019-03-11 16:09:36 -0500

    show breeding window as a drop down on the pregnancy form page

--- WNPRC_EHR/resources/queries/study/breedingEncounterWindow.sql
+++ WNPRC_EHR/resources/queries/study/breedingEncounterWindow.sql
@@ -1,2 +1,2 @@
-SELECT date || ' to ' || enddate AS window
+SELECT lsid, to_char(date, 'Mon DD, YYYY HH24:MI') || ' to' || chr(10) || to_char(enddate, 'Mon DD, YYYY HH24:MI') AS window
 FROM study.breeding_encounters

28dbf23750baf528e41ddea5946ee3cfd9556790
Binal <binalp@labkey.com>
2019-03-19 10:58:43 -0700

    Updated expected row count.

--- WNPRC_EHR/test/src/org/labkey/test/tests/wnprc_ehr/WNPRC_EHRTest.java
+++ WNPRC_EHR/test/src/org/labkey/test/tests/wnprc_ehr/WNPRC_EHRTest.java
@@ -968,7 +968,7 @@ public class WNPRC_EHRTest extends AbstractGenericEHRTest implements PostgresOnl
 
     private void testInvoicedItems()
     {
-        testReports("All Invoiced Items", 6, "test2312318","640991" , "$19.50", "$15.00",
+        testReports("All Invoiced Items", 7, "test2312318","640991" , "$19.50", "$15.00",
                 "Blood draws - Additional Tubes", "Per diems", "Misc. Fees", "vaccine supplies", "$195.00");
     }
 

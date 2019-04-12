072d4cab9487d6cb1e9b5d535a87136a60f1d616
Gabriel <gabriel.gottfredsen@gmail.com>
2019-04-12 14:55:28 -0500

    added method to kick off pregnancy notifications and a method to escalate security to update already completed breeding encounter records

--- WNPRC_EHR/src/org/labkey/wnprc_ehr/TriggerScriptHelper.java
+++ WNPRC_EHR/src/org/labkey/wnprc_ehr/TriggerScriptHelper.java
@@ -2,21 +2,28 @@ package org.labkey.wnprc_ehr;
 
 import org.apache.log4j.Logger;
 import org.json.JSONArray;
+import org.json.JSONObject;
 import org.labkey.api.data.CompareType;
 import org.labkey.api.data.Container;
 import org.labkey.api.data.ContainerManager;
-import org.labkey.api.ldk.notification.NotificationSection;
+import org.labkey.api.data.TableInfo;
+import org.labkey.api.ehr.security.EHRSecurityEscalator;
 import org.labkey.api.ldk.notification.NotificationService;
 import org.labkey.api.module.Module;
 import org.labkey.api.module.ModuleLoader;
 import org.labkey.api.security.User;
 import org.labkey.api.security.UserManager;
+import org.labkey.api.study.security.SecurityEscalator;
 import org.labkey.dbutils.api.SimpleQueryFactory;
+import org.labkey.dbutils.api.SimpleQueryUpdater;
 import org.labkey.dbutils.api.SimplerFilter;
 import org.labkey.wnprc_ehr.notification.DeathNotification;
+import org.labkey.wnprc_ehr.notification.PregnancyNotification;
 import org.labkey.wnprc_ehr.notification.VvcNotification;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by jon on 7/13/16.
@@ -55,6 +62,41 @@ public class TriggerScriptHelper {
             DeathNotification idNotification = new DeathNotification();
             idNotification.setParam(DeathNotification.idParamName, id);
             idNotification.sendManually(container, user);
+        }
+    }
+
+    public void sendPregnancyNotification(final List<String> ids, final List<String> objectids) {
+        if (!NotificationService.get().isServiceEnabled() && NotificationService.get().isActive(new PregnancyNotification(), container)){
+            _log.info("Notification service is not enabled, will not send pregnancy notification");
+            return;
+        }
+        if (ids.size() != objectids.size()) {
+            _log.warn("Mismatch between list of animal ids and object ids.  Will not send pregnancy notification");
+            return;
+        }
+        for (int i = 0; i < ids.size(); i++) {
+            PregnancyNotification pregnancyNotification = new PregnancyNotification();
+            pregnancyNotification.setParam(PregnancyNotification.idParamName, ids.get(i));
+            pregnancyNotification.setParam(PregnancyNotification.objectidsParamName, objectids.get(i));
+            pregnancyNotification.sendManually(container, user);
+        }
+    }
+
+    public void updateBreedingOutcome(final List<String> lsids) {
+        try (SecurityEscalator escalator = EHRSecurityEscalator.beginEscalation(user, container, "Escalating so that breeding encounter outcome can be changed to true")) {
+            SimpleQueryUpdater queryUpdater = new SimpleQueryUpdater(user, container, "study", "breeding_encounters");
+
+            List<Map<String, Object>> updateRows = new ArrayList<>();
+            for (String lsid : lsids) {
+                JSONObject row = new JSONObject();
+                row.put("lsid", lsid);
+                row.put("outcome", true);
+                updateRows.add(row);
+            }
+
+            queryUpdater.update(updateRows);
+        } catch (Exception e) {
+            _log.error(e);
         }
     }
 

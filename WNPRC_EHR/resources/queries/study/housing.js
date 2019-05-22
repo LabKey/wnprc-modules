f7302ce4ca77f68d4c4ba3e3c24efc7b6a46387a
Gabriel <gabriel.gottfredsen@gmail.com>
2019-05-22 10:35:23 -0500

    refactored to save breeding encounters in onUpsert instead of onComplete so that error messages can be shown

--- WNPRC_EHR/resources/queries/study/housing.js
+++ WNPRC_EHR/resources/queries/study/housing.js
@@ -6,20 +6,48 @@ function onInit(event, helper) {
     helper.setScriptOptions({
         skipHousingCheck: true
     });
+
+    helper.decodeExtraContextProperty('housingInTransaction');
 }
 
-function onComplete(event, errors, helper) {
-    let housingRows = helper.getRows();
+function onUpsert(helper, scriptErrors, row, oldRow){
+    let breedingDataSaved = helper.getProperty('breedingEncountersSaved');
 
-    let myarray = [];
+    if (helper.isValidateOnly() || (!helper.isValidateOnly() && !breedingDataSaved)) {
+        let housingRows = helper.getProperty('housingInTransaction');
+        let housingArray = [];
 
-    for (let i = 0; i < housingRows.length; i++) {
-        if (!housingRows[i].row.enddate) {
-            myarray.push(housingRows[i].row);
+        //loop through all housing change rows and add them to the housing array
+        //if they have all of the correct fields filled in.
+        for (let propertyName in housingRows) {
+            if (housingRows.hasOwnProperty(propertyName)) {
+                for (let i = 0; i < housingRows[propertyName].length; i++) {
+                    if (!housingRows[propertyName][i].enddate && housingRows[propertyName][i].reason && housingRows[propertyName][i].room
+                            && housingRows[propertyName][i].cage && housingRows[propertyName][i].date && housingRows[propertyName][i].Id) {
+                        housingRows[propertyName][i].date = new Date(housingRows[propertyName][i].date);
+                        housingRows[propertyName][i].currentId = row.Id;
+                        housingRows[propertyName][i].validateOnly = helper.isValidateOnly();
+                        housingArray.push(housingRows[propertyName][i]);
+                    }
+                }
+            }
         }
-    }
 
-    if (myarray.length > 0) {
-        WNPRC.Utils.getJavaHelper().createBreedingRecordsFromHousingChanges(myarray);
+        //call java method to create the breeding encounter records from the housing changes
+        //add any errors that are returned to the page
+        if (housingArray.length > 0) {
+            let javaErrors = WNPRC.Utils.getJavaHelper().createBreedingRecordsFromHousingChanges(housingArray);
+            if (javaErrors) {
+                for (let i = 0; i < javaErrors.length; i++) {
+                    let error = javaErrors[i];
+                    console.log('Field: ' + error.field + ', Message: ' + error.message + ', Severity: ' + error.severity);
+                    EHR.Server.Utils.addError(scriptErrors, error.field, error.message, error.severity);
+                }
+            }
+        }
+
+        //set data saved to true so that it doesn't save for every row
+        //this is necessary because we're saving everything all at once
+        helper.setProperty('breedingEncountersSaved', true);
     }
 }

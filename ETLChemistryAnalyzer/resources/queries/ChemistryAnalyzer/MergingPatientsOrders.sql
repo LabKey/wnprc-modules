6a897368f2c352639c3f421112a51936d42f2cc2
fdnicolalde <fdnicolalde@primate.wisc.edu>
2019-06-26 12:50:09 -0500

    ChemistryAnalyzer importing from Vet Axcel and adding to clinpath request.

--- ETLChemistryAnalyzer/resources/queries/ChemistryAnalyzer/MergingPatientsOrders.sql
+++ ETLChemistryAnalyzer/resources/queries/ChemistryAnalyzer/MergingPatientsOrders.sql
@@ -1,22 +1,52 @@
-SELECT HDR.Header_ID,HDR.Date_and_Time AS date,
-PT.Patient_ID, Laboratory_Assigned_Patient_ID AS Id, Patient_Name_Name_First_name, Birthdate, Patient_Sex, Special_Field_1,
-ORD.Orden_ID, ORD.Requested_Ordered_Date_and_Time,ORD.Sample_ID,
-CMO.OrderComment as OrderComment,
-RST.Universal_Test_ID AS testid, RST.oor AS resultOORIndicator ,RST.result_value AS result, RST.Unit AS units
+SELECT
+GROUPRESULTS.Sample_ID, GROUPRESULTS.RequestDateTime,
+GROUPRESULTS.testid, GROUPRESULTS.resultOORIndicator, GROUPRESULTS.result, GROUPRESULTS.units,
+GROUPRESULTS.OrderComment, GROUPRESULTS.order_date, GROUPRESULTS.comment_text,
+PT.Patient_ID, PT.Laboratory_Assigned_Patient_ID AS Id, PT.Patient_Name_Name_First_name, Birthdate, Patient_Sex, Special_Field_1,
+HDR.Header_ID,HDR.Date_and_Time AS date
+FROM
+(
+    SELECT
 
-FROM CHEMISTRYANALYZER.patient_view PT
+    MAX(ORD.Patient_ID) AS Patient_ID, MAX(ORD.Orden_ID) AS Orden_ID, ORD.Requested_Ordered_Date_and_Time AS RequestDateTime,ORD.Sample_ID,
+    MAX(CMO.text) as OrderComment, CMO.order_date, CMO.comment_text,
+    RST.Universal_Test_ID AS testid, MAX(RST.oor) AS resultOORIndicator ,
+    -- MAX(RST.result_value) AS result,
 
-JOIN CHEMISTRYANALYZER.order_view ORD
-ON PT.Patient_ID = ORD.Patient_ID
+    CASE
+    WHEN MAX(RST.result_value) = MIN(RST.result_value) THEN RST.result_value
+
+    ELSE -1
+
+    END AS result,
+
+    MAX(RST.Unit) AS units
+
+    FROM CHEMISTRYANALYZER.order_view ORD
 
 
-JOIN  CHEMISTRYANALYZER.result_view RST
-ON ORD.Orden_ID = RST.Orden_ID
 
-JOIN  CHEMISTRYANALYZER.header_view HDR
-ON HDR.Header_ID = PT.Header_ID
+    JOIN  CHEMISTRYANALYZER.result_view RST
+    ON ORD.Orden_ID = RST.Orden_ID
+    
+    --Duplicating comments from first order in the set to all the orders with the same Orden_ID
+    LEFT JOIN (SELECT CMO2.text, CMO2.order_date, CMO2.comment_text, CMO2.Orden_ID, ORD2.Patient_ID AS PTID2
+            FROM CHEMISTRYANALYZER.comment_order_view CMO2
+            JOIN CHEMISTRYANALYZER.order_view ORD2
+            ON CMO2.Orden_ID = ORD2.Orden_ID
+            ) CMO
+    ON CMO.PTID2 = ORD.Patient_ID
 
-LEFT JOIN CHEMISTRYANALYZER.MergingComentOrders CMO
-ON  ORD.Orden_ID = CMO.Orden_ID
+    GROUP BY RST.Universal_Test_ID, ORD.Sample_ID, ORD.Requested_Ordered_Date_and_Time
+
+) GROUPRESULTS
+
+JOIN CHEMISTRYANALYZER.patient_view PT
+ON PT.Patient_ID = GROUPRESULTS.Patient_ID
+
+JOIN CHEMISTRYANALYZER.header_view HDR
+    ON HDR.Header_ID = PT.Header_ID
 
 WHERE startswith(Patient_Name_Name_First_name,'MONKEY')
+
+--

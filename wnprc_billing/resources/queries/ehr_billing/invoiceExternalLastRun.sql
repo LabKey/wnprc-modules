71e16b6037cf85ba6319abf1ce81abf0b37bb48d
Binal <binalp@labkey.com>
2019-08-07 14:11:47 -0700

    Code Review change - remove group by in the queries (its not needed).

--- wnprc_billing/resources/queries/ehr_billing/invoiceExternalLastRun.sql
+++ wnprc_billing/resources/queries/ehr_billing/invoiceExternalLastRun.sql
@@ -1,4 +1,4 @@
-SELECT
+SELECT DISTINCT
   invExternal.rowId,
   invExternal.invoiceNumber,
   invExternal.invoiceRunId,
@@ -12,17 +12,3 @@ SELECT
   invExternal.address,
   invExternal.invoiceAmount
 FROM ehr_billing.invoiceExternal invExternal WHERE invExternal.invoiceRunId =(SELECT max(invoiceRunId) from ehr_billing.invoiceExternal)
-
-GROUP BY
-         invExternal.rowId,
-         invExternal.invoiceNumber,
-         invExternal.invoiceRunId,
-         invExternal.billingPeriodStart,
-         invExternal.billingPeriodEnd,
-         invExternal.BillingRunDate,
-         invExternal.accountNumber,
-         invExternal.type,
-         invExternal.po_number,
-         invExternal.contact_name,
-         invExternal.address,
-         invExternal.invoiceAmount

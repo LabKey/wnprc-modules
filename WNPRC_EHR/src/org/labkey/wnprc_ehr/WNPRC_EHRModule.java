67cf4b8fa2671e41a4c7492679b350b00e9dff77
Chad Sebranek <csebranek@gmail.com>
2019-08-13 12:07:38 -0500

    Revert "Add new manner of death lookup"
    This reverts commit f19666aee36dcbe1a39f1236648be642001cfd08.

--- WNPRC_EHR/src/org/labkey/wnprc_ehr/WNPRC_EHRModule.java
+++ WNPRC_EHR/src/org/labkey/wnprc_ehr/WNPRC_EHRModule.java
@@ -190,7 +190,7 @@ public class WNPRC_EHRModule extends ExtendedSimpleModule
 
     public double getVersion()
     {
-        return 15.19;
+        return 15.18;
     }
 
     public boolean hasScripts()

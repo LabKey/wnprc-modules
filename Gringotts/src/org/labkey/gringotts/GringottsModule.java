317279aba5d291621d176271579c618f4d25e755
Chad Sebranek <csebranek@gmail.com>
2019-08-29 13:45:43 -0500

    Add gringotts back in

---
+++ Gringotts/src/org/labkey/gringotts/GringottsModule.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright (c) 2015 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.labkey.gringotts;
+
+import org.jetbrains.annotations.NotNull;
+import org.labkey.api.data.Container;
+import org.labkey.api.data.ContainerManager;
+import org.labkey.api.ldk.ExtendedSimpleModule;
+import org.labkey.api.module.DefaultModule;
+import org.labkey.api.module.ModuleContext;
+import org.labkey.api.view.WebPartFactory;
+import org.labkey.gringotts.api.GringottsService;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Set;
+
+public class GringottsModule extends ExtendedSimpleModule {
+    public static final String NAME = "Gringotts";
+
+    @Override
+    public String getName() {
+        return NAME;
+    }
+
+    @Override
+    public double getVersion() {
+        return 15.11;
+    }
+
+    @Override
+    public boolean hasScripts() {
+        return true;
+    }
+
+    @Override
+    @NotNull
+    protected Collection<WebPartFactory> createWebPartFactories() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    protected void init() {
+        addController(GringottsController.NAME, GringottsController.class);
+    }
+
+    @Override
+    public void doStartupAfterSpringConfig(ModuleContext moduleContext) {
+        // add a container listener so we'll know when our container is deleted:
+        ContainerManager.addContainerListener(new GringottsContainerListener());
+
+        GringottsService.set(new GringottsServiceImpl());
+    }
+
+    @Override
+    @NotNull
+    public Collection<String> getSummary(Container c) {
+        return Collections.emptyList();
+    }
+
+    @Override
+    @NotNull
+    public Set<String> getSchemaNames() {
+        return Collections.singleton(GringottsSchema.NAME);
+    }
+}

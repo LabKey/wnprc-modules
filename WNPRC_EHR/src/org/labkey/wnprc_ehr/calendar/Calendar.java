58b8f3c9d18c06bea51e67fc3b6ddbcc6c3bc8b1
Gabriel <gabriel.gottfredsen@gmail.com>
2019-09-12 11:45:30 -0500

    added a few required methods to the Calendar interface

--- WNPRC_EHR/src/org/labkey/wnprc_ehr/calendar/Calendar.java
+++ WNPRC_EHR/src/org/labkey/wnprc_ehr/calendar/Calendar.java
@@ -1,6 +1,11 @@
 package org.labkey.wnprc_ehr.calendar;
 
+import org.labkey.api.data.Container;
+import org.labkey.api.security.User;
+
 public interface Calendar
 {
-
+    void setUser(User u);
+    void setContainer(Container c);
+    String getCalendarEventsAsJson(String calendarId);
 }

bf918b70d778f8d523b54c9cab581194675bc89c
Gabriel <gabriel.gottfredsen@gmail.com>
2019-09-25 12:54:00 -0500

    created query to find all animals that were on a research project during a given date range

---
+++ WNPRC_EHR/resources/queries/study/totalResearchAssignmentsDuringRange.sql
@@ -0,0 +1,13 @@
+PARAMETERS
+    (START_DATE TIMESTAMP, END_DATE TIMESTAMP)
+
+SELECT id
+     , GROUP_CONCAT(project, ', ')       as Projects
+     , GROUP_CONCAT(date, ', ')          as AssignmentDates
+     , GROUP_CONCAT(enddate, ', ')       as ReleaseDates
+     , GROUP_CONCAT(project.avail, ', ') as ProjectAvails
+FROM study.assignment
+WHERE date <= END_DATE
+  AND (enddate >= START_DATE OR enddate IS NULL)
+  AND (project.avail = 'r' OR project.avail = 'rr')
+GROUP BY id;

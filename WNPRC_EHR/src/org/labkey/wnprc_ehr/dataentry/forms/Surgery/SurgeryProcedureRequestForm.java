381c4ae3b792a70c3b974a3d452b01ed4f8989e1
Gabriel <gabriel.gottfredsen@gmail.com>
2019-12-05 12:51:33 -0600

    added drug section and food deprive section

--- WNPRC_EHR/src/org/labkey/wnprc_ehr/dataentry/forms/Surgery/SurgeryProcedureRequestForm.java
+++ WNPRC_EHR/src/org/labkey/wnprc_ehr/dataentry/forms/Surgery/SurgeryProcedureRequestForm.java
@@ -3,16 +3,21 @@ package org.labkey.wnprc_ehr.dataentry.forms.Surgery;
 import org.labkey.api.ehr.dataentry.DataEntryFormContext;
 import org.labkey.api.ehr.dataentry.FormSection;
 import org.labkey.api.ehr.dataentry.RequestFormSection;
+import org.labkey.api.ehr.dataentry.SimpleFormSection;
 import org.labkey.api.module.Module;
 import org.labkey.api.view.template.ClientDependency;
 import org.labkey.wnprc_ehr.WNPRCConstants;
+import org.labkey.wnprc_ehr.dataentry.forms.FoodDeprivesRequest.FormSections.FoodDeprivesRequestFormSections;
 import org.labkey.wnprc_ehr.dataentry.forms.Surgery.FormSections.SurgeryProcedureRequestSection;
 import org.labkey.wnprc_ehr.dataentry.generics.forms.SimpleRequestForm;
 import org.labkey.wnprc_ehr.dataentry.generics.sections.AnimalDetailsPanel;
+import org.labkey.wnprc_ehr.dataentry.generics.sections.SlaveGridSection;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class SurgeryProcedureRequestForm extends SimpleRequestForm
 {
@@ -23,6 +28,25 @@ public class SurgeryProcedureRequestForm extends SimpleRequestForm
         super(ctx, owner, NAME, "Request Surgery/Procedure", WNPRCConstants.DataEntrySections.PATHOLOGY_CLINPATH, Arrays.<FormSection>asList(
                 new RequestFormSection(),
                 new SurgeryProcedureRequestSection(),
+                //new SimpleFormSection("study", "Drug Administration", "Treatments", "ehr-gridpanel"),
+                new SlaveGridSection("study", "Drug Administration", "Drugs")
+                {
+                    @Override
+                    public Set<String> getSlaveFields() {
+                        Set<String> fields = new HashSet<>();
+
+                        fields.add("Id");
+                        fields.add("date");
+
+                        return fields;
+                    }
+
+                    @Override
+                    protected List<String> getFieldNames() {
+                        return Arrays.asList("Id", "date", "tissue", "qualifier", "weight", "remark");
+                    }
+                },
+                new FoodDeprivesRequestFormSections(),
                 //new SimpleFormPanelSection("wnprc", "surgeries", "Surgery Request"),
                 new AnimalDetailsPanel()
         ));

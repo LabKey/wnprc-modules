2c8793cfba13be6a4bfe8a8992e577241ac53723
Chad Sebranek <csebranek@gmail.com>
2020-03-31 09:05:07 -0500

    put in random hash for container

--- EnterWeights/resources/schemas/dbscripts/postgresql/enterweights-18.31-18.32.sql
+++ EnterWeights/resources/schemas/dbscripts/postgresql/enterweights-18.31-18.32.sql
@@ -1,1 +1,1 @@
-insert into ehr.form_framework_types (schemaname,queryname,framework,container) select 'study', 'weight', 'reactjs', container from ehr.tasks where rowid=1;
+insert into ehr.form_framework_types (schemaname,queryname,framework,container) select 'study', 'weight', 'reactjs', '29e3860b-02b5-102d-b524-493dbd27b599';

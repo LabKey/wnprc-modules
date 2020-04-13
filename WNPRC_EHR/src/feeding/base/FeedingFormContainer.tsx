d5898811efd883bf04df55f327fe398ed3a35e2e
Chad Sebranek <csebranek@gmail.com>
2020-04-13 11:18:37 -0500

    configure query xml

--- WNPRC_EHR/src/feeding/base/FeedingFormContainer.tsx
+++ WNPRC_EHR/src/feeding/base/FeedingFormContainer.tsx
@@ -10,6 +10,12 @@ const FeedingFormContainer: React.FunctionComponent<any> = props => {
         setVal(val);
     };
 
+    //the form could receive an array of queryColumns that then decide what to do with the fields...
+    //but each field needs its own action what to do on blur / focus... validation etc
+    //for starters i could reduce the array into like "animalid" > [type,hidden, etc]
+    //and onBlur for each function, pass it to a parent functiont hat checks what it hsould do
+    // const parentFunction() => {if e.currentTarget.name == 'animalid' then do this...}
+
     return (
         <FeedingForm liftUpValue={liftUp} value={val} />
     );

36c05a5f4765e2c1b2aa30a5c56d10c15cfbcc18
Gabriel Gottfredsen <gabriel.gottfredsen@gmail.com>
2020-05-18 09:51:16 -0500

    organize research ultrasounds field into 2 columns

--- WNPRC_EHR/src/org/labkey/wnprc_ehr/dataentry/forms/ResearchUltrasounds/FormSections/ResearchUltrasoundsFormSection.java
+++ WNPRC_EHR/src/org/labkey/wnprc_ehr/dataentry/forms/ResearchUltrasounds/FormSections/ResearchUltrasoundsFormSection.java
@@ -1,9 +1,16 @@
 package org.labkey.wnprc_ehr.dataentry.forms.ResearchUltrasounds.FormSections;
 
+import org.json.JSONObject;
+import org.labkey.api.ehr.dataentry.DataEntryFormContext;
 import org.labkey.api.ehr.dataentry.SimpleFormSection;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class ResearchUltrasoundsFormSection extends SimpleFormSection
 {
+    Integer maxItemsPerColumn = 20;
+
     public ResearchUltrasoundsFormSection() {
         super("study", "research_ultrasounds", "Research Ultrasounds", "ehr-formpanel");
 
@@ -13,6 +20,24 @@ public class ResearchUltrasoundsFormSection extends SimpleFormSection
 //                "dateRequested",
 //                "Project"
 //        );
+    }
 
+    @Override
+    public JSONObject toJSON(DataEntryFormContext ctx, boolean includeFormElements) {
+        JSONObject ret = super.toJSON(ctx, includeFormElements);
+
+        Map<String, Object> formConfig = new HashMap<>();
+        Map<String, Object> bindConfig = new HashMap<>();
+        bindConfig.put("createRecordOnLoad", true);
+        formConfig.put("bindConfig", bindConfig);
+
+        if ( maxItemsPerColumn != null ) {
+            // Make the form appear in two columns
+            formConfig.put("maxItemsPerCol", maxItemsPerColumn);
+        }
+
+        ret.put("formConfig", formConfig);
+
+        return ret;
     }
 }

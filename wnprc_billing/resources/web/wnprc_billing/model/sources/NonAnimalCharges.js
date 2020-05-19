c3102a0f2c4191e58201ec73e124b8e316aec809
Gabriel Gottfredsen <gabriel.gottfredsen@gmail.com>
2020-05-19 15:01:37 -0500

    Revert "change ehr_billing to ehr_billing_public so that certain users can enter charges"
    This reverts commit f220d974fa9341f10991ba9c807cb8f5d52ce2c3.

--- wnprc_billing/resources/web/wnprc_billing/model/sources/NonAnimalCharges.js
+++ wnprc_billing/resources/web/wnprc_billing/model/sources/NonAnimalCharges.js
@@ -18,7 +18,7 @@ EHR.model.DataModelManager.registerMetadata('NonAnimalCharges', {
                 hidden: false,
                 xtype: 'wnprc_billing-nonanimalchargesdebitacctfield',
                 lookup: {
-                    schemaName: 'ehr_billing_public',
+                    schemaName: 'ehr_billing',
                     queryName: 'aliases',
                     filterArray: [LABKEY.Filter.create('isAcceptingCharges', 'N', LABKEY.Filter.Types.NEQ_OR_NULL)]
                 },

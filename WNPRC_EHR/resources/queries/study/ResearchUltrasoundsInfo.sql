24301cbcf3d44f764a80cd8facf6564236ccaf6e
Gabriel Gottfredsen <gabriel.gottfredsen@gmail.com>
2020-08-05 17:55:10 -0500

    added coalesce so that there is always a value returned

--- WNPRC_EHR/resources/queries/study/ResearchUltrasoundsInfo.sql
+++ WNPRC_EHR/resources/queries/study/ResearchUltrasoundsInfo.sql
@@ -7,7 +7,7 @@ SELECT ru.objectid
       ,ru.fetal_heartbeat
       ,ru.performedby
       ,ru.remark
-      ,(select ur.completed from ultrasound_review ur where ur.taskid = ru.taskid) as completed
+      ,COALESCE((select ur.completed from ultrasound_review ur where ur.taskid = ru.taskid), 'false') as completed
       ,ru.taskid
   FROM research_ultrasounds ru
 ORDER BY ru.date DESC

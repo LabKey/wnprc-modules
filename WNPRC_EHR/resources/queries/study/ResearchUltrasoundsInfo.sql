ef81a0ed69ff1a83eafb67f7f56f2886ef853b1e
Gabriel Gottfredsen <gabriel.gottfredsen@gmail.com>
2020-08-12 16:59:56 -0500

    added support for displaying gestation day at the time of the ultrasound when viewing the research ultrasound records

--- WNPRC_EHR/resources/queries/study/ResearchUltrasoundsInfo.sql
+++ WNPRC_EHR/resources/queries/study/ResearchUltrasoundsInfo.sql
@@ -2,12 +2,24 @@ SELECT ru.objectid
       ,ru.id
       ,ru.date
       ,ru.pregnancyid
+      ,CASE WHEN EXISTS(SELECT 1 FROM pregnancy_outcomes po WHERE ru.pregnancyid = po.pregnancyid)
+              THEN 'Pregnancy Completed'
+            WHEN EXISTS(SELECT 1 FROM pregnancies p WHERE ru.pregnancyid = p.lsid)
+                  THEN CONCAT(CAST((SELECT timestampdiff('SQL_TSI_DAY', p.date_conception, ru.date)
+                         FROM pregnancies p
+                         WHERE ru.pregnancyid = p.lsid) AS VARCHAR), ' day(s)')
+            ELSE 'No Associated Pregnancy'
+       END AS gestation_day
       ,ru.project
       ,ru.restraint
       ,ru.fetal_heartbeat
       ,ru.performedby
       ,ru.remark
-      ,COALESCE((select ur.completed from ultrasound_review ur where ur.taskid = ru.taskid), 'false') as completed
+      ,COALESCE((
+                SELECT ur.completed
+                FROM ultrasound_review ur
+                WHERE ur.taskid = ru.taskid), 'false')
+                AS completed
       ,ru.taskid
   FROM research_ultrasounds ru
 ORDER BY ru.date DESC
--- WNPRC_EHR/src/org/labkey/wnprc_ehr/dataentry/forms/ResearchUltrasounds/ResearchUltrasoundsTaskForm.java
+++ WNPRC_EHR/src/org/labkey/wnprc_ehr/dataentry/forms/ResearchUltrasounds/ResearchUltrasoundsTaskForm.java
@@ -5,8 +5,8 @@ import org.labkey.api.ehr.dataentry.DataEntryFormContext;
 import org.labkey.api.ehr.dataentry.TaskFormSection;
 import org.labkey.api.module.Module;
 import org.labkey.wnprc_ehr.WNPRCConstants;
-import org.labkey.wnprc_ehr.dataentry.forms.ResearchUltrasounds.FormSections.ResearchUltrasoundsFormSection;
 import org.labkey.wnprc_ehr.dataentry.forms.ResearchUltrasounds.FormSections.ResearchUltrasoundsInstructionsFormSection;
+import org.labkey.wnprc_ehr.dataentry.forms.ResearchUltrasounds.FormSections.ResearchUltrasoundsTaskFormSection;
 import org.labkey.wnprc_ehr.dataentry.forms.ResearchUltrasounds.FormSections.UltrasoundMeasurementsFormSection;
 import org.labkey.wnprc_ehr.dataentry.forms.ResearchUltrasounds.FormSections.UltrasoundReviewFormSection;
 import org.labkey.wnprc_ehr.dataentry.generics.forms.SimpleTaskForm;
@@ -22,7 +22,7 @@ public class ResearchUltrasoundsTaskForm extends SimpleTaskForm
             new TaskFormSection(),
             new AnimalDetailsFormSection(),
             new ResearchUltrasoundsInstructionsFormSection(),
-            new ResearchUltrasoundsFormSection(),
+            new ResearchUltrasoundsTaskFormSection(),
             new UltrasoundMeasurementsFormSection(),
             new UltrasoundReviewFormSection()
         ));

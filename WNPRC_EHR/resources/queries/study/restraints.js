1c2f5c19787988d6a71dbeecb961c8310e47358f
Gabriel Gottfredsen <gabriel.gottfredsen@gmail.com>
2020-08-20 16:03:43 -0500

    no longer required since submit for review button is correctly propagating the qcstate now

--- WNPRC_EHR/resources/queries/study/restraints.js
+++ WNPRC_EHR/resources/queries/study/restraints.js
@@ -8,22 +8,4 @@ function onUpdate(helper, scriptErrors, row, oldRow) {
 }
 
 function onUpsert(helper, scriptErrors, row, oldRow) {
-    if (row.Id) {
-        LABKEY.Query.selectRows({
-            schemaName: 'study',
-            queryName: 'research_ultrasounds',
-            columns: 'QCState',
-            filterArray: [LABKEY.Filter.create('Id', row.Id, LABKEY.Filter.Types.EQUAL), LABKEY.Filter.create('taskid', row.taskid, LABKEY.Filter.Types.EQUAL)],
-            scope: this,
-            success: function (results) {
-                if (results.rows && results.rows.length) {
-                    let resultsRow = results.rows[0];
-                    row.QCStateLabel = EHR.Server.Security.getQCStateByRowId(resultsRow.QCState).Label;
-                }
-            },
-            failure: function (data) {
-                EHR.Server.Utils.onFailure
-            }
-        });
-    }
 }

5ebd9bc5c7ba99e393c10b6392bf17cfd49d6c23
Chad Sebranek <csebranek@gmail.com>
2020-09-03 18:32:59 -0500

    Add new fields and validation checks

--- WNPRC_EHR/resources/scripts/wnprc_ehr/lib/Utils.js
+++ WNPRC_EHR/resources/scripts/wnprc_ehr/lib/Utils.js
@@ -10,3 +10,46 @@ Utils.getJavaHelper = function() {
 Utils.lookupGender = function(gCode) {
     return Utils.getJavaHelper().lookupGender(gCode);
 };
+
+Utils.splitIds = function(subjectArray){
+    if (!subjectArray){
+        return [];
+    }
+
+    //subjectArray = Ext4.String.trim(subjectArray);
+    subjectArray = subjectArray.replace(/[\s,;]+/g, ';');
+    subjectArray = subjectArray.replace(/(^;|;$)/g, '');
+    subjectArray = subjectArray.toLowerCase();
+
+    if (subjectArray){
+        subjectArray = subjectArray.split(';');
+    }
+    else {
+        subjectArray = [];
+    }
+
+    return subjectArray;
+};
+
+Utils.contains = function(v,arr) {
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] === v) {
+            return true;
+        }
+    }
+    return false;
+};
+
+Utils.unique = function (chk) {
+    var arr = [];
+    var contain = false;
+    for (var i = 0; i < chk.length; i++) {
+        if (!Utils.contains(chk[i],arr)) {
+            arr.push(chk[i]);
+        }
+        else {
+            contain = true;
+        }
+    }
+    return !contain;
+}

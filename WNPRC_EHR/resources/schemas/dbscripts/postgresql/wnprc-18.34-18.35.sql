1e28da9c6df65e9bb651aac6d54a06634ac33707
Chad Sebranek <csebranek@gmail.com>
2020-09-23 11:53:03 -0500

    Explicitly include css files, remove macaque from species lookup, add viral status

--- WNPRC_EHR/resources/schemas/dbscripts/postgresql/wnprc-18.34-18.35.sql
+++ WNPRC_EHR/resources/schemas/dbscripts/postgresql/wnprc-18.34-18.35.sql
@@ -7,3 +7,5 @@ alter table wnprc.animal_requests add column animalidstooffer TEXT;
 insert into ehr_lookups.lookup_sets (setname,container)  select 'animal_requests_yes_no' as setname, container from ehr_lookups.lookup_sets where setname='animal_requests_sex';
 insert into ehr_lookups.lookups (set_name,container,value) select setname, container, 'Yes' as value from ehr_lookups.lookup_sets where setname='animal_requests_yes_no';
 insert into ehr_lookups.lookups (set_name,container,value) select setname, container, 'No' as value from ehr_lookups.lookup_sets where setname='animal_requests_yes_no';
+
+insert into ehr_lookups.lookups (set_name,container,value) select setname, container, 'SPF6 (-AAV & -RRV)' as value from ehr_lookups.lookup_sets where setname='viral_status';

b290617c2bd20a5cb80ebb0cde5abe3c3998d862
Chad Sebranek <csebranek@gmail.com>
2020-10-28 15:08:36 -0500

    use most recent date for now

--- WNPRC_EHR/resources/queries/study/demographicsMostRecentNecropsyAbstract.sql
+++ WNPRC_EHR/resources/queries/study/demographicsMostRecentNecropsyAbstract.sql
@@ -2,15 +2,17 @@ SELECT
 
     w.remark,
     w.id,
+    w.DateChanged,
     w.project
     /*timestampdiff('SQL_TSI_DAY', w.DateChanged, now()) AS DaysSinceFeedingChange,*/
 
 FROM (
 
-         SELECT m.Id, m.remark, m.project
+         SELECT m.Id, m.remark, t.DateChanged, m.project
          FROM (
                   SELECT
                       w.Id AS Id,
+                      max(w.date) AS DateChanged
 
                   FROM study.NecropsyAbstract w
                       /*WHERE w.qcstate.publicdata = true and w.weight is not null*/

88e89c356527d6856dde94d2291c1360f13a3dea
Binal <binalp@labkey.com>
2021-01-01 19:58:28 -0800

    Add other acct and investigator to the overview query

--- WNPRC_Purchasing/resources/queries/ehr_purchasing/purchasingRequestsOverview.sql
+++ WNPRC_Purchasing/resources/queries/ehr_purchasing/purchasingRequestsOverview.sql
@@ -1,5 +1,6 @@
 SELECT pr.rowId,
        pr.account,
+       pr.otherAcctAndInves,
        pr.qcState        AS requestStatus,
        pr.created          AS requestDate,
        pr.createdBy        AS requestor,
@@ -8,6 +9,7 @@ FROM ehr_purchasing.purchasingRequests pr
 LEFT JOIN ehr_purchasing.lineItems items ON pr.requestId = items.requestId
 GROUP BY pr.rowId,
          pr.account,
+         pr.otherAcctAndInves,
          pr.qcstate,
          pr.created,
          pr.createdBy

9fb5454ecb5ce7df2306e6de5562ae358281cc6f
Gabriel Gottfredsen <gabriel.gottfredsen@gmail.com>
2021-01-06 11:11:44 -0600

    add gestation day to each ultrasound measurement for data export purposes

--- WNPRC_EHR/resources/queries/study/_UltrasoundMeasurementsByResearchUltrasoundId.sql
+++ WNPRC_EHR/resources/queries/study/_UltrasoundMeasurementsByResearchUltrasoundId.sql
@@ -1,5 +1,6 @@
 PARAMETERS ( PARENT_RECORD_ID VARCHAR )
 SELECT
+  um.ultrasound_id,
   um.date,
   um.measurement_label,
   um.measurement_value,
@@ -8,6 +9,14 @@ SELECT
     FROM ultrasound_measurements um2
     WHERE um2.ultrasound_id = PARENT_RECORD_ID
     AND um2.measurement_name = um.measurement_name
-    GROUP BY measurement_label, measurement_unit) AS measurement_averages
+    GROUP BY measurement_label, measurement_unit) AS measurement_averages,
+  CASE WHEN EXISTS(SELECT 1 FROM pregnancy_outcomes po WHERE (SELECT ru.pregnancyid FROM study.research_ultrasounds ru WHERE ru.objectid = PARENT_RECORD_ID) = po.pregnancyid)
+          THEN 'Pregnancy Completed'
+       WHEN EXISTS(SELECT 1 FROM pregnancies p WHERE (SELECT ru.pregnancyid FROM study.research_ultrasounds ru WHERE ru.objectid = PARENT_RECORD_ID) = p.lsid)
+          THEN CONCAT(CAST((SELECT timestampdiff('SQL_TSI_DAY', p.date_conception, (SELECT ru.date FROM study.research_ultrasounds ru WHERE ru.objectid = PARENT_RECORD_ID))
+                            FROM pregnancies p
+                            WHERE (SELECT ru.pregnancyid FROM study.research_ultrasounds ru WHERE ru.objectid = PARENT_RECORD_ID) = p.lsid) AS VARCHAR), ' day(s)')
+          ELSE 'No Associated Pregnancy'
+  END AS gestation_day
 FROM ultrasound_measurements um
 WHERE um.ultrasound_id = PARENT_RECORD_ID

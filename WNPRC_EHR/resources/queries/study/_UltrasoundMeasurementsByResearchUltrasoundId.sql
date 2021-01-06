d86032320e30f9b7d1ab87d07ea8834a9a71ac5f
Gabriel Gottfredsen <gabriel.gottfredsen@gmail.com>
2021-01-06 09:53:05 -0600

    add averages to each ultrasound measurement

--- WNPRC_EHR/resources/queries/study/_UltrasoundMeasurementsByResearchUltrasoundId.sql
+++ WNPRC_EHR/resources/queries/study/_UltrasoundMeasurementsByResearchUltrasoundId.sql
@@ -3,6 +3,11 @@ SELECT
   um.date,
   um.measurement_label,
   um.measurement_value,
-  um.measurement_unit
+  um.measurement_unit,
+  (SELECT ROUND(AVG(um2.measurement_value), 2)
+    FROM ultrasound_measurements um2
+    WHERE um2.ultrasound_id = PARENT_RECORD_ID
+    AND um2.measurement_name = um.measurement_name
+    GROUP BY measurement_label, measurement_unit) AS measurement_averages
 FROM ultrasound_measurements um
 WHERE um.ultrasound_id = PARENT_RECORD_ID

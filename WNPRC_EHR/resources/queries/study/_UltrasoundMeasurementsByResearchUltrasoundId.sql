8052117fa068cf9058e521fa75d21d3ed3d78a16
Gabriel Gottfredsen <gabriel.gottfredsen@gmail.com>
2021-01-13 11:03:42 -0600

    show estimated gestational day at the time that the measurements were taken

--- WNPRC_EHR/resources/queries/study/_UltrasoundMeasurementsByResearchUltrasoundId.sql
+++ WNPRC_EHR/resources/queries/study/_UltrasoundMeasurementsByResearchUltrasoundId.sql
@@ -10,9 +10,7 @@ SELECT
     WHERE um2.ultrasound_id = PARENT_RECORD_ID
     AND um2.measurement_name = um.measurement_name
     GROUP BY measurement_label, measurement_unit) AS measurement_averages,
-  CASE WHEN EXISTS(SELECT 1 FROM pregnancy_outcomes po WHERE (SELECT ru.pregnancyid FROM study.research_ultrasounds ru WHERE ru.objectid = PARENT_RECORD_ID) = po.pregnancyid)
-          THEN 'Pregnancy Completed'
-       WHEN EXISTS(SELECT 1 FROM pregnancies p WHERE (SELECT ru.pregnancyid FROM study.research_ultrasounds ru WHERE ru.objectid = PARENT_RECORD_ID) = p.lsid)
+  CASE WHEN EXISTS(SELECT 1 FROM pregnancies p WHERE (SELECT ru.pregnancyid FROM study.research_ultrasounds ru WHERE ru.objectid = PARENT_RECORD_ID) = p.lsid)
           THEN CONCAT(CAST((SELECT timestampdiff('SQL_TSI_DAY', p.date_conception, (SELECT ru.date FROM study.research_ultrasounds ru WHERE ru.objectid = PARENT_RECORD_ID))
                             FROM pregnancies p
                             WHERE (SELECT ru.pregnancyid FROM study.research_ultrasounds ru WHERE ru.objectid = PARENT_RECORD_ID) = p.lsid) AS VARCHAR), ' day(s)')

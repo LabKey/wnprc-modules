8ac9c6e3449716fc35814a6deaa07584e46fe2ed
Gabriel Gottfredsen <gabriel.gottfredsen@gmail.com>
2021-03-02 17:34:51 -0600

    fixed denied request edit link to contain a correct returnUrl

--- WNPRC_EHR/resources/queries/study/SurgeryProcedureIndividualRequests.sql
+++ WNPRC_EHR/resources/queries/study/SurgeryProcedureIndividualRequests.sql
@@ -1,6 +1,6 @@
 SELECT 'EDIT' as editLink,participantid,date,enddate,procedurecategory,procedurename,project,account,
       surgeon,consultRequest,biopsyNeeded,surgerytechneeded,spineeded,vetneeded,statuschangereason,qcstate,
-      requestid,(select formtype from ehr.requests where requestid = sp.requestid) as formtype
+      requestid,(select formtype from ehr.requests where requestid = sp.requestid) as formtype, contextPath() || '/wnprc_ehr' || folderPath() || '/dataEntry.view' AS returnUrl
 FROM study.surgery_procedure sp
 WHERE sp.requestid IN
       (SELECT r.requestid FROM ehr.requests r

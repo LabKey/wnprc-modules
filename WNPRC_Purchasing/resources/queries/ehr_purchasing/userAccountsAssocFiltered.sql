7690d0da67b60d05810638d1c481152c10774343
Binal Patel <binalp@labkey.com>
2021-07-06 22:20:51 -0700

    Filter charge account on isAcceptingCharges for 'User Account Associations' and data entry page drop down.

--- WNPRC_Purchasing/resources/queries/ehr_purchasing/userAccountsAssocFiltered.sql
+++ WNPRC_Purchasing/resources/queries/ehr_purchasing/userAccountsAssocFiltered.sql
@@ -4,7 +4,8 @@ FROM (
 --    associate all accounts to the user if accessToAllAccounts flag is set to true
          SELECT a.rowid AS rowid,
                 a.displayName AS account,
-                u.userId
+                u.userId,
+                a.isAcceptingCharges
          FROM ehr_billingLinked.aliases a,
               (SELECT ua.userId,
                       ua.account
@@ -17,9 +18,10 @@ FROM (
 -- get accounts associated with user
          SELECT a.rowid    AS rowid,
                 a.displayName AS account,
-                ua.userId
+                ua.userId,
+                a.isAcceptingCharges
          FROM ehr_purchasing.userAccountAssociations ua
                   LEFT JOIN ehr_billingLinked.aliases a ON ua.account = a.alias
          WHERE ua.accessToAllAccounts IS NULL OR ua.accessToAllAccounts = false) userAndAccts
 
-WHERE ISMEMBEROF(userAndAccts.userId) --only display accounts associated with the current user
+WHERE userAndAccts.isAcceptingCharges IS TRUE AND ISMEMBEROF(userAndAccts.userId) --only display accounts associated with the current user

273c29ec123bcd38c9244cc3e632ed5445e6fa0d
F. Daniel Nicolalde <dnicolalde@gmail.com>
2021-07-28 15:11:15 -0500

    Change on name for class to Apprentice

--- WNPRC_EHR/src/org/labkey/wnprc_ehr/dataentry/forms/WaterMonitoring/FormSections/WaterSingleDayApprenticeSection.java
+++ WNPRC_EHR/src/org/labkey/wnprc_ehr/dataentry/forms/WaterMonitoring/FormSections/WaterSingleDayApprenticeSection.java
@@ -1,6 +1,7 @@
 package org.labkey.wnprc_ehr.dataentry.forms.WaterMonitoring.FormSections;
 
 import org.labkey.api.ehr.dataentry.SimpleFormSection;
+import org.labkey.api.view.template.ClientDependency;
 import org.labkey.wnprc_ehr.dataentry.generics.sections.SimpleGridSection;
 import org.labkey.wnprc_ehr.dataentry.generics.sections.SlaveGridSection;
 
@@ -9,14 +10,14 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-public class WaterSingleDaySlaveSection extends SlaveGridSection
+public class WaterSingleDayApprenticeSection extends SlaveGridSection
 {
-    public WaterSingleDaySlaveSection(){
+    public WaterSingleDayApprenticeSection(){
         super ("study", "waterAmount", "Order Additional Water for Today");
-        //setClientStoreClass("WNPRC.ext.data.SingleAnimal.WaterClientStore");
+        setClientStoreClass("WNPRC.ext.data.SingleAnimal.WaterApprenticeSectionClientStore");
        // setAllowBulkAdd(true);
     }
-    public WaterSingleDaySlaveSection(String title){
+    public WaterSingleDayApprenticeSection(String title){
         super ("study", "waterAmount", title);
         this.addConfigSource("WNPRC_Request");
         //setClientStoreClass("WNPRC.ext.data.SingleAnimal.WaterClientStore");
@@ -25,10 +26,8 @@ public class WaterSingleDaySlaveSection extends SlaveGridSection
     @Override
     public Set<String> getSlaveFields(){
         Set<String> fields = new HashSet<>();
-
         fields.add("Id");
         fields.add("project");
-
         return fields;
     }
     @Override

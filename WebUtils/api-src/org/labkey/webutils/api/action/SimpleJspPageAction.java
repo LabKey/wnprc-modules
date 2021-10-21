3183459ab8e1bbbc10a5412279e5af44bd582b0b
Adam Rauch <adam@labkey.com>
2021-10-21 11:26:42 -0700

    Require absolute paths to JSP files

--- WebUtils/api-src/org/labkey/webutils/api/action/SimpleJspPageAction.java
+++ WebUtils/api-src/org/labkey/webutils/api/action/SimpleJspPageAction.java
@@ -1,6 +1,5 @@
 package org.labkey.webutils.api.action;
 
-import org.jetbrains.annotations.NotNull;
 import org.labkey.api.view.JspView;
 
 /**
@@ -9,64 +8,23 @@ import org.labkey.api.view.JspView;
 public abstract class SimpleJspPageAction extends JspPageAction
 {
     @Override
-    public JspView getView()
+    public JspView<?> getView()
     {
-        return new JspView(resolveJspPath(getPathToJsp(), getModule().getClass()), getModel());
+        String path = getPathToJsp();
+        assert path.startsWith("/") && path.endsWith(".jsp") : "JSP paths must be absolute and end with .jsp";
+        return new JspView<>(path, getModel());
     }
 
-    /**
-     * Resolves a JSP path to the absolute file name.
-     *
-     * Examples:
-     * <ul>
-     *     <li>/org/labkey/webutils/JspPages/views/pages/TestPage.jsp --> /org/labkey/webutils/JspPages/views/pages/TestPage.jsp</li>
-     *     <li>/org/labkey/webutils/JspPages/views/pages/TestPage     --> /org/labkey/webutils/JspPages/views/pages/TestPage.jsp</li>
-     *     <li>pages/TestPage.jsp, JspController.class                --> /org/labkey/webutils/JspPages/views/pages/TestPage.jsp</li>
-     *     <li>pages/TestPage, JspController.class                    --> /org/labkey/webutils/JspPages/views/pages/TestPage.jsp</li>
-     * </ul>
-     *
-     * @param path to the JSP file. It can be relative or absolute, and it can optionally omit the ".jsp"
-     * @param baseClass A class to resolve the path relative to, if the path doesn't start with "/"
-     * @return A properly formatted JSP path, ready for JspView.
-     */
-    private String resolveJspPath(@NotNull String path, Class baseClass)
-    {
-        if (path.startsWith("/")) {
-            return path;
-        }
-
-        String newPath = getPackageDirFromClass(baseClass) + path;
-
-        if (!newPath.endsWith(".jsp")) {
-            newPath = newPath + ".jsp";
-        }
-
-        return newPath;
-    }
-
-    /**
-     * Gets the package directory for a class.
-     *
-     * Takes the package name, prefixes and postfixes a "/", and changes all internal "."'s to "/"'s.
-     * @param clazz The class to get the package path for.
-     * @return A string representing the path to the class's directory, relative to source root.
-     */
-    private String getPackageDirFromClass(Class clazz)
-    {
-        return "/" + clazz.getPackage().getName().replace(".", "/") + "/";
-    }
-
+    // This is never overridden. TODO: Remove this method and switch to JspView(String jspPath) constructor above?
     public Object getModel() {
         return null;
     }
 
     /**
-     * Returns a path (relative or absolute) to the JSP.
+     * Returns an absolute path to the JSP, starting with "/org/labkey/..." and ending with ".jsp". Relative paths are
+     * NOT supported.
      *
-     * By default, relative paths (ones that don't start with "/") are resolved relative module class that
-     * registered the controller returned by getControllerName().
-     *
-     * @return a path to the JSP
+     * @return an absolute path to the JSP
      */
     public abstract String getPathToJsp();
 }

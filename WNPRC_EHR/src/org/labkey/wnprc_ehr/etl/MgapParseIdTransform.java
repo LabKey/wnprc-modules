6360c90ee7079bc7a6ed57ee71f1cabc76992a65
Gabriel Gottfredsen <gabriel.gottfredsen@gmail.com>
2021-12-06 15:46:35 -0600

    add java class to transform originalId column into actual animal Id - test code only for now

--- WNPRC_EHR/src/org/labkey/wnprc_ehr/etl/MgapParseIdTransform.java
+++ WNPRC_EHR/src/org/labkey/wnprc_ehr/etl/MgapParseIdTransform.java
@@ -1,2 +1,15 @@
-package org.labkey.wnprc_ehr.etl;public class MgapParseIdTransform {
+package org.labkey.wnprc_ehr.etl;
+
+import org.labkey.api.di.columnTransform.ColumnTransform;
+
+public class MgapParseIdTransform extends ColumnTransform
+{
+    @Override
+    protected Object doTransform(Object inputValue)
+    {
+        //Object prefix = getInputValue("id");
+        return "this is a test";
+        //String prefixStr = null == prefix ? "" : prefix.toString();
+        //return prefixStr + "_" + inputValue + "_" + getConstant("myConstant");
+    }
 }

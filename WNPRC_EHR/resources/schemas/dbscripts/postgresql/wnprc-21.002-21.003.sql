711c447554255f51b193bcdbb0d2ab280f8a409b
F. Daniel Nicolalde <dnicolalde@gmail.com>
2021-12-06 14:53:56 -0600

    Fixing problem with the change of frequency from string to int. Creating table for watermonitoring in wnprc schema.

---
+++ WNPRC_EHR/resources/schemas/dbscripts/postgresql/wnprc-21.002-21.003.sql
@@ -0,0 +1,11 @@
+CREATE TABLE wnprc.watermonitoring_access;
+(
+    rowid                   serial                    NOT NULL,
+    date                    TIMESTAMP,
+    alloweduser             userid,
+    project                 integer,
+    createdBy               userid,
+    principalinvestigator   userid,
+    CONSTRAINT PK_watermonitoring_access PRIMARY KEY (rowid)
+);
+

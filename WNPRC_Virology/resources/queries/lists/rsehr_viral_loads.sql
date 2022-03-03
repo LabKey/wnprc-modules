45dc76af4308be9a289e40842d0e799253267977
Chad Sebranek <csebranek@gmail.com>
2022-03-03 11:35:47 -0600

    Concatenate results shared w multiple accounts, concatenate viral_load_replicates, and add metadata file for the filter query

--- WNPRC_Virology/resources/queries/lists/rsehr_viral_loads.sql
+++ WNPRC_Virology/resources/queries/lists/rsehr_viral_loads.sql
@@ -7,8 +7,8 @@ SELECT
     v.sourceMaterial.type AS source_type,
     v.comment AS comment,
     v.run.exptNumber as experiment_number,
-    vsq.funding_string,
-    GROUP_CONCAT(CAST(v.viralLoadScientific AS BIGINT ), ' ; ') AS viral_load_replicates,
+    GROUP_CONCAT(DISTINCT vsq.funding_string, ' ; ') as account,
+    GROUP_CONCAT(DISTINCT CAST(v.viralLoadScientific AS BIGINT ), ' ; ') AS viral_load_replicates,
     CASE WHEN (MIN(v.viralLoadScientific) = 0 AND MAX(v.viralLoadScientific) != 0) THEN ('Equoivical') END AS equivocal,
     MAX(v.RowId)as Key
 FROM "/WNPRC/WNPRC_Units/Research_Services/Virology_Services/VL_DB/".assay.Viral_Loads.Viral_Load.Data v
@@ -17,9 +17,8 @@ FROM "/WNPRC/WNPRC_Units/Research_Services/Virology_Services/VL_DB/".assay.Viral
          INNER JOIN "/WNPRC/WNPRC_Units/Research_Services/Virology_Services/VS_group_wiki/".lists.QPCR_QC_list q
                     ON CAST(q.Expt_nr AS integer) = CAST(v.run.exptNumber AS integer)
 
-
+-- join on vl_sample_queue to pull in the account # (funding string) for filtering results out on rsehr
 LEFT JOIN "/WNPRC/WNPRC_Units/Research_Services/Virology_Services/viral_load_sample_tracker/".lists.vl_sample_queue vsq
-
 ON vsq.Id = v.subjectId AND vsq.Sample_date = v.date
 
 WHERE
@@ -27,4 +26,4 @@ WHERE
 
 -- groupBy viral load so these can be averaged
 GROUP BY
-    v.sourceMaterial.type, v.sampleType, v.subjectId, v.date, v.assayId, v.comment, v.run.exptNumber, vsq.funding_string
+    v.sourceMaterial.type, v.sampleType, v.subjectId, v.date, v.assayId, v.comment, v.run.exptNumber

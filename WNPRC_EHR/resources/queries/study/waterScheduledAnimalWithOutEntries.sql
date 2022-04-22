73b7da60fbec3781be62d9b764b5707e875b81b8
F. Daniel Nicolalde <dnicolalde@gmail.com>
2022-04-22 15:35:09 -0500

    Adding date to the animalwithoutentries sql report.

--- WNPRC_EHR/resources/queries/study/waterScheduledAnimalWithOutEntries.sql
+++ WNPRC_EHR/resources/queries/study/waterScheduledAnimalWithOutEntries.sql
@@ -1,10 +1,10 @@
 PARAMETERS(CheckDate TIMESTAMP)
 
 SELECT DISTINCT(id), CheckDate AS date FROM
-    (SELECT a.id, a.MostRecentWaterCondition FROM study.demographicsMostRecentWaterCondition a
+    (SELECT a.id, a.MostRecentWaterCondition, a.MostRecentWaterConditionDate FROM study.demographicsMostRecentWaterCondition a
      WHERE NOT EXISTS (
          SELECT 1 FROM study.waterTotalByDate b
                        WHERE a.id = b.id AND b.date = CheckDate AND b.TotalWater IS NOT NULL
          )
     )
-WHERE MostRecentWaterCondition = 'regulated'
+WHERE MostRecentWaterCondition = 'regulated' AND MostRecentWaterConditionDate <= CheckDate

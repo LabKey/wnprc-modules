bb16648c63fe92a05a53a3fff49e69c7325984a6
Chad Sebranek <csebranek@gmail.com>
2022-05-20 13:30:28 -0500

    Initial commit for arrow connection with etls

---
+++ WNPRC_EHR/resources/queries/wnprc/MaxSpeciesDistinct.sql
@@ -0,0 +1,15 @@
+SELECT DISTINCT b.protocol_id,
+                b.protocol_title,
+                b.pi_name,
+                b.date_approved,
+                b.date_expiration,
+                b.date_modified,
+                a.sum_three_yr,
+                b.usda_code
+FROM arrow_protocols b
+         INNER JOIN (
+    SELECT protocol_id,
+           sum(max_three_year) AS sum_three_yr
+    FROM arrow_protocols
+    GROUP BY protocol_id
+) a ON a.protocol_id = b.protocol_id

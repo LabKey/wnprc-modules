4258f8d1f75c335c4e6ce3aca9996f61ac2097e6
Adam Rauch <adam@labkey.com>
2022-09-02 15:33:28 -0700

    Wrangle discrepancies

--- WNPRC_EHR/src/org/labkey/wnprc_ehr/calendar/GoogleCalendar.java
+++ WNPRC_EHR/src/org/labkey/wnprc_ehr/calendar/GoogleCalendar.java
@@ -4,7 +4,7 @@ import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.HttpRequestInitializer;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
-import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.json.gson.GsonFactory;
 import com.google.api.client.util.DateTime;
 import com.google.api.services.calendar.Calendar;
 import com.google.api.services.calendar.CalendarScopes;
@@ -37,8 +37,7 @@ public abstract class GoogleCalendar implements org.labkey.wnprc_ehr.calendar.Ca
             "Google Calendar API Java Quickstart";
 
     /** Global instance of the JSON factory. */
-    private static final JsonFactory JSON_FACTORY =
-            JacksonFactory.getDefaultInstance();
+    private static final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
 
     private static final String TOKENS_DIRECTORY_PATH = "tokens";
 

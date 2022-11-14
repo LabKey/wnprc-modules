c8f7b031d54be2d82afa3d92003e6fa1ab0b9661
Chad Sebranek <csebranek@gmail.com>
2022-11-14 10:49:30 -0600

    Add if exists check to update script

--- WNPRC_EHR/resources/schemas/dbscripts/postgresql/wnprc-21.006-21.007.sql
+++ WNPRC_EHR/resources/schemas/dbscripts/postgresql/wnprc-21.006-21.007.sql
@@ -1,5 +1,5 @@
-alter table wnprc.animal_requests add column pregnantanimalsrequiredterminfant varchar(100);
-alter table wnprc.animal_requests add column pregnantanimalsrequiredtermdam varchar(100);
-alter table wnprc.animal_requests add column majorsurgery varchar(100);
-alter table wnprc.animal_requests add column previousexposures text;
-alter table wnprc.animal_requests add column contacts text;
+alter table if exists wnprc.animal_requests add column pregnantanimalsrequiredterminfant varchar(100);
+alter table if exists wnprc.animal_requests add column pregnantanimalsrequiredtermdam varchar(100);
+alter table if exists wnprc.animal_requests add column majorsurgery varchar(100);
+alter table if exists wnprc.animal_requests add column previousexposures text;
+alter table if exists wnprc.animal_requests add column contacts text;

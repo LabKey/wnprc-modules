470953d6581780c7cfab996685b6f90c7d56a2e4
Adam Rauch <adam@labkey.com>
2023-01-19 19:33:01 -0800

    Migrate DataEntryForm and FormSection to new JSONObject

--- WNPRC_EHR/src/org/labkey/wnprc_ehr/dataentry/generics/sections/HTMLSection.java
+++ WNPRC_EHR/src/org/labkey/wnprc_ehr/dataentry/generics/sections/HTMLSection.java
@@ -1,14 +1,15 @@
 package org.labkey.wnprc_ehr.dataentry.generics.sections;
 
-import org.json.old.JSONObject;
+import org.json.JSONObject;
 import org.labkey.api.ehr.dataentry.DataEntryFormContext;
 import org.labkey.api.ehr.dataentry.NonStoreFormSection;
 
 /**
  * Created by jon on 3/4/16.
  */
-public abstract class HTMLSection extends NonStoreFormSection {
-    private static String XTYPE = "panel";
+public abstract class HTMLSection extends NonStoreFormSection
+{
+    private static final String XTYPE = "panel";
 
     public HTMLSection(String name, String label) {
         super(name, label, XTYPE);
@@ -17,15 +18,18 @@ public abstract class HTMLSection extends NonStoreFormSection {
     public abstract String getHTML();
 
     @Override
-    public JSONObject toJSON(DataEntryFormContext ctx, boolean includeFormElements) {
+    public JSONObject toJSON(DataEntryFormContext ctx, boolean includeFormElements)
+    {
         JSONObject json = super.toJSON(ctx, includeFormElements);
 
         // formConfig gets copied onto the section Ext4 config.
         JSONObject formConfig;
-        if (json.containsKey("formConfig")) {
+        if (json.has("formConfig"))
+        {
             formConfig = json.getJSONObject("formConfig");
         }
-        else {
+        else
+        {
             formConfig = new JSONObject();
         }
         formConfig.put("html", getHTML());

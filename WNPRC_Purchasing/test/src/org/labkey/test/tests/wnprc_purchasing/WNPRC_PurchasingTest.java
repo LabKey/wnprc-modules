f55bc2aad9d9fb22db4e30ca49bc5da9394890a4
labkey-tchad <tchad@labkey.com>
2023-01-23 08:45:19 -0800

    Use DomainFormPanel component

--- WNPRC_Purchasing/test/src/org/labkey/test/tests/wnprc_purchasing/WNPRC_PurchasingTest.java
+++ WNPRC_Purchasing/test/src/org/labkey/test/tests/wnprc_purchasing/WNPRC_PurchasingTest.java
@@ -35,6 +35,7 @@ import org.labkey.test.TestTimeoutException;
 import org.labkey.test.categories.EHR;
 import org.labkey.test.categories.WNPRC_EHR;
 import org.labkey.test.componenes.CreateVendorDialog;
+import org.labkey.test.components.domain.DomainFormPanel;
 import org.labkey.test.components.dumbster.EmailRecordTable;
 import org.labkey.test.components.html.SiteNavBar;
 import org.labkey.test.util.APIUserHelper;
@@ -173,9 +174,8 @@ public class WNPRC_PurchasingTest extends BaseWebDriverTest implements PostgresO
         selectQuery("ehr_billing", "aliases");
         clickAndWait(Locator.linkWithText("create definition"), 5000);
 
-        Locator.XPathLocator manuallyDefineFieldsLoc = Locator.tagWithClass("div", "domain-form-manual-btn");
-        click(manuallyDefineFieldsLoc);
-        setFormElement(Locator.inputByNameContaining("domainpropertiesrow-name"), extensibleCol);
+        DomainFormPanel domainFormPanel = new DomainFormPanel.DomainFormPanelFinder(getDriver()).find();
+        domainFormPanel.manuallyDefineFields(extensibleCol);
         clickButton("Save");
     }
 

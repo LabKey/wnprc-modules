870d2f23ec42d3943f189d07e0c5014479b60303
Adam Rauch <adam@labkey.com>
2023-01-30 20:27:53 -0800

    Migrate DataEntryForm and FormSection to new JSONObject (#243)

--- WNPRC_EHR/src/org/labkey/wnprc_ehr/dataentry/generics/sections/SlaveFormSection.java
+++ WNPRC_EHR/src/org/labkey/wnprc_ehr/dataentry/generics/sections/SlaveFormSection.java
@@ -1,7 +1,7 @@
 package org.labkey.wnprc_ehr.dataentry.generics.sections;
 
-import org.json.old.JSONArray;
-import org.json.old.JSONObject;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.labkey.api.ehr.dataentry.DataEntryFormContext;
 
 import java.util.Set;
@@ -9,19 +9,23 @@ import java.util.Set;
 /**
  * Created by jon on 4/13/16.
  */
-abstract public class SlaveFormSection extends SimpleFormSection {
-    public SlaveFormSection(String schemaName, String queryName, String label) {
+abstract public class SlaveFormSection extends SimpleFormSection
+{
+    public SlaveFormSection(String schemaName, String queryName, String label)
+    {
         super(schemaName, queryName, label);
 
         setClientStoreClass("WNPRC.ext.data.SingleAnimal.SlaveSectionClientStore");
     }
 
     @Override
-    public JSONObject toJSON(DataEntryFormContext ctx, boolean includeFormElements) {
+    public JSONObject toJSON(DataEntryFormContext ctx, boolean includeFormElements)
+    {
         JSONObject json = super.toJSON(ctx, includeFormElements);
 
         JSONArray slaveFields = new JSONArray();
-        for(String field : getSlaveFields()) {
+        for (String field : getSlaveFields())
+        {
             slaveFields.put(field);
         }
         json.put("slaveFieldsToInclude", slaveFields);

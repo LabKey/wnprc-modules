c3982b9589ec69bf23f4068304383ed9065219cd
Binal Patel <binalp@labkey.com>
2023-03-29 16:54:29 -0700

    Add an 22.002 upgrade script that would run on dev machines (with an existing 22.001 wnprc_ehr script)

---
+++ WNPRC_EHR/resources/schemas/dbscripts/postgresql/wnprc-22.001-22.002.sql
@@ -0,0 +1,7 @@
+-- wnprc-21.006-21.007.sql was added after wnprc-22.000-22.001.sql, in which case wnprc-21.006-21.007.sql would not run on some dev machines
+-- this script is a conditionalized version of what wnprc-21.006-21.007.sql intended to achieve
+alter table wnprc.animal_requests add column if not exists pregnantanimalsrequiredterminfant varchar(100);
+alter table wnprc.animal_requests add column if not exists pregnantanimalsrequiredtermdam varchar(100);
+alter table wnprc.animal_requests add column if not exists majorsurgery varchar(100);
+alter table wnprc.animal_requests add column if not exists previousexposures text;
+alter table wnprc.animal_requests add column if not exists contacts text;

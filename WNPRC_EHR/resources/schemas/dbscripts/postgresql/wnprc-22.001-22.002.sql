4fc73b65d51ab8c79f8770916280622569dc98aa
Binal Patel <binalp@labkey.com>
2023-03-29 16:54:58 -0700

    update comment

--- WNPRC_EHR/resources/schemas/dbscripts/postgresql/wnprc-22.001-22.002.sql
+++ WNPRC_EHR/resources/schemas/dbscripts/postgresql/wnprc-22.001-22.002.sql
@@ -1,5 +1,5 @@
 -- wnprc-21.006-21.007.sql was added after wnprc-22.000-22.001.sql, in which case wnprc-21.006-21.007.sql would not run on some dev machines
--- this script is a conditionalized version of what wnprc-21.006-21.007.sql intended to achieve
+-- this script is a correct conditionalized version of what wnprc-21.006-21.007.sql intended to achieve
 alter table wnprc.animal_requests add column if not exists pregnantanimalsrequiredterminfant varchar(100);
 alter table wnprc.animal_requests add column if not exists pregnantanimalsrequiredtermdam varchar(100);
 alter table wnprc.animal_requests add column if not exists majorsurgery varchar(100);

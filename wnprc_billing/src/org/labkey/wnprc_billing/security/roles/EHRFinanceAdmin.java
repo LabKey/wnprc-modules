b7c75d51ca8928e0b24f8ee9f805ee0a07876252
aschmidt34 <aschmidt34@wisc.edu>
2023-03-29 00:27:51 -0500

    Added EHRFinanceAdmin.java and EHRFinanceAdminPermission.java.  Updated wnprc_billing module.xml variables CreditToAccount and JETCSVSetting.

--- wnprc_billing/src/org/labkey/wnprc_billing/security/roles/EHRFinanceAdmin.java
+++ wnprc_billing/src/org/labkey/wnprc_billing/security/roles/EHRFinanceAdmin.java
@@ -1,5 +1,7 @@
 package org.labkey.wnprc_billing.security.roles;
 
+import org.labkey.api.security.permissions.AdminPermission;
+import org.labkey.api.security.permissions.UpdatePermission;
 import org.labkey.api.security.roles.AbstractRole;
 import org.labkey.wnprc_billing.security.permissions.EHRFinanceAdminPermission;
 
@@ -7,6 +9,11 @@ public class EHRFinanceAdmin extends AbstractRole {
     public EHRFinanceAdmin() {
         super("EHR Finance Admin",
                 "This role allows users to modify the current program income account.",
-                EHRFinanceAdminPermission.class);
+                EHRFinanceAdminPermission.class
+        );
+//        super("EHR Finance Admin",
+//                "This role allows users to modify the current program income account.",
+//                EHRFinanceAdminPermission.class
+//        );
     }
 }

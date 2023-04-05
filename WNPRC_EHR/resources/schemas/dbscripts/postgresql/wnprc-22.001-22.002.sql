0ad678ff1de4155983849292e669ed3d38f63f2f
Chad Sebranek <csebranek@gmail.com>
2023-04-05 10:12:23 -0500

    Add old pi field to wnprc schema metadata, hide it, and add comments to sql update script

--- WNPRC_EHR/resources/schemas/dbscripts/postgresql/wnprc-22.001-22.002.sql
+++ WNPRC_EHR/resources/schemas/dbscripts/postgresql/wnprc-22.001-22.002.sql
@@ -1,4 +1,4 @@
---Modifying table for animal_requests renaming column and removing constraints.
+--Modifying wnprc.animal_requests table by renaming pi column and adding constraint 
 ALTER TABLE IF EXISTS wnprc.animal_requests RENAME COLUMN principalinvestigator TO principalinvestigatorold;
 ALTER TABLE IF EXISTS wnprc.animal_requests ALTER COLUMN principalinvestigatorold DROP NOT NULL;
 ALTER TABLE IF EXISTS wnprc.animal_requests ADD COLUMN principalinvestigator INTEGER;

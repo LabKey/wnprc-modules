c8f50e512eed74a42916b11b6ef3f9f645b706ce
aschmidt34 <aschmidt34@wisc.edu>
2023-07-31 15:09:34 -0500

    Saving test alert.

---
+++ docker/ehrcron/scripts/automaticAlerts/myTestAlert.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/env perl
+
+=head1 DESCRIPTION
+
+This is a test script designed to run as a cron job.
+
+=head1 LICENSE
+
+This package and its accompanying libraries are free software; you can
+redistribute it and/or modify it under the terms of the GPL (either
+version 1, or at your option, any later version) or the Artistic
+License 2.0.
+
+=head1 AUTHOR
+
+Alex Schmidt
+
+=cut
+
+#config options:

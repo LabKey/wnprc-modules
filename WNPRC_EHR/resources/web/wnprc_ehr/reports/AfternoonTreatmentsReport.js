cd6868ac1dc92e3c127acb594b6260ce7072c715
LeviCameron1 <lcameron3@wisc.edu>
2023-08-15 12:44:53 -0500

    Updates to the treatment grid panels

--- WNPRC_EHR/resources/web/wnprc_ehr/reports/AfternoonTreatmentsReport.js
+++ WNPRC_EHR/resources/web/wnprc_ehr/reports/AfternoonTreatmentsReport.js
@@ -2,9 +2,37 @@ Ext4.namespace('EHR.reports');
 EHR.reports.AfternoonTreatmentsReport = function (panel, tab) {
 
     const renderUrgent = () => {
-        const target = tab.add({xtype: 'ldk-contentresizingpanel'});
+        let filterArray = panel.getFilterArray(tab);
+        let title = panel.getTitleSuffix();
 
-        const config = {
+        let target = tab.add({tag: 'span', style: 'padding-bottom: 20px'});
+        tab.doLayout();
+        let reportStartDate = new Date();
+        reportStartDate.setDate(reportStartDate.getDate()-40);
+        reportStartDate = reportStartDate.format(LABKEY.extDefaultDateFormat)
+
+        let config = panel.getQWPConfig({
+            title: 'Water Treatments',
+            schemaName: 'study',
+            queryName: 'WaterScheduleCoalesced',
+            viewName: 'Treatments PM',
+            parameters: {'NumDays': '180', 'StartDate': reportStartDate},
+            filters: filterArray.nonRemovable,
+            removeableFilters: filterArray.removable,
+            frame: true
+
+        });
+
+        tab.add({
+            xtype: 'ldk-querypanel',
+            style: 'margin-bottom:20px;',
+            queryConfig: config
+
+        });
+
+        target = tab.add({xtype: 'ldk-contentresizingpanel'});
+
+        config = {
             schemaName: 'study',
             queryName: 'treatmentSchedule',
             viewName: 'PM Treatments',

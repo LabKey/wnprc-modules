6273f09bdd3a88f0bd287ef08e31f799f276f743
Chad Sebranek <csebranek@gmail.com>
2023-08-18 11:57:41 -0500

    Adjust tooltip and date alignment, reduce trashcan size

--- WNPRC_EHR/src/client/weight/containers/Forms/EnterWeightFormContainer.tsx
+++ WNPRC_EHR/src/client/weight/containers/Forms/EnterWeightFormContainer.tsx
@@ -720,7 +720,7 @@ const EnterWeightFormContainer: React.FunctionComponent<any> = props => {
                 .filter(item => item.command.value != "delete")
                 .map((item, i) => (
                   <div className={`row ${item.visibility.value}`} key={i}>
-                    <div className="col-xs-10">
+                    <div className="col-xs-11">
                       <div className="row card" key={i}>
                         <div
                           className={`card-header ${item.validated.value ? "valid" : "invalid"}`}
@@ -805,8 +805,8 @@ const EnterWeightFormContainer: React.FunctionComponent<any> = props => {
                       >
                         <svg
                           xmlns="http://www.w3.org/2000/svg"
-                          width="48"
-                          height="48"
+                          width="32"
+                          height="32"
                           viewBox="0 0 24 24"
                         >
                           <path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z" />
@@ -844,7 +844,7 @@ const EnterWeightFormContainer: React.FunctionComponent<any> = props => {
             </button>
             {errorLevel === "submittable" &&
             <div>
-              <span id="invalid-tooltip" data-tooltip={"Fill in missing information for boxes outlined in red."}>⚠️</span>
+              <span id="invalid-tooltip" data-tooltip={"Fill in missing information for boxes marked with ❗"}>⚠️ </span>
             </div>
             }
           </div>

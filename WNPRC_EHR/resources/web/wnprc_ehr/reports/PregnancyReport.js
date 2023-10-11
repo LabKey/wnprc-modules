af489acf1c9b194d33e2b3cb02895dc27c370320
LeviCameron1 <lcameron3@wisc.edu>
2023-10-11 15:44:32 -0500

    Fixed pregnancies report not filtering

--- WNPRC_EHR/resources/web/wnprc_ehr/reports/PregnancyReport.js
+++ WNPRC_EHR/resources/web/wnprc_ehr/reports/PregnancyReport.js
@@ -1,7 +1,16 @@
 Ext4.namespace('EHR.reports');
 EHR.reports.PregnancyReport = function (panel, tab) {
     const target = tab.add({xtype: 'ldk-contentresizingpanel'});
-
+    let config = {
+        schemaName: 'study',
+        queryName: 'PregnancyInfo',
+        viewName: '',
+        columnStyles: JSON.stringify({remark: "remark-column-style"}),
+        filterConfig: JSON.stringify({
+            subjects: tab.filters.subjects,
+            filters: tab.filters,
+        }),
+    };
     try {
         // according to the DOM spec, the mutation observer should be GC'd if/when the target node is removed
         const observer = new MutationObserver(target.fireEvent.bind(target, 'contentsizechange'));
@@ -12,7 +21,7 @@ EHR.reports.PregnancyReport = function (panel, tab) {
     }
 
     const wp = new LABKEY.WebPart({
-        partConfig: {subjects: (tab.filters.subjects || []).join(';')},
+        partConfig: config,
         partName: 'Pregnancies Webpart',
         renderTo: target.renderTarget,
         style: 'margin-bottom: 20px;'

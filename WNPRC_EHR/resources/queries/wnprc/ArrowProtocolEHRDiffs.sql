ed16143bb712a988555185d399abae5a6f0b8b4f
Chad Sebranek <csebranek@gmail.com>
2024-01-08 12:38:41 -0600

    Fix arrow deleted protocol ETL problem (#486)
    * Use current time for delete source, to always delete, update ETL timestamp column to fix null pointer
    
    * Update jdbc driver for arrow connection

--- WNPRC_EHR/resources/queries/wnprc/ArrowProtocolEHRDiffs.sql
+++ WNPRC_EHR/resources/queries/wnprc/ArrowProtocolEHRDiffs.sql
@@ -1,6 +1,6 @@
 --used to find rows that do not exist in arrow connection but not in ehr protocol, for deletion in ETL
 SELECT protocol as protocol_id,
-       modified as date_modified
+        NOW() as date_modified
 FROM ehr.protocol a
 WHERE NOT EXISTS
     (SELECT w.protocol_id as protocol

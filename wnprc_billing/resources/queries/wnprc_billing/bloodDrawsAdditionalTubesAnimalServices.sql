8ff91667c0ab32be4c50735ea6f5fd1a174be403
Binal Patel <binalp@labkey.com>
2024-04-02 22:22:14 -0700

    Refactor blood draws procedure query. Update BillingPipelineJobProcess to use the new method.

---
+++ wnprc_billing/resources/queries/wnprc_billing/bloodDrawsAdditionalTubesAnimalServices.sql
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2024 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+-- Get rows where num_tubes > 1, the first tube is included in the bloodDrawsOneTubeAnimalServices.sql
+SELECT
+    Id,
+    date,
+    project,
+    project.account.alias AS debitedAccount,
+    project.account.tier_rate.tierRate AS otherRate,
+    objectid AS sourceRecord,
+    null AS comment,
+    (num_tubes - 1) AS quantity,
+    taskId,
+    performedby
+FROM studyLinked.BloodSchedule bloodSch WHERE num_tubes > 1 AND billedBy.value = 'a'
---
+++ wnprc_billing/resources/queries/wnprc_billing/bloodDrawsOneTubeAnimalServices.sql
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2024 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+-- Get rows where num_tubes = 1
+SELECT
+    Id,
+    date,
+    project,
+    project.account.alias AS debitedAccount,
+    project.account.tier_rate.tierRate AS otherRate,
+    objectid AS sourceRecord,
+    ('Blood Draws ' || Id) AS comment,
+    num_tubes AS quantity,
+    taskId,
+    performedby
+FROM studyLinked.BloodSchedule bloodSch WHERE num_tubes = 1 AND billedBy.value = 'a'
+
+UNION
+
+-- Get the 1st tube where num_tubes > 1
+SELECT
+    Id,
+    date,
+    project,
+    project.account.alias AS debitedAccount,
+    project.account.tier_rate.tierRate AS otherRate,
+    objectid AS sourceRecord,
+    ('Blood Draws ' || Id) AS comment,
+    1 AS quantity,
+    taskId,
+    performedby
+FROM studyLinked.BloodSchedule bloodSch WHERE num_tubes > 1 AND billedBy.value = 'a'

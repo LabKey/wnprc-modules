dfa97e23d5db49dcf365543df67311bae4d299b2
LeviCameron1 <lcameron3@wisc.edu>
2024-05-10 16:02:41 -0500

    Added some styles for modifictiona

--- CageUI/src/client/components/RoomDisplay.tsx
+++ CageUI/src/client/components/RoomDisplay.tsx
@@ -10,7 +10,7 @@ interface DisplayProps {
 export const RoomDisplay: FC<DisplayProps> = (props) => {
     const {name} = props;
     const [currRoom, setCurrRoom] = useState<Rack[]>(loadRoom(name));
-    // insert logic to find ro
+    // insert logic to find room
     return (
         <div className={"room-display"}>
             <RoomLayout
--- CageUI/src/client/components/typings.ts
+++ CageUI/src/client/components/typings.ts
@@ -1,24 +1,151 @@
+
 export interface Cage {
     id: number
     name: string;
+    cageState: CageState;
+    position: string;
+}
+
+interface RackTwoOfTwo {
+    topDivider: Modification,
+    bottomDivider: Modification,
+    leftFloor: Modification,
+    rightFloor: Modification
 }
 
 export interface Rack {
-    id: number
-    cages: Cage[]
+    id: number;
+    type: string;
+    cages: Cage[];
+    separators: RackTwoOfTwo; //TODO add additional rack interfaces
+}
+export enum RackTypes {
+    OneOfOne,
+    TwoOfTwo,
+    Pen,
+    MultiHorizontal
+}
+export enum ModTypes {
+    StandardFloor,
+    MeshFloor,
+    NoFloor,
+    SolidDivider,
+    PCDivider, // protected contact
+    VCDivider, // visual contact
+    PrivacyDivider,
+    NoDivider,
+    CTunnel,
+    PopTop,
+    BumpOut,
+    PlayCage
+}
+
+export interface Modification {
+    name: string;
+    mod: ModTypes;
+    styles: {
+        property: string;
+        value: string;
+    }[]
 }
 
 interface SchematicRoomProps {
     [key: string]: {
         rackNum: number;
         cageNum: number;
+        rackTypes: RackTypes[];
     }
 }
+
+
+export interface CageState {
+    divider: Modification | undefined;
+    floor: Modification | undefined;
+    extraMods: Modification[] | undefined;
+}
+
 const Schematics: SchematicRoomProps = {
     "AB140-167.svg": {
         rackNum: 6,
-        cageNum: 4
+        cageNum: 4,
+        rackTypes: [RackTypes.TwoOfTwo], // rack types starting at cage 1 going up (single value means all racks the same)
     }
+}
+
+//TODO finish styles
+export const Modifications = {
+    standardFloor: {
+        name: "Standard Floor",
+        mod: ModTypes.StandardFloor,
+        styles: [{
+            property: "stroke",
+            value: "black"
+        }]
+    },
+    meshFloor: {
+        name: "Mesh Floor",
+        mod: ModTypes.MeshFloor,
+        styles: [
+            {
+                property: "stroke",
+                value: "black"
+            },
+            {
+                property: "stroke-dasharray",
+                value: "10"
+            }
+        ]
+    },
+    noFloor: {
+        name: "No Floor",
+        mod: ModTypes.NoFloor,
+        styles: [
+            {
+                property: "stroke",
+                value: "none"
+            }
+        ]
+    },
+    solidDivider: {
+        name: "Solid Divider",
+        mod: ModTypes.SolidDivider,
+        styles: [{
+            property: "stroke",
+            value: "black"
+        }]
+    },
+    pcDivider: {
+        name: "Protected Contact Divider",
+        mod: ModTypes.PCDivider
+    },
+    vcDivider: {
+        name: "Visual Contact Divider",
+        mod: ModTypes.VCDivider
+    },
+    privacyDivider: {
+        name: "Privacy Divider",
+        mod: ModTypes.PrivacyDivider
+    },
+    noDivider: {
+        name: "No Divider",
+        mod: ModTypes.NoDivider
+    },
+    cTunnel: {
+        name: "C-Tunnel",
+        mod: ModTypes.CTunnel
+    },
+    popTop: {
+        name: "Pop Top",
+        mod: ModTypes.PopTop
+    },
+    bumpOut: {
+        name: "Bump Out",
+        mod: ModTypes.BumpOut
+    },
+    playCage: {
+        name: "Play Cage",
+        mod: ModTypes.PlayCage
+    },
 }
 
 export const RoomSchematics = {
@@ -29,4 +156,15 @@ export const RoomSchematics = {
     ab163: Schematics["AB140-167.svg"],
     ab165: Schematics["AB140-167.svg"],
     ab167: Schematics["AB140-167.svg"],
+}
+export const RackCombinors = {
+    rackTwoOfTwo: {
+        topDivider: Modifications.solidDivider,
+        bottomDivider: Modifications.solidDivider,
+        leftFloor: Modifications.standardFloor,
+        rightFloor: Modifications.standardFloor
+    },
+    rackOneOfOne: {
+        floor: Modifications.standardFloor
+    }
 }

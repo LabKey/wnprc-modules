c87f5e30902856bcb08f2f31ff214d209b7a698f
LeviCameron1 <lcameron3@wisc.edu>
2024-05-13 17:03:45 -0500

    Separated options for selecting divider/floors/extras

--- CageUI/src/client/components/CageDetailsModifications.tsx
+++ CageUI/src/client/components/CageDetailsModifications.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react';
 import { FC, useEffect, useState } from 'react';
-import { Cage, Modifications, ModTypes } from './typings';
+import { Cage } from './typings';
 import Select from 'react-select';
-import { convertLocationName } from './helpers';
+import { convertLocationName, getModOptions } from './helpers';
 interface ModificationDetailsProps {
     cage: Cage
 }
@@ -11,9 +11,6 @@ export const CageDetailsModifications: FC<ModificationDetailsProps> = (props) =>
 
     const [modRows, setModRows] = useState<React.JSX.Element[]>([]);
 
-    const modOptions = Object.keys(Modifications).map((mod, idx) => (
-        {value: Modifications[mod].mod, label: Modifications[mod].name}
-    ))
     const addMod = () => {
         console.log("Add Mod");
     }
@@ -22,9 +19,10 @@ export const CageDetailsModifications: FC<ModificationDetailsProps> = (props) =>
         console.log(event);
         console.log(cage);
     }
-
+    //TODO move table row to sep file to improve readability
     useEffect(() => {
         const newModRows = Object.keys(cage.cageState).map((key, idx) => {
+            const modOptions = getModOptions(key);
             if (Array.isArray(cage.cageState[key])) { // finds extra mods
                 return(
                     cage.cageState[key].map((mod, idx) => {
@@ -32,11 +30,40 @@ export const CageDetailsModifications: FC<ModificationDetailsProps> = (props) =>
                         return (
                             <tr key={`extraMod-${idx}`}>
                                 <td>{convertLocationName(key)}</td>
-                                <td>{mod.name}</td>
+                                <td>
+                                    <Select
+                                        options={modOptions}
+                                        isDisabled={false}// TODO add permissions to access this
+                                        defaultValue={modOptions.find((mod) => (
+                                            Array.isArray(cage.cageState[key]) &&
+                                            cage.cageState[key].some((item) => item.mod === mod.value)
+                                        ))}
+                                        onChange={changeMod}
+                                        styles={{
+                                            control: (base) => ({
+                                                ...base,
+                                                backgroundColor: 'inherit',
+                                                borderColor: 'inherit'
+                                            }),
+                                            indicatorsContainer: (base) => ({
+                                                ...base,
+                                                color: 'inherit',
+                                            }),
+                                            indicatorSeparator: (base) => ({
+                                                ...base,
+                                                backgroundColor: 'black',
+                                            }),
+                                            singleValue: (base) => ({
+                                                ...base,
+                                                color: 'black',
+                                            }),
+                                        }}
+                                    />
+                                </td>
                                 <td>#testCageNum</td>
                             </tr>
                         );
-                }));
+                    }));
             } else { // finds separators
                 return (
                     <tr key={`separator-${idx}`}>
@@ -52,16 +79,16 @@ export const CageDetailsModifications: FC<ModificationDetailsProps> = (props) =>
                                 styles={{
                                     control: (base) => ({
                                         ...base,
-                                        backgroundColor: "inherit",
-                                        borderColor: "inherit"
+                                        backgroundColor: 'inherit',
+                                        borderColor: 'inherit'
                                     }),
                                     indicatorsContainer: (base) => ({
                                         ...base,
-                                        color: "black",
+                                        color: 'inherit',
                                     }),
                                     indicatorSeparator: (base) => ({
                                         ...base,
-                                        backgroundColor: "black",
+                                        backgroundColor: 'black',
                                     }),
                                     singleValue: (base) => ({
                                         ...base,
@@ -69,13 +96,13 @@ export const CageDetailsModifications: FC<ModificationDetailsProps> = (props) =>
                                     }),
                                 }}
                             />
-                            {/*cage.cageState[key].name*/}
                         </td>
                         <td>#0004</td>
                     </tr>
                 );
             }
         });
+
         setModRows((prevState) => [...prevState, ...newModRows]);
     }, []);
     useEffect(() => {

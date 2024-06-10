67bbac3aa1c9d2592a708ff01725bf41aaa3f33c
F. Daniel Nicolalde <dnicolalde@gmail.com>
2024-06-10 15:58:29 -0500

    Adding parameter for the full history report. Bug introduce with waterTotal query.

---
+++ WNPRC_EHR/resources/web/wnprc_ehr/wnprcFullHistoryReports.js
@@ -0,0 +1,31 @@
+Ext4.namespace('EHR.reports');
+
+EHR.reports.wnprcFullHistory = function (panel, tab) {
+    var filterArray = panel.getFilterArray(tab);
+    var title = panel.getTitleSuffix();
+    var animalIds= tab.filters.subjects;
+
+    var target = tab.add({tag: 'span', style: 'padding-bottom: 20px'});
+    tab.doLayout();
+    var reportEndDate = new Date();
+    reportEndDate = reportEndDate.format(LABKEY.extDefaultDateFormat)
+
+    var config = panel.getQWPConfig({
+        title: 'Full History - '+animalIds,
+        schemaName: 'study',
+        queryName: 'wnprcFullHistory',
+        parameters: {'STARTTARGET': '2021-01-01', 'ENDTARGETDATE': reportEndDate},
+        filters: filterArray.nonRemovable,
+        removeableFilters: filterArray.removable,
+        frame: true
+
+    });
+
+    tab.add({
+        xtype: 'ldk-querypanel',
+        style: 'margin-bottom:20px;',
+        queryConfig: config
+
+    });
+    
+};

63ce32bd1caf4f05cf3946d1721ef07005a17cad
LeviCameron1 <lcameron3@wisc.edu>
2024-06-14 14:25:55 -0500

    Added ability to minimize the legend

--- CageUI/src/client/components/RoomLegend.tsx
+++ CageUI/src/client/components/RoomLegend.tsx
@@ -1,19 +1,24 @@
 import * as React from 'react';
-import {FC} from 'react';
+import { FC, useState } from 'react';
 import { ActionURL } from '@labkey/api';
 import { changeStyleProperty, getRackSeparators, parseSeparator } from './helpers';
 import { ReactSVG } from 'react-svg';
 
 export const RoomLegend: FC<any> = () => {
+    const [showLegend, setShowLegend] = useState<boolean>(true);
+
     return (
         <div className={"room-legend"}>
-            <h1 className={"legend-header"}>Legend</h1>
-
-            <ReactSVG
-                src={`${ActionURL.getContextPath()}/cageui/static/legend.svg`}
-                wrapper={"div"}
-                className={"legend-svg"}
-            />
+            <button className={"legend-header-btn"} onClick={() => setShowLegend(!showLegend)}>
+                <h2 className={'legend-header'}>Legend</h2>
+            </button>
+            {showLegend && (
+                <ReactSVG
+                    src={`${ActionURL.getContextPath()}/cageui/static/legend.svg`}
+                    wrapper={"div"}
+                    className={"legend-svg"}
+                />
+            )}
         </div>
     );
 }

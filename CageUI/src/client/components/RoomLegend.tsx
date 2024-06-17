cca8a36b3b7c270288176d45ad24807590133cb4
LeviCameron1 <lcameron3@wisc.edu>
2024-06-17 11:56:25 -0500

    Added some legend styling

--- CageUI/src/client/components/RoomLegend.tsx
+++ CageUI/src/client/components/RoomLegend.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { FC, useState } from 'react';
+import { FC, useEffect, useState } from 'react';
 import { ActionURL } from '@labkey/api';
 import { changeStyleProperty, getRackSeparators, parseSeparator } from './helpers';
 import { ReactSVG } from 'react-svg';
@@ -7,8 +7,18 @@ import { ReactSVG } from 'react-svg';
 export const RoomLegend: FC<any> = () => {
     const [showLegend, setShowLegend] = useState<boolean>(true);
 
+    const [legendStyle, setLegendStyle] = useState("room-legend-open");
+    useEffect(() => {
+        if(showLegend){
+            setLegendStyle("room-legend-open");
+        }else{
+            setLegendStyle("room-legend-close");
+        }
+    }, [showLegend]);
+
+
     return (
-        <div className={"room-legend"}>
+        <div className={legendStyle}>
             <button className={"legend-header-btn"} onClick={() => setShowLegend(!showLegend)}>
                 <h2 className={'legend-header'}>Legend</h2>
             </button>

17044cbeb76cfa6aa246fd1092d747bd571b92cc
Marty Pradere <martyp@labkey.com>
2024-06-21 05:51:30 -0700

    Use wnprc specific protocol count verification

--- WNPRC_EHR/resources/scripts/wnprc_ehr/wnprc_triggers.js
+++ WNPRC_EHR/resources/scripts/wnprc_ehr/wnprc_triggers.js
@@ -22,6 +22,7 @@ exports.init = function (EHR) {
 
     EHR.Server.TriggerManager.registerHandler(EHR.Server.TriggerManager.Events.INIT, function (event, helper, EHR) {
         EHR.Server.TriggerManager.unregisterAllHandlersForQueryNameAndEvent('study', 'blood', EHR.Server.TriggerManager.Events.BEFORE_UPSERT);
+        EHR.Server.TriggerManager.unregisterAllHandlersForQueryNameAndEvent('study', 'assignment', EHR.Server.TriggerManager.Events.BEFORE_UPSERT);
         setStudyBloodBeforeUpsertTrigger();
     });
 

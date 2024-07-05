7b7b202727373b983bca4dbcbeafa9b4ff39637f
Chad Sebranek <csebranek@gmail.com>
2024-07-05 13:54:43 -0500

    restore comment

--- wnprc_billing/resources/queries/wnprc_billing/procedureFeesWithTierRates.sql
+++ wnprc_billing/resources/queries/wnprc_billing/procedureFeesWithTierRates.sql
@@ -1,4 +1,4 @@
- gltet accounts that are in ehr_billing.aliases for active or null tierRates
+-- get accounts that are in ehr_billing.aliases for active or null tierRates
 SELECT * FROM
   (
     SELECT

e581029407fec73b500b5bb9a4bdf41950fe6f8e
Cory Nathe <cnathe@labkey.com>
2024-07-10 14:50:14 -0500

    Update entryPoints.js to use permissionClasses instead of permission property (#608)
    * Update entryPoints.js to use permissionClasses instead of permission property
    * Remove entryPoints.js permission props for generateLib since they don't apply for libs

--- WNPRC_EHR/src/client/entryPoints.js
+++ WNPRC_EHR/src/client/entryPoints.js
@@ -2,51 +2,50 @@ module.exports = {
     apps: [{
         name: 'grid_panel',
         title: 'Default Grid',
-        permission: 'read',
+        permissionClasses: ['org.labkey.api.security.permissions.ReadPermission'],
         path: './src/client/components/grid_panel'
     },{
         name: 'grid_panel_webpart',
         title: 'Default Grid Webpart',
-        permission: 'read',
+        permissionClasses: ['org.labkey.api.security.permissions.ReadPermission'],
         path: './src/client/components/grid_panel/webpart',
         generateLib: true
     },{
         name: 'breeding',
         title: 'Pregnancies',
-        permission: 'read',
+        permissionClasses: ['org.labkey.api.security.permissions.ReadPermission'],
         path: './src/client/breeding'
     },{
         name: 'breeding_webpart',
         title: 'Pregnancies Webpart',
-        permission: 'read',
+        permissionClasses: ['org.labkey.api.security.permissions.ReadPermission'],
         path: './src/client/breeding/webpart',
         generateLib: true
     },{
         name: 'feeding',
         title: 'Feeding',
-        permission: 'read',
+        permissionClasses: ['org.labkey.api.security.permissions.ReadPermission'],
         path: './src/client/feeding/base'
     },{
         name: 'abstract',
         title: 'Abstract',
-        permission: 'read',
         path: './src/client/abstract/base',
         generateLib: true
     },{
         name: 'research_ultrasounds',
         title: 'Research Ultrasounds',
-        permission: 'read',
+        permissionClasses: ['org.labkey.api.security.permissions.ReadPermission'],
         path: './src/client/researchUltrasounds'
     },{
         name: 'research_ultrasounds_webpart',
         title: 'Research Ultrasounds Webpart',
-        permission: 'read',
         path: './src/client/researchUltrasounds/webpart',
         generateLib: true
     },{
         name: 'weight',
         title: 'Weight',
-        permission: 'login',
+        // permission: 'login',
+        permissionClasses: ['org.labkey.api.security.permissions.ReadPermission'],
         path: './src/client/weight'
     }]
 };

2c148e3d52f7e1c213ddbb9dd3f762cabc1cb295
aschmidt34 <124093649+aschmidt34@users.noreply.github.com>
2024-08-13 12:20:12 -0500

    24.3 fb notification revamps (#637)
    * - In Blood Draws Today (All, Animal Care, and Vet Staff), I changed 'Assignment Status' column header to 'Unassigned' and 'Completion Status' to 'Incomplete'.  I also sorted all results so Incomplete draws show up first.
    - In BloodDrawReviewTriggerNotification, I made it so warnings only send when the draw has a date of today.
    - In BloodDrawReviewDailyNotification, I added a check to verify that there are no upcoming blood draws that will overdraw.
    
    - TODO: After testing this on test servers, make sure to remove test dates in BloodDrawsTodayAll and BloodDrawReviewDailyNotification.
    
    * Migrated automated test changes from 23.11 to 24.3.  23.11_notificationRevamps is now ready to be deleted.
    
    * Added blood overdraw trigger notification.
    
    * Registered new notification.
    
    * Updated code so message sends when testing/running in browser.  Updated log for wnprc_triggers.js for when function is run to help debug until issue with webpack generation is fixed.
    
    * Updated overdraw notification.  Notification is now created everytime blood is updated.  Logic is run inside notification to determine if current draw is an overdraw.  If it is an overdraw, the message is sent.
    
    * Fixed issue where 'Animal Replacement Fee' showed incorrectly in the revamped Death Notification.  This occurred when the Type of Death resulted in a fee in the table 'ehr_lookups' > 'death_cause', but the 'prepaid' field was empty in the table 'study' > 'demographics'.  This was due to labkeys table lookup resulting in a string 'null' instead of a real null when using the TableSelector.  My new notifications use a new query function i wrote, but this Death Notification used the old TableSelector method.
    
    * Reordered messages in BloodDrawReviewDailyNotification.java so overdraws are listed first.
    
    * -NotificationToolkit.java: Updated getWeightFromAnimalID() and getSexFromAnimalID() so they use new query function getTableMultiRowMultiColumnWithFieldKeys() instead of old function getTableRowAsList().  Also moved DeathNecropsyObject and DeathDemographicObject to DeathNotificationRevamp.java.
    - DeathNotificationRevamp.java: Moved DeathNecropsyObject and DeathDemographicObject here from NotificationToolkit.java.  Also cleaned up these objects and updated these them to use the new query getTableMultiRowMultiColumnWithFieldKeys.
    
    * Removed automated tests that don't aren't finished.
    
    * Added 2 new functions to NotificationToolkit for creating URL's.  This is to avoid hardcoding URL's per new LabKey policy.  Implimented these 2 new functions in DeathNotificationRevamp.java for getting the necropsy and animal abstract URL's.

--- WNPRC_EHR/src/org/labkey/wnprc_ehr/notification/DeathNotificationRevamp.java
+++ WNPRC_EHR/src/org/labkey/wnprc_ehr/notification/DeathNotificationRevamp.java
@@ -1,10 +1,16 @@
 package org.labkey.wnprc_ehr.notification;
 
+import org.labkey.api.data.CompareType;
 import org.labkey.api.data.Container;
+import org.labkey.api.data.SimpleFilter;
+import org.labkey.api.data.Sort;
 import org.labkey.api.module.Module;
 import org.labkey.api.security.User;
+import org.labkey.api.util.Path;
+import org.labkey.api.view.ActionURL;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * Created by Alex Schmidt on
@@ -14,9 +20,9 @@ public class DeathNotificationRevamp extends AbstractEHRNotification {
     //Class Variables
     NotificationToolkit notificationToolkit = new NotificationToolkit();
     NotificationToolkit.StyleToolkit styleToolkit = new NotificationToolkit.StyleToolkit();
-    public String deathID;
-    public User currentUser;
-    public String hostName;
+    String deathID = null;
+    User currentUser = null;
+    String hostName = null;
 
 
     //Constructors
@@ -35,9 +41,9 @@ public class DeathNotificationRevamp extends AbstractEHRNotification {
      */
     public DeathNotificationRevamp(Module owner, String deathid, User currentuser, String hostname) {
         super(owner);
-        deathID = deathid;
-        currentUser = currentuser;
-        hostName = hostname;
+        this.deathID = deathid;
+        this.currentUser = currentuser;
+        this.hostName = hostname;
     }
 
 
@@ -98,11 +104,14 @@ public class DeathNotificationRevamp extends AbstractEHRNotification {
         final StringBuilder messageBody = new StringBuilder();
         if (deathID == null) {
             //If no ID exists (testing purposes), this gets the most recent necropsy animal id.
-            ArrayList<String[]> necropsiesInOrder = notificationToolkit.getTableAsList(c, u, "necropsy", "mostRecentNecropsy");
-            deathID = necropsiesInOrder.get(0)[0].toString();
+            Sort mySort = new Sort("-date");
+            String[] targetColumns = new String[]{"id"};
+            ArrayList<HashMap<String, String>> necropsyList = notificationToolkit.getTableMultiRowMultiColumnWithFieldKeys(c, u, "study", "necropsy", null, mySort, targetColumns);
+            deathID = necropsyList.get(0).get("id");
         }
-        NotificationToolkit.DeathDemographicObject deathDemographicObject = new NotificationToolkit.DeathDemographicObject(c, u, deathID, true);
-        NotificationToolkit.DeathNecropsyObject deathNecropsyObject = new NotificationToolkit.DeathNecropsyObject(c, u, deathID, hostName, true);
+        DeathDemographicObject deathDemographicObject = new DeathDemographicObject(c, u, deathID, true);
+//        DeathNecropsyObject deathNecropsyObject = new DeathNecropsyObject(c, u, deathID, hostName, true);
+        DeathNecropsyObject deathNecropsyObject = new DeathNecropsyObject(c, u, deathID, true);
 
         //Creates CSS.
         messageBody.append(styleToolkit.beginStyle());
@@ -141,7 +150,264 @@ public class DeathNotificationRevamp extends AbstractEHRNotification {
             messageBody.append("<p><b>Manner of Death:</b> " + deathNecropsyObject.necropsyMannerOfDeath + "</p>");
         }
 
+        // Resets data.  This is necessary to get 'this.deathID' to reset everytime it's run in the browser.
+        this.deathID = null;
+
         //Returns string.
         return messageBody.toString();
+    }
+
+
+
+
+
+    /**
+     * This is an object used in the WNPRC DeathNotification.java file that defines all the info presented for a dead animal's necropsy.
+     * It contains the following data (returning blank strings for non-existent data):
+     *  If necropsy exists (true/false).
+     *  Necropsy case number.
+     *  Necropsy task id hyperlink.
+     *  Necropsy date.
+     *  Necropsy time of death.
+     *  Necropsy type of death.
+     *  Necropsy grant number.
+     *  Necropsy manner of death.
+     *  Necropsy animal weight.
+     *  Necropsy animal replacement fee.
+     */
+    public static class DeathNecropsyObject {
+        Boolean necropsyExists = false;
+        String necropsyCaseNumber = "";
+        String necropsyTaskIdHyperlink = "";
+        String necropsyDate = "";
+        String necropsyTimeOfDeath = "";
+        String necropsyTypeOfDeath = "";
+        String necropsyGrantNumber = "";
+        String necropsyMannerOfDeath = "";
+        String animalWeight = "";
+        String animalReplacementFee = "";
+
+        public DeathNecropsyObject(Container c, User u, String animalID, Boolean withHtmlPlaceHolders) {
+            NotificationToolkit notificationToolkit = new NotificationToolkit();
+
+            // Creates filter.
+            SimpleFilter myFilter = new SimpleFilter("id", animalID, CompareType.EQUAL);
+            String[] targetColumns = new String[]{"caseno", "taskid", "date", "timeofdeath", "causeofdeath", "account", "mannerofdeath"};
+            // Runs query.
+            ArrayList<HashMap<String, String>> animalNecropsy = notificationToolkit.getTableMultiRowMultiColumnWithFieldKeys(c, u, "study", "necropsy", myFilter, null, targetColumns);
+
+            // Assigns values to object variables.
+            if (!animalNecropsy.isEmpty()) {
+                this.necropsyExists = true;
+                //Gets necropsy data.
+                this.necropsyCaseNumber = animalNecropsy.get(0).get("caseno");
+                this.necropsyDate = animalNecropsy.get(0).get("date");
+                this.necropsyTimeOfDeath = animalNecropsy.get(0).get("timeofdeath");
+                this.necropsyTypeOfDeath = animalNecropsy.get(0).get("causeofdeath");
+                this.necropsyGrantNumber = animalNecropsy.get(0).get("account");
+                this.necropsyMannerOfDeath = animalNecropsy.get(0).get("mannerofdeath");
+                this.animalWeight = notificationToolkit.getWeightFromAnimalID(c, u, animalID);
+                this.animalReplacementFee = notificationToolkit.getAnimalReplacementFee(c, u, this.necropsyTypeOfDeath, animalID);
+
+                //Creates task id with hyperlink.
+                String necropsyTaskID = animalNecropsy.get(0).get("taskid");
+                String taskUrlAsString = notificationToolkit.createFormURL(c, "necropsy", necropsyTaskID);
+
+                // Creates filter.
+                SimpleFilter myTaskFilter = new SimpleFilter("taskid", necropsyTaskID, CompareType.EQUAL);
+                String[] targetTaskColumns = new String[]{"rowid"};
+                // Runs query.
+                ArrayList<HashMap<String, String>> necropsyTask = notificationToolkit.getTableMultiRowMultiColumnWithFieldKeys(c, u, "ehr", "tasks", myTaskFilter, null, targetTaskColumns);
+                String taskRowID = necropsyTask.get(0).get("rowid");
+                this.necropsyTaskIdHyperlink = notificationToolkit.createHyperlink(taskRowID, taskUrlAsString);
+            }
+
+            // Assigns 'Not Specified' placeholders for blank values.
+            if (withHtmlPlaceHolders) {
+                String placeholderText = "<em>Not Specified</em>";
+                if (this.necropsyCaseNumber.isEmpty()) {
+                    this.necropsyCaseNumber = placeholderText;
+                }
+                if (this.necropsyTaskIdHyperlink.isEmpty()) {
+                    this.necropsyTaskIdHyperlink = placeholderText;
+                }
+                if (this.necropsyDate.isEmpty()) {
+                    this.necropsyDate = placeholderText;
+                }
+                if (this.necropsyTimeOfDeath.isEmpty()) {
+                    this.necropsyTimeOfDeath = placeholderText;
+                }
+                if (this.necropsyTypeOfDeath.isEmpty()) {
+                    this.necropsyTypeOfDeath = placeholderText;
+                }
+                if (this.necropsyGrantNumber.isEmpty()) {
+                    this.necropsyGrantNumber = placeholderText;
+                }
+                if (this.necropsyMannerOfDeath.isEmpty()) {
+                    this.necropsyMannerOfDeath = placeholderText;
+                }
+                if (this.animalWeight.isEmpty()) {
+                    this.animalWeight = placeholderText;
+                }
+                if (this.animalReplacementFee.isEmpty()) {
+                    this.animalReplacementFee = placeholderText;
+                }
+            }
+        }
+
+//        public DeathNecropsyObject(Container c, User u, String animalID, String hostName, Boolean withHtmlPlaceHolders) {
+//            NotificationToolkit notificationToolkit = new NotificationToolkit();
+//
+//            if (notificationToolkit.getTableRowCount(c, u, "study", "Necropsy", "notificationView") > 0) {  //Added this if/else check to getTableRowAsList(), I can remove this after testing.
+//                String[] targetColumns = {"caseno", "taskid", "date", "timeofdeath", "causeofdeath", "account", "mannerofdeath"};
+//
+//                ArrayList<String> necropsyTableRow = notificationToolkit.getTableRowAsList(c, u, "study", "necropsy", null, "id", animalID, targetColumns);
+//
+//                //Necropsy does exist.
+//                if (!necropsyTableRow.isEmpty()) {
+//                    this.necropsyExists = true;
+//                    //Gets necropsy data.
+//                    this.necropsyCaseNumber = necropsyTableRow.get(0);
+//                    this.necropsyDate = necropsyTableRow.get(2);
+//                    this.necropsyTimeOfDeath = necropsyTableRow.get(3);
+//                    this.necropsyTypeOfDeath = necropsyTableRow.get(4);
+//                    this.necropsyGrantNumber = necropsyTableRow.get(5);
+//                    this.necropsyMannerOfDeath = necropsyTableRow.get(6);
+//                    this.animalWeight = notificationToolkit.getWeightFromAnimalID(c, u, animalID);
+//                    this.animalReplacementFee = notificationToolkit.getAnimalReplacementFee(c, u, this.necropsyTypeOfDeath, animalID);
+//
+//                    //Creates task id with hyperlink.
+//                    String necropsyTaskID = necropsyTableRow.get(1);
+//                    Path taskURL = new Path(ActionURL.getBaseServerURL(), "ehr", c.getPath(), "taskDetails.view");
+//                    String taskUrlAsString = taskURL.toString() + "?formtype=Necropsy&taskid=" + necropsyTaskID;
+//                    String taskRowID = "";
+//                    if (notificationToolkit.getTableRowCount(c, u, "ehr", "tasks", "") > 0) {  //Added this if/else check to getTableRowAsList(), I can remove this after testing.
+//                        ArrayList<String> taskRow = notificationToolkit.getTableRowAsList(c, u, "ehr", "tasks", null, "taskid", necropsyTaskID, new String[]{"rowid"});
+//                        if (!taskRow.isEmpty()) {
+//                            taskRowID = taskRow.get(0);
+//                        }
+//                        this.necropsyTaskIdHyperlink = notificationToolkit.createHyperlink(taskRowID, taskUrlAsString);
+//                    }
+//                }
+//            }
+//            if (withHtmlPlaceHolders) {
+//                String placeholderText = "<em>Not Specified</em>";
+//                if (this.necropsyCaseNumber == null) {
+//                    this.necropsyCaseNumber = placeholderText;
+//                }
+//                else if (this.necropsyCaseNumber.equals("") || this.necropsyCaseNumber.equals("null")) {
+//                    this.necropsyCaseNumber = placeholderText;
+//                }
+//                if (this.necropsyTaskIdHyperlink == null) {
+//                    this.necropsyTaskIdHyperlink = placeholderText;
+//                }
+//                else if (this.necropsyTaskIdHyperlink.equals("") || this.necropsyTaskIdHyperlink.equals("null")) {
+//                    this.necropsyTaskIdHyperlink = placeholderText;
+//                }
+//                if (this.necropsyDate == null) {
+//                    this.necropsyDate = placeholderText;
+//                }
+//                else if (this.necropsyDate.equals("") || this.necropsyDate.equals("null")) {
+//                    this.necropsyDate = placeholderText;
+//                }
+//                if (this.necropsyTimeOfDeath == null) {
+//                    this.necropsyTimeOfDeath = placeholderText;
+//                }
+//                else if (this.necropsyTimeOfDeath.equals("") || this.necropsyTimeOfDeath.equals("null")) {
+//                    this.necropsyTimeOfDeath = placeholderText;
+//                }
+//                if (this.necropsyTypeOfDeath == null) {
+//                    this.necropsyTypeOfDeath = placeholderText;
+//                }
+//                else if (this.necropsyTypeOfDeath.equals("") || this.necropsyTypeOfDeath.equals("null")) {
+//                    this.necropsyTypeOfDeath = placeholderText;
+//                }
+//                if (this.necropsyGrantNumber == null) {
+//                    this.necropsyGrantNumber = placeholderText;
+//                }
+//                else if (this.necropsyGrantNumber.equals("") || this.necropsyGrantNumber.equals("null")) {
+//                    this.necropsyGrantNumber = placeholderText;
+//                }
+//                if (this.necropsyMannerOfDeath == null) {
+//                    this.necropsyMannerOfDeath = placeholderText;
+//                }
+//                else if (this.necropsyMannerOfDeath.equals("") || this.necropsyMannerOfDeath.equals("null")) {
+//                    this.necropsyMannerOfDeath = placeholderText;
+//                }
+//                if (this.animalWeight == null) {
+//                    this.animalWeight = placeholderText;
+//                }
+//                else if (this.animalWeight.equals("") || this.animalWeight.equals("null")) {
+//                    this.animalWeight = placeholderText;
+//                }
+//                if (this.animalReplacementFee == null) {
+//                    this.animalReplacementFee = placeholderText;
+//                }
+//                else if (this.animalReplacementFee.equals("") || this.animalReplacementFee.equals("null")) {
+//                    this.animalReplacementFee = placeholderText;
+//                }
+//            }
+//        }
+    }
+
+    /**
+     * This is an object used in the WNPRC DeathNotification.java file that defines all the info presented for a dead animal's demographics.
+     * It contains the following data (returning blank strings for non-existent data):
+     *  Animal ID hyperlink.
+     *  Animal sex.
+     */
+    public static class DeathDemographicObject {
+        String animalIdHyperlink = "";
+        String animalSex = "";
+        String animalDam = "";
+        String animalSire = "";
+        String animalConception = "";
+        public DeathDemographicObject(Container c, User u, String animalID, Boolean withHtmlPlaceHolders) {
+            NotificationToolkit notificationToolkit = new NotificationToolkit();
+
+            //Gets hyperlink for animal id in animal history abstract.
+            String animalAbstractUrlAsString = notificationToolkit.createAnimalHistoryURL(c, animalID);
+            this.animalIdHyperlink = notificationToolkit.createHyperlink(animalID, animalAbstractUrlAsString);
+
+            //Gets animal sex.
+            String animalSex = notificationToolkit.getSexFromAnimalID(c, u, animalID);
+            this.animalSex = animalSex;
+
+            //Gets prenatal information if necessary.
+            if (notificationToolkit.checkIfPrenatalID(animalID)) {
+                // Creates filter.
+                SimpleFilter myFilter = new SimpleFilter("id", animalID, CompareType.EQUAL);
+                String[] targetColumns = new String[]{"dam", "sire", "conception"};
+                // Runs query.
+                ArrayList<HashMap<String, String>> prenatalDeathRow = notificationToolkit.getTableMultiRowMultiColumnWithFieldKeys(c, u, "study", "prenatal", myFilter, null, targetColumns);
+
+                // Checks return data.
+                if (!prenatalDeathRow.isEmpty()) {
+                    this.animalDam = prenatalDeathRow.get(0).get("dam");
+                    this.animalSire = prenatalDeathRow.get(0).get("sire");
+                    this.animalConception = prenatalDeathRow.get(0).get("conception");
+                }
+            }
+
+            //Adds HTML placeholders for empty fields.
+            if (withHtmlPlaceHolders) {
+                String placeholderText = "<em>Not Specified</em>";
+                if (this.animalIdHyperlink.isEmpty()) {
+                    this.animalIdHyperlink = placeholderText;
+                }
+                if (this.animalSex.isEmpty()) {
+                    this.animalSex = placeholderText;
+                }
+                if (this.animalDam.isEmpty()) {
+                    this.animalDam = placeholderText;
+                }
+                if (this.animalSire.isEmpty()) {
+                    this.animalSire = placeholderText;
+                }
+                if (this.animalConception.isEmpty()) {
+                    this.animalConception = placeholderText;
+                }
+            }
+        }
     }
 }

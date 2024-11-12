772b1ef723ed61a705880060b16a856179aacf37
F. Daniel Nicolalde <dnicolalde@gmail.com>
2024-11-12 12:54:24 -0600

    adding report for water total so it does not request start and end date for the report. Adding a height for Water Grid Calendar.

--- WNPRC_EHR/resources/web/wnprc_ehr/wnprcHusbandryReports.js
+++ WNPRC_EHR/resources/web/wnprc_ehr/wnprcHusbandryReports.js
@@ -24,8 +24,40 @@ EHR.reports.waterGridCalendar = function (panel, tab) {
     tab.add({
         xtype: 'ldk-querypanel',
         style: 'margin-bottom:20px;',
-        queryConfig: config
+        queryConfig: config,
+        height: 3000
 
     });
     
+};
+
+EHR.reports.totalWaterByDay = function (panel, tab) {
+    var filterArray = panel.getFilterArray(tab);
+    var title = panel.getTitleSuffix();
+
+    var target = tab.add({tag: 'span', style: 'padding-bottom: 20px'});
+    tab.doLayout();
+    var reportStartDate = new Date("2021-01-01");
+    reportStartDate = reportStartDate.format(LABKEY.extDefaultDateFormat);
+    var reportEndDate = new Date();
+    reportEndDate = reportEndDate.format(LABKEY.extDefaultDateFormat);
+
+    var config = panel.getQWPConfig({
+        title: 'Total Water By Date',
+        schemaName: 'study',
+        queryName: 'waterTotalByDateWithWeight',
+        parameters: {'STARTTARGET': reportStartDate, 'ENDTARGETDATE': reportEndDate},
+        filters: filterArray.nonRemovable,
+        removeableFilters: filterArray.removable,
+        frame: true
+
+    });
+
+    tab.add({
+        xtype: 'ldk-querypanel',
+        style: 'margin-bottom:20px;',
+        queryConfig: config
+
+    });
+
 };

322dc3ade33e33c89da77f494a0aa29c0faf1938
Chad Sebranek <csebranek@gmail.com>
2025-02-14 10:03:18 -0600

    update WNPRC_Virology npm packages with new lock file

--- WNPRC_Virology/src/client/AccountsForm/app.tsx
+++ WNPRC_Virology/src/client/AccountsForm/app.tsx
@@ -5,6 +5,9 @@ import DropdownContainer from "./DropdownContainer";
 import { App } from "@labkey/api";
 import * as ReactDOM from "react-dom";
 import { DropdownContainerProps } from '../typings/main';
+import { createRoot } from 'react-dom/client';
 App.registerApp<any>('DropdownSelect', (target: string, ctx: DropdownContainerProps) => {
-    ReactDOM.render(<ContextProvider><DropdownContainer update={ctx.update} /></ContextProvider>, document.getElementById(target));
+    const container = document.getElementById(target);
+    const root = createRoot(container); // createRoot(container!) if you use TypeScript
+    root.render(<ContextProvider><DropdownContainer update={ctx.update} /></ContextProvider>);
 });

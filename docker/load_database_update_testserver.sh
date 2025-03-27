80a5f6b90c1bd6b7fa8529a04eab60bc3d8030f3
F. Daniel Nicolalde <fdnicolalde@wisc.edu>
2025-03-27 15:58:53 -0500

    Prune system

--- docker/load_database_update_testserver.sh
+++ docker/load_database_update_testserver.sh
@@ -259,11 +259,14 @@ echo
 
 #-------------------------------------------------------------------------------
 # Updating Docker image
-#
+# Removing unused images in the system
 #-------------------------------------------------------------------------------
 if [[ -z $dock ]]; then
   echo -n 'Updating Docker images from Docker Hub'
   docker pull wnprcehr/labkeysnapshot:24.11
+  echo -e '\033[0;32mdone\033[0m'
+  echo -n 'Removing unused images from the OS'
+  docker image prune -a -f
   echo -e '\033[0;32mdone\033[0m'
 fi
 

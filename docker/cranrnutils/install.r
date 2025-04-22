18e0428ee93555846d7c4ef4189258261f693654
F. Daniel Nicolalde <fdnicolalde@wisc.edu>
2025-04-22 13:32:56 -0500

    separating remotes to be installed first

--- docker/cranrnutils/install.r
+++ docker/cranrnutils/install.r
@@ -1,8 +1,8 @@
 n <- max(parallel::detectCores() - 2L, 1L)
 print(n)
 #options(Ncpus=n)
-
-install.packages(c('devtools','quadprog','pedigree','Matrix', 'RCurl','kinship2','getopt', 'rjson','dplyr','remotes','Rlabkey'), repos='http://cran.us.r-project.org')
+install.packages('remotes', repos='http://cran.us.r-project.org')
+install.packages(c('devtools','quadprog','pedigree','Matrix', 'RCurl','kinship2','getopt', 'rjson','dplyr','Rlabkey'), repos='http://cran.us.r-project.org')
 
 require('remotes')
 install_github("luansheng/visPedigree")

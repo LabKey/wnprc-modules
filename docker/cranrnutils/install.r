a4a2380ce7057ba6ad58d406b60d32893d788016
F. Daniel Nicolalde <fdnicolalde@wisc.edu>
2025-04-23 10:56:33 -0500

    removing remotes and vizpedigree

--- docker/cranrnutils/install.r
+++ docker/cranrnutils/install.r
@@ -1,8 +1,13 @@
 n <- max(parallel::detectCores() - 2L, 1L)
 print(n)
 #options(Ncpus=n)
-install.packages('remotes', repos='http://cran.us.r-project.org')
-install.packages(c('devtools','quadprog','pedigree','Matrix', 'RCurl','kinship2','getopt', 'rjson','dplyr','Rlabkey'), repos='http://cran.us.r-project.org')
-
-require('remotes')
-install_github("luansheng/visPedigree")
+#install.packages('remotes', 
+#                  quiet = FALSE, 
+#                  verbose = TRUE, 
+#                  repos='http://cran.us.r-project.org')
+install.packages(c('devtools','quadprog','pedigree','Matrix', 'RCurl','kinship2','getopt', 'rjson','dplyr','Rlabkey'), 
+                  quiet = FALSE, 
+                  verbose = TRUE, 
+                  repos='http://cran.us.r-project.org')
+#require('remotes')
+#install_github("luansheng/visPedigree")

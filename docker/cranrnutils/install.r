560b450c8956a4ff11c0ad033ea2045a31cb6682
F. Daniel Nicolalde <fdnicolalde@wisc.edu>
2025-04-29 11:43:34 -0500

    removing dowload only scripts

--- docker/cranrnutils/install.r
+++ docker/cranrnutils/install.r
@@ -1,14 +1,14 @@
 n <- max(parallel::detectCores() - 2L, 1L)
 print(n)
 #options(Ncpus=n)
-#install.packages('remotes', 
-#                  quiet = FALSE, 
-#                  verbose = TRUE, 
-#                  repos='http://cran.us.r-project.org')
+install.packages('remotes', 
+                  quiet = FALSE, 
+                  verbose = TRUE, 
+                  repos='http://cran.us.r-project.org')
 install.packages(c('devtools','quadprog','pedigree','Matrix', 'RCurl','kinship2','getopt', 'rjson','dplyr','Rlabkey'), 
                   quiet = FALSE, 
                   verbose = TRUE, 
                   Ncpus=n,
                   repos='http://cran.us.r-project.org')
-#require('remotes')
-#install_github("luansheng/visPedigree")
+require('remotes')
+install_github("luansheng/visPedigree")

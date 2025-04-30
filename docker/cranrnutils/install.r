eefd9f45105ec1f97aca5332b86be06854770c60
F. Daniel Nicolalde <fdnicolalde@wisc.edu>
2025-04-30 10:46:26 -0500

    Adding a script to have a up to date test server that restore nightly (#784)
    * Adding timeout to docker down
    
    * new script to load a uptodate test server
    
    * adding timeout to docker down
    
    * making new file executable
    
    * adding tmp directory
    
    * adding restore file location
    
    * adding random tmp directory
    
    * removing / from restorefile
    
    * Adding wait time for postgres
    
    * Adding wait time for postgres to start. Starting rest of containers.
    
    * adding PATH for cron job
    
    * fixing typo
    
    * adding some echos
    
    * Adding whole path for postgres file
    
    * Changing directory
    
    * Adding more echo outputs
    
    * Updating docker image to latest build from Docker Hub
    
    * removing \n and adding jobs as parameter.
    
    * correcting typo
    
    * Adding default jobs
    
    * Chaging theme of test server and name
    
    * Changing different properties from production db
    
    * Prune system
    
    * Moving prune after docker start up
    
    * getting labkey version from .env file
    
    * Adding URL for test server via module property in WNPRC Module
    
    * Adding target to open a new tab when accessing secondary test server
    
    * Adding rsync for files folder
    
    * Adding file location via .env file
    
    * correcting new env format
    
    * adding additinal platform for arm hardware
    
    * adding special driver to build arm64
    
    * Adding builder to use arm64
    
    * improving comment
    
    * Adding two step process to download packages first and than install
    
    * adding support for multiple platform installation for R using rig
    
    * usign bash script to download R
    
    * Changin buildard to targetarch
    
    * adding specific builder to create mutliple platform image
    
    * removing download r script and using parallel install
    
    * removing parallel building
    
    * separating remotes to be installed first
    
    * removing remotes and vizpedigree
    
    * simplifying dockerfile for cranutils
    
    * using build script to build locally and adding platform variable
    
    * fixing location of dockerfile
    
    * enabling multiple cpus
    
    * Adding documentation about docker services
    
    * adding repos
    
    * Adding documentation
    
    * fixing warnings for dockerfile
    
    * Removing wite spaces
    
    * removing dowload only scripts
    
    * simplifying insllation remotes
    
    * adding some log for docker hub

--- docker/cranrnutils/install.r
+++ docker/cranrnutils/install.r
@@ -1,14 +1,13 @@
-install.packages('devtools', repos='http://cran.us.r-project.org')
-install.packages('quadprog', repos='http://cran.us.r-project.org')
-install.packages('pedigree', repos='http://cran.us.r-project.org')
-install.packages('Matrix',   repos='http://cran.us.r-project.org')
-install.packages('RCurl',    repos='http://cran.us.r-project.org')
-install.packages('kinship2', repos='http://cran.us.r-project.org')
-install.packages('getopt',   repos='http://cran.us.r-project.org')
-install.packages('rjson',    repos='http://cran.us.r-project.org')
-install.packages('dplyr',    repos='http://cran.us.r-project.org')
-install.packages('remotes',  repos='http://cran.us.r-project.org')
-install.packages('Rlabkey',  repos='http://cran.us.r-project.org')
-
+n <- max(parallel::detectCores() - 2L, 1L)
+print(n)
+#options(Ncpus=n)
+install.packages('remotes', 
+                  quiet = FALSE, 
+                  verbose = TRUE)
+install.packages(c('devtools','quadprog','pedigree','Matrix', 'RCurl','kinship2','getopt', 'rjson','dplyr','Rlabkey'), 
+                  quiet = FALSE, 
+                  verbose = TRUE, 
+                  Ncpus=n,
+                  repos='http://cran.us.r-project.org')
 require('remotes')
 install_github("luansheng/visPedigree")

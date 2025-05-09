5b9b9a095a252a1d50048c99505bb005a9e544cf
F. Daniel Nicolalde <fdnicolalde@wisc.edu>
2025-05-09 15:59:29 -0500

    Rounding some results.

--- WNPRC_EHR/resources/queries/study/currentBloodDraws.sql
+++ WNPRC_EHR/resources/queries/study/currentBloodDraws.sql
@@ -27,10 +27,10 @@ SELECT
     t.mostRecentWeightDate,
     t.death,
     cast(round(t.allowableBlood,1) as numeric) as maxAllowableBlood,
-    cast(t.bloodPrevious as double) as bloodPrevious,
-    cast((t.allowableBlood - t.bloodPrevious) as double) as allowablePrevious,
+    round(cast(t.bloodPrevious as double),1) as bloodPrevious,
+    round(cast((t.allowableBlood - t.bloodPrevious) as double),1) as allowablePrevious,
 
-    cast(t.bloodFuture as double) as bloodFuture,
+    round(cast(t.bloodFuture as double),1) as bloodFuture,
     cast((t.allowableBlood - t.bloodFuture) as double) as allowableFuture,
 
     ROUND(CAST((t.allowableBlood - t.bloodPrevious) AS double),1)  as allowableBlood,

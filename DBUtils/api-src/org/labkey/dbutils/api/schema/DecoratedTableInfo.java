a3edc8c5792aa45324e1df9042cbfb1e0b1e30d6
labkey-nicka <nickk@labkey.com>
2026-04-03 13:05:47 -0700

    TableInfo interface changes

--- DBUtils/api-src/org/labkey/dbutils/api/schema/DecoratedTableInfo.java
+++ DBUtils/api-src/org/labkey/dbutils/api/schema/DecoratedTableInfo.java
@@ -396,8 +396,8 @@ public class DecoratedTableInfo implements TableInfo {
     }
 
     @Override
-    public void fireRowTrigger(Container c, User u, TriggerType type, boolean before, int rowNumber, @Nullable Map<String, Object> newRow, @Nullable Map<String, Object> oldRow, Map<String, Object> extraContext, @Nullable Map<String, Object> existingRecord) throws ValidationException {
-        _tableInfo.fireRowTrigger(c, u, type, before, rowNumber, newRow, oldRow, extraContext, existingRecord);
+    public void fireRowTrigger(Container c, User u, TriggerType type, boolean before, int rowNumber, @Nullable Map<String, Object> newRow, @Nullable Map<String, Object> oldRow, Map<String, Object> extraContext, @Nullable Map<String, Object> existingRecord, boolean manageColumns) throws ValidationException {
+        _tableInfo.fireRowTrigger(c, u, type, before, rowNumber, newRow, oldRow, extraContext, existingRecord, manageColumns);
     }
 
     @Override

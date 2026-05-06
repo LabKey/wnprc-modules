4a5369ff22b89ec97a008e6a5f6a17c322b150b3
LeviCameron1 <lcameron3@wisc.edu>
2026-05-06 12:58:08 -0500

    Use Martys suggestion for updating the pivot query

--- WNPRC_EHR/resources/queries/study/hematologyPivot.sql
+++ WNPRC_EHR/resources/queries/study/hematologyPivot.sql
@@ -1,38 +1,37 @@
-
 SELECT
-b.id,
-b.date,
-b.testId,
-group_concat(b.result) as results
+    b.id,
+    COALESCE(cpr.date, max(b.date)) as date,
+    b.testId,
+    group_concat(b.result) as results
 
 FROM (SELECT
-b.id,
-b.date,
-b.testId,
-coalesce(b.taskid, b.parentid, b.runId) as runId,
-b.resultoorindicator,
-CASE
-WHEN b.result IS NULL THEN  b.qualresult
-  ELSE CAST(CAST(ROUND(CAST(b.result AS NUMERIC), 4) AS DOUBLE) AS VARCHAR)
-END as result
-FROM study."Hematology Results" b
+          b.id,
+          b.date,
+          b.testId,
+          coalesce(b.taskid, b.parentid, b.runId) as runId,
+          b.resultoorindicator,
+          CASE
+              WHEN b.result IS NULL THEN b.qualresult
+              ELSE CAST(CAST(ROUND(CAST(b.result AS NUMERIC), 4) AS DOUBLE) AS VARCHAR)
+              END as result
+      FROM study."Hematology Results" b
 
-WHERE testId IN ('WBC', 'RBC', 'HGB', 'HCT', 'MCV', 'MCH','MCHC', 'RDW','RDW-SD', 'RDW-CV', 'PLT', 'MPV','PCV','NE','LY','MN','EO','BS','BANDS','METAMYELO','MYELO','TP','RETICULO', 'RETIC-AB', 'IRF', 'NRBC', 'NRBC#', 'RETIC HGB', 'IPF', 'PDW', 'P-LCR', 'PCT', 'LFR', 'MFR', 'HFR', 'PRO MYELO', 'ATYP', 'OTHER')
-and b.qcstate.publicdata = true
+      WHERE testId IN ('WBC', 'RBC', 'HGB', 'HCT', 'MCV', 'MCH','MCHC', 'RDW','RDW-SD', 'RDW-CV', 'PLT', 'MPV','PCV','NE','LY','MN','EO','BS','BANDS','METAMYELO','MYELO','TP','RETICULO', 'RETIC-AB', 'IRF', 'NRBC', 'NRBC#', 'RETIC HGB', 'IPF', 'PDW', 'P-LCR', 'PCT', 'LFR', 'MFR', 'HFR', 'PRO MYELO', 'ATYP', 'OTHER')
+        and b.qcstate.publicdata = true
 
-UNION ALL
+      UNION ALL
 
-SELECT
-b.id,
-b.date,
-b.testId,
-coalesce(b.taskid, b.parentid) as runId,
-b.percentoorindicator,
-CAST(CAST(ROUND(CAST(b.result AS NUMERIC), 2) AS DOUBLE) AS VARCHAR)
-FROM study.hematologyAbsCount b
+      SELECT
+          b.id,
+          b.date,
+          b.testId,
+          coalesce(b.taskid, b.parentid) as runId,
+          b.percentoorindicator,
+          CAST(CAST(ROUND(CAST(b.result AS NUMERIC), 2) AS DOUBLE) AS VARCHAR) AS result
+      FROM study.hematologyAbsCount b
 
-) b
+     ) b
+         LEFT JOIN study.clinpathRuns cpr ON b.runId = cpr.objectId
 
-GROUP BY b.id, b.date, b.runId, b.testId
-PIVOT results BY testId IN ('WBC', 'RBC', 'HGB', 'HCT', 'MCV', 'MCH','MCHC', 'RDW','RDW-SD', 'RDW-CV', 'PLT', 'MPV','PCV','NE','NE-ABS','LY','LY-ABS','MN','MN-ABS','EO','EO-ABS','BS','BS-ABS','BANDS','BANDS-ABS','METAMYELO','MYELO','TP','RETICULO', 'RETIC-AB', 'IRF', 'NRBC', 'NRBC#', 'RETIC HGB', 'IPF', 'PDW', 'P-LCR', 'PCT', 'LFR', 'MFR', 'HFR', 'PRO MYELO', 'ATYP', 'OTHER')
-
+GROUP BY b.id, b.runId, b.testId, cpr.date
+    PIVOT results BY testId IN ('WBC', 'RBC', 'HGB', 'HCT', 'MCV', 'MCH','MCHC', 'RDW','RDW-SD', 'RDW-CV', 'PLT', 'MPV','PCV','NE','NE-ABS','LY','LY-ABS','MN','MN-ABS','EO','EO-ABS','BS','BS-ABS','BANDS','BANDS-ABS','METAMYELO','MYELO','TP','RETICULO', 'RETIC-AB', 'IRF', 'NRBC', 'NRBC#', 'RETIC HGB', 'IPF', 'PDW', 'P-LCR', 'PCT', 'LFR', 'MFR', 'HFR', 'PRO MYELO', 'ATYP', 'OTHER')

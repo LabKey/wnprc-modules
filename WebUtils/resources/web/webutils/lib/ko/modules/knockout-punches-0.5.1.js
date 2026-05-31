d3d652ce951eaa0e3e2a49cdf4256c5d0603f6ea
Adam Rauch <adam@labkey.com>
2026-05-31 12:21:35 -0700

    Back out a few licenses inadvertently applied to third-party components (#964)

--- WebUtils/resources/web/webutils/lib/ko/modules/knockout-punches-0.5.1.js
+++ WebUtils/resources/web/webutils/lib/ko/modules/knockout-punches-0.5.1.js
@@ -1,8 +1,3 @@
-/*
- * Copyright (c) 2018-2026 Board of Regents of the University of Wisconsin System
- *
- * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
- */
 /**
  * @license Knockout.Punches
  * Enhanced binding syntaxes for Knockout 3+

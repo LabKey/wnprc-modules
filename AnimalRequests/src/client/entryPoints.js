590dcfb5eb496ad007e7f9c0f5411e398559aedf
labkey-nicka <nickk@labkey.com>
2026-06-22 13:27:23 -0700

    AnimalRequests: migrate app to use @labkey/build

---
+++ AnimalRequests/src/client/entryPoints.js
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2026 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
+ */
+module.exports = {
+    apps: [{
+        name: 'app',
+        title: 'Animal Request Form',
+        // permissionClasses: ['org.labkey.api.security.permissions.ReadPermission'],
+        path: './src/client',
+        // generateLib: true, // used by views/animalRequest.html
+    }]
+};

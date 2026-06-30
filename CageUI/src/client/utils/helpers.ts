2f0faf0a1bf19417a2e5ecb4eefce788c5b8d409
LeviCameron1 <lcameron3@wisc.edu>
2026-06-30 13:35:39 -0500

    Remove numbers and context menus from ghost racks

--- CageUI/src/client/utils/helpers.ts
+++ CageUI/src/client/utils/helpers.ts
@@ -622,6 +622,8 @@ export const addPrevRoomSvgs = async (user: GetUserPermissionsResponse, mode: 'e
     // this function renders the actual visible svg in some groups
     const createRackGroup = (parentGroup, rack: Rack, isSingleRack, groupRotation: GroupRotation) => {
         const rackTypeString: RackStringType = roomItemToString(rack.type.type) as RackStringType;
+        // Ghost racks have 0 item id
+        const isGhostRack = rack.itemId === 0;
 
         const rackGroup = isSingleRack ? parentGroup : parentGroup.append('g')
             .attr('id', rack.svgId)
@@ -643,13 +645,13 @@ export const addPrevRoomSvgs = async (user: GetUserPermissionsResponse, mode: 'e
             shape.classed('draggable', false);
             shape.style('pointer-events', 'none');
 
-            // in order to set the event pass in the context menu ref and styles to show/hide it
-            (shape.select('tspan').node() as SVGTSpanElement).textContent = `${parseRoomItemNum(cage.cageNum)}`;
+            if(!isGhostRack){
+                (shape.select('tspan').node() as SVGTSpanElement).textContent = `${parseRoomItemNum(cage.cageNum)}`;
+            }
 
             if (mode === 'view') {
                 loadCageMods(cage, shape, groupRotation);
-                // Ghost racks have 0 item id
-                if(rack.itemId === 0){
+                if(isGhostRack){
                     shape.select('[id=cageRect]')
                         .style("fill", '#878787')
                         .style("opacity", '0.7');
@@ -662,9 +664,13 @@ export const addPrevRoomSvgs = async (user: GetUserPermissionsResponse, mode: 'e
             }
 
             cageGroup.append(() => shape.node());
-            // attach context menu if user has permissions for cages
-            if(canOpenContextMenu(user, rack.type.type)){
-                setupEditCageEvent(cageGroup.node(), setSelectedObj, contextMenuRef, mode, setCtxMenuStyle);
+            // Dont attach menus to ghost racks
+            if(!isGhostRack){
+                // attach context menu if user has permissions for cages
+                if(canOpenContextMenu(user, rack.type.type)){
+                    // in order to set the event pass in the context menu ref and styles to show/hide it
+                    setupEditCageEvent(cageGroup.node(), setSelectedObj, contextMenuRef, mode, setCtxMenuStyle);
+                }
             }
 
         });

219880ca5aa795e3d6f8c67173d9e0c379da6e5b
Nick Kerr <nickk@labkey.com>
2026-07-03 15:14:37 -0700

    Virology: package updates (#978)

--- WNPRC_Virology/src/client/helpers/helper.ts
+++ WNPRC_Virology/src/client/helpers/helper.ts
@@ -1,8 +1,7 @@
 import { Query } from '@labkey/api';
-import { SelectRowsOptions } from '@labkey/api/dist/labkey/query/SelectRows';
 
 export function labkeyActionSelectWithPromise(
-    options: SelectRowsOptions
+    options: Query.SelectRowsOptions
 ): Promise<any> {
   return new Promise((resolve, reject) => {
     options.success = (data) => {resolve(data)};
